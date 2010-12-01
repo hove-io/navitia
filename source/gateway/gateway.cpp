efa1d7555dc632f1dbb22619734369a11f1a799a
TGR <TGR@a734f7ea-df10-47d3-a172-9f42111c9061>
2010-12-01 17:48:49 +0000

    Ajout du squelette de la future gateway
    git-svn-id: https://ctp-gesconf.canaltp.fr/svn/NAViTiACpp/trunk@15 a734f7ea-df10-47d3-a172-9f42111c9061

---
+++ source/gateway/gateway.cpp
@@ -0,0 +1,104 @@
+#include "gateway.h"
+#include "http.h"
+#define FCGI
+#include "threadpool.h"
+#include "boost/lexical_cast.hpp"
+
+Navitia::Navitia(const std::string & server, const std::string & path) : server(server), path(path), error_count(0)
+{
+}
+
+std::string Navitia::query(const std::string & request)
+{
+    return get_http(server, path + request);
+}
+
+void NavitiaPool::add(const Navitia & n)
+{
+    navitias.push_back(n);
+    next_navitia = navitias.begin();
+}
+
+
+struct Worker {
+    webservice::ResponseData operator()(const webservice::RequestData & data, NavitiaPool & pool){
+        webservice::ResponseData resp;
+        resp.status_code = 200;
+        resp.content_type = "text/xml";
+        if(data.path == "/status"){
+            resp.response = "<info>Passerelle en FastCGI</info>";
+        }
+        else if(data.path == "/api" || data.path == "/const") {
+            try{
+                resp.response = pool.query( data.path + "?" + data.params );
+            }
+            catch(http_error e){
+                resp.response = "<error>"
+                    "<http code=\"";
+                resp.response += boost::lexical_cast<std::string>(e.code)+ "\">";
+                    resp.response += e.message;
+                    resp.response += "</http>"
+                    "</error>";
+            }
+        }
+        else {
+            resp.status_code = 400;
+            resp.response = "<error>API inconnue</error>";
+        }
+
+        return resp;
+    }
+};
+
+std::string NavitiaPool::query(const std::string & query)
+{
+
+    iter_mutex.lock();
+    next_navitia++;
+    if(next_navitia == navitias.end())
+        next_navitia = navitias.begin();
+    if(next_navitia == navitias.end())
+        return "Aucune instance de NAViTiA présente";
+    iter_mutex.unlock();
+
+    return next_navitia->query(query);
+}
+/*
+   void NavitiaPool::const_stats(std::string &data)
+   {
+   rapidxml::xml_document<> doc;    // character type defaults to char
+   char * data_ptr = doc.allocate_string(data.c_str());
+   doc.parse<0>(data_ptr);    // 0 means default parse flags
+
+   rapidxml::xml_node<> *node = doc.first_node("const");
+   if(node)
+   {
+   node = node->first_node("Thread");
+   if(node)
+   {
+   node = node->first_node("ActiveThread");
+   if (node)
+   {
+   const_calls++;
+   nb_threads_sum += atoi(node->value());
+   if(const_calls % 100 == 0){
+   std::cout << "Nombre d'appels : " << const_calls << " moyenne de threads actifs : " << (nb_threads_sum/const_calls) << std::endl;
+   }
+   }
+   }
+   }
+   }
+   */
+int main(int, char**)
+{
+    NavitiaPool pool;
+    pool.add(Navitia("10.2.0.16","/navitia/vfe/cgi-bin/navitia_GU.dll"));
+    pool.add(Navitia("10.2.0.16","/navitia/vfe1/cgi-bin/navitia_GU.dll"));
+    pool.add(Navitia("10.2.0.16","/navitia/vfe2/cgi-bin/navitia_GU.dll"));
+    pool.add(Navitia("10.2.0.16","/navitia/vfe3/cgi-bin/navitia_GU.dll"));
+    pool.add(Navitia("10.2.0.16","/navitia/vfe4/cgi-bin/navitia_GU.dll"));
+    pool.add(Navitia("10.2.0.16","/navitia/vfe5/cgi-bin/navitia_GU.dll"));
+    webservice::ThreadPool<NavitiaPool, Worker>(pool,64);
+    webservice::run_fcgi();
+    return 0;
+}
---
+++ source/gateway/gateway.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include <boost/thread.hpp>
+
+/** Contient toutes les informations relatives aux instances NAViTiA
+ * pilotées par la passerelle
+ */
+class Navitia{
+private:
+    /// Serveur utilisé
+    std::string server;
+
+    /// Chemin vers dll/fcgi que l'on désire interroger
+    std::string path;
+
+    /// Nombre d'erreurs rencontrées
+    int error_count;
+
+public:
+    /// Constructeur : on passe le serveur, et le chemin vers dll/fcgi que l'on désire utiliser
+    Navitia(const std::string & server, const std::string & path);
+
+    /// On interroge dll/fcgi avec la requête passée en paramètre
+    std::string query(const std::string & request);
+};
+
+/** Contient un ensemble de NAViTiA qui sont interrogeables */
+class NavitiaPool {
+private:
+    /// Iterateur vers le prochain NAViTiA à interroger
+    std::vector<Navitia>::iterator next_navitia;
+
+    /// Structure contenant l'ensemble des navitias
+    std::vector<Navitia> navitias;
+
+    /// Mutex pour proteger l'itérateur indiquant le prochain NAViTiA à utiliser
+    boost::mutex iter_mutex;
+
+public:
+    /// Rajoute un nouveau navitia au Pool. Celui-ci sera copié
+    void add(const Navitia & n);
+
+    /// Choisit un NAViTiA et lui fait executer la requête
+    std::string query(const std::string & query);
+};
+
