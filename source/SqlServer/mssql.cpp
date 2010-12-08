79224f790129a6639fa3132cd89a5501e1fc3f22
TGR <TGR@a734f7ea-df10-47d3-a172-9f42111c9061>
2010-12-08 15:09:49 +0000

    Wrapper MSSql avancé
    — procédures stockées
    — tests
    
    git-svn-id: https://ctp-gesconf.canaltp.fr/svn/NAViTiACpp/trunk@20 a734f7ea-df10-47d3-a172-9f42111c9061

--- source/SqlServer/mssql.cpp
+++ source/SqlServer/mssql.cpp
@@ -1,19 +1,103 @@
 #include "mssql.h"
-
-#include <iostream>
 #include <cstdlib>
+#include <cstdio>
+#include <cstring>
+
 namespace Sql {
+    int err_handler(DBPROCESS * dbproc, int severity, int dberr, int oserr, char *dberrstr, char *oserrstr) {									
+        std::cout << "ERR HANDLER" << std::endl;
+        if (dberr) {							
+            std::cerr << "Error " << dberr << ", level " << severity << std::endl;
+            std::cerr << dberrstr << std::endl;
+        }
+
+        else {
+            std::cerr << "Lib error" << std::endl;
+            std::cerr << dberrstr << std::endl;
+        }
+        return 0;
+    }
+
+    int msg_handler(DBPROCESS *dbproc, DBINT msgno, int msgstate, int severity, 
+            char *msgtext, char *srvname, char *procname, int line)
+    {
+        std::cout << "MSG HANDLER " << msgno << " " << msgtext << std::endl;
+        enum {changed_database = 5701, changed_language = 5703 };	
+
+        if (msgno == changed_database || msgno == changed_language) {
+            return 0;
+        }
+
+        if (msgno > 0) {
+            fprintf(stderr, "Msg %ld, Level %d, State %d\n", 
+                    (long) msgno, severity, msgstate);
+
+            if (strlen(srvname) > 0)
+                fprintf(stderr, "Server '%s', ", srvname);
+            if (strlen(procname) > 0)
+                fprintf(stderr, "Procedure '%s', ", procname);
+            if (line > 0)
+                fprintf(stderr, "Line %d", line);
+
+            fprintf(stderr, "\n\t");
+        }
+        fprintf(stderr, "MSG %s\n", msgtext);
+
+        if (severity > 10) {						
+            fprintf(stderr, "%s: error: severity %d > 10, exiting\n", 
+                    "bleh", severity);
+            exit(severity);
+        }
+        return 0;							
+    }
+
+
+    RegisteredProcedure::RegisteredProcedure(DBPROCESS * dbproc, char * procedure_name) : dbproc(dbproc) {
+        if(dbrpcinit(dbproc, procedure_name, (DBSMALLINT)0) == FAIL)
+            throw SqlException("Unable to initialize rpc");
+    }
+
+
+    RegisteredProcedure::~RegisteredProcedure() {
+        for(std::vector<int *>::iterator it = integers.begin(); it != integers.end(); it++)
+            delete *it;
+        integers.clear();
+        for(std::vector<std::string *>::iterator it = strings.begin(); it != strings.end(); it++)
+            delete *it;
+        strings.clear();
+    }
+
+    RegisteredProcedure & RegisteredProcedure::arg(int  i){
+        integers.push_back(new int(i));
+        if (dbrpcparam(dbproc, NULL, (BYTE)0, SYBINT4, -1, -1, (BYTE *)integers.back())== FAIL)
+            throw SqlException("Unable to add parameter to registered procedure call");
+        return *this;
+    }
+
+    RegisteredProcedure & RegisteredProcedure::arg(const char * str){
+        std::string * strp = new std::string(str);
+        strings.push_back(strp);
+        if (dbrpcparam(dbproc, NULL, (BYTE)0, SYBCHAR, -1, strlen(str), (BYTE *)(strp->c_str()))== FAIL)
+            throw SqlException("Unable to add parameter to registered procedure call");
+        return *this;
+    }
+
+
     MSSql::MSSql(const std::string & host, const std::string & name, const std::string & passwd, const std::string & db){
-        //    dbmsghandle(msg_handler);
+        dberrhandle(err_handler);
+        dbmsghandle(msg_handler);
 
         if (dbinit() == FAIL)
             throw SqlException("dbinit() failed");
+
 
         if ((login = dblogin()) == NULL) 
             throw SqlException("Unable to allocate login structure");
 
         DBSETLUSER(login, name.c_str());
         DBSETLPWD(login, passwd.c_str());
+        //login->tds_login->major_version = 8;
+        //login->tds_login->minor_version = 0;
 
         if ((dbproc = dbopen(login, host.c_str())) == NULL) 
             throw SqlException("Unable to connect to server");
@@ -23,20 +107,21 @@ namespace Sql {
     }
 
     Result MSSql::exec(const std::string & query){
+        if(dbcancel(dbproc) == FAIL)
+            std::cerr << "Unable to cancel" << std::endl;
         if (dbfcmd(dbproc, query.c_str()) == FAIL) 
             throw SqlException("Unable to build request");
         if (dbsqlexec(dbproc) == FAIL)
             throw SqlException("Unable to execute request");
 
-        Result res(dbproc);
-        return res;
+        return Result(dbproc);
     }
-    
-    Result::Result(DBPROCESS * dbproc) : dbproc(dbproc){
-        if(dbresults(dbproc) == FAIL)
-            throw SqlException("Unable to get results");
 
-        ncols = dbnumcols(dbproc);
+    RegisteredProcedure MSSql::exec_proc(char * procedure_name){
+        return RegisteredProcedure(dbproc, procedure_name);
+    }
+
+    void Result::init() {
         if ((columns = (COL*)calloc(ncols, sizeof(struct COL))) == NULL) 
             throw SqlException("Unable to allocate memory for memory data");
 
@@ -51,8 +136,6 @@ namespace Sql {
                 pcol->size = dbwillconvert(pcol->type, SYBCHAR);
             }
 
-            std::cout << pcol->size << " " << pcol->name << std::endl;
-
             if ((pcol->buffer = (char*)calloc(1, pcol->size + 1)) == NULL)
                 throw SqlException("Unable to allocate memory for column data");
             if(dbbind(dbproc, c, NTBSTRINGBIND,	pcol->size+1, (BYTE*)pcol->buffer) == FAIL)
@@ -60,14 +143,59 @@ namespace Sql {
             if(dbnullbind(dbproc, c, &pcol->status) == FAIL)	
                 throw SqlException("dnullbind failed");
         }
-
     }
 
-    Result::~Result(){
+    Result::Result(DBPROCESS * dbproc) : dbproc(dbproc){
+        if(dbresults(dbproc) == FAIL)
+            throw SqlException("Unable to get results");
+        ncols = dbnumcols(dbproc);
+
+        init();
+    }
+
+    
+    Result & Result::operator=(const Result & result) {
+        if(&result != this){
+            cleanup();
+            ncols = result.ncols;
+            dbproc = result.dbproc;
+
+            init();
+        }
+        return *this;
+    }
+
+    void Result::init(const RegisteredProcedure & proc){
+        dbproc = proc.dbproc;
+        dbcancel(dbproc);
+        if(dbrpcsend(dbproc) == FAIL)
+            throw SqlException("Unable to send registered procedure");
+        if(dbresults(dbproc) == FAIL)
+            throw SqlException("Unable to get results from registered procedure");
+        ncols = dbnumcols(dbproc);
+        init();
+    }
+
+
+    Result::Result(const RegisteredProcedure & proc){
+        init(proc);
+    }
+
+    Result & Result::operator=(const RegisteredProcedure & proc){
+        cleanup();
+        init(proc);
+        return *this;
+    }
+
+    void Result::cleanup() {
         for (pcol=columns; pcol - columns < ncols; pcol++) {
             free(pcol->buffer);
         }
         free(columns);
+    }
+
+    Result::~Result(){
+        cleanup();
     }
 
     void Result::iterator::get_row() {
@@ -81,8 +209,7 @@ namespace Sql {
                     throw SqlException("Unable to get next row");
                     break;
                 case NO_MORE_ROWS:
-                    result == NULL;
-                    std::cout << "result est null" << std::endl;
+                    result = NULL;
                     break;
                 case REG_ROW:
                     data.reserve(result->ncols);
@@ -97,6 +224,8 @@ namespace Sql {
                 case BUF_FULL:
                     throw SqlException("Buffer full, can't get row");
                     break;
+                default:
+                    std::cout << "WTF ??" << std::endl;
             }
             read_rows++;
         }
--- source/SqlServer/mssql.h
+++ source/SqlServer/mssql.h
@@ -5,6 +5,7 @@
 #include <sqlfront.h>	/* sqlfront.h always comes first */
 #include <sybdb.h>	/* sybdb.h is the only other file you need */
 
+#include <stdint.h>
 namespace Sql {
     typedef std::vector<std::string> StringVect;
 
@@ -18,11 +19,40 @@ namespace Sql {
         virtual ~SqlException() throw() {}
     };
 
+    /** Structure intermédiaire pour construire un appel à une procédure stockée
+     *
+     * N'est pas destiné à être utilisé directement
+     */
+    class RegisteredProcedure {
+        DBPROCESS * dbproc;
+        std::vector<int *> integers;
+        std::vector<std::string*> strings;
+        private:
+        /// Le constructeur est privé pour éviter un utilisateur trop malin/idiot veuille l'utiliser
+        /// La conséquence serait imprévisible (problème de pointeurs invalides dans la nature)
+        /// (mais au moins y'a pas de fuite de mémoire)
+        RegisteredProcedure(DBPROCESS * dbproc, char * procedure_name);
+        RegisteredProcedure(const RegisteredProcedure &){};
+        /// L'opérateur est desactivé tout comme les constructeurs;
+        void operator=(const RegisteredProcedure &){};
+        public:
+        ~RegisteredProcedure();
+        /// On rajoute un argument de type entier
+        RegisteredProcedure & arg(int i);
+        /// On rajoute un argument de type chaine de caractères
+        RegisteredProcedure & arg(const char * str);
+
+        /// Seul MSSql et Result le droit d'appeler le constructeur et accéder aux membres
+        friend class MSSql;
+        friend class Result;
+
+
+    };
+
     /** Contient les réponses d'une requête */
     class Result {
         public:
         /** Permet d'itérer sur les résultats */
-
         class iterator{
             public:
             typedef StringVect value_type;
@@ -30,7 +60,7 @@ namespace Sql {
             typedef StringVect& reference;
             typedef std::forward_iterator_tag iterator_category;
             typedef int difference_type;
-//            private:
+            private:
             /// Pour savoir sur quoi pointe l'itérateur
             Result * result;
             /// Données courantes
@@ -44,22 +74,20 @@ namespace Sql {
             /// Par défaut, c'est un itérateur pour lequel il n'y a pas d'autre résultat (aka == .end())
             iterator() : result(NULL), read_rows(0){}
             iterator(Result * result) : result(result), read_rows(0) {get_row();};
-            iterator(const iterator & it) : result(it.result), read_rows(it.read_rows) {};
             /// On va chercher l'élément suivant
-//            iterator & operator++(){get_row(); return *this;};
-            void operator++(){get_row();};
+            iterator & operator++(){get_row(); return *this;};
             /// On teste uniquement les deux itérateurs pointes sur NULL => .end()
-            bool operator==(const iterator & it) {std::cout << (result == NULL && it.result == NULL) << std::endl;return result == NULL && it.result == NULL;};
-            bool operator!=(const iterator & it) {std::cout << "Opé != " << std::endl;return result != NULL || it.result != NULL;};
+            bool operator==(const iterator & it) {return result == NULL && it.result == NULL;};
+            bool operator!=(const iterator & it) {return result != NULL || it.result != NULL;};
             /// On obtient les résultats sous forme de tableau de string
             value_type & operator*() {return data;};
-            ~iterator(){std::cout << "Ds le destructeur" << std::endl;};
+            /// Pour simmuler un pointeur
+            value_type * operator->(){return &data;};
         };
 
         typedef iterator const_iterator;
         /** Structure utilisée pour récupérer les données au niveau de la lib C */
-        struct COL 						
-        { 
+        struct COL {
             char *name;
             char *buffer; 
             int type, size, status; 
@@ -71,14 +99,28 @@ namespace Sql {
         /// Connecteur de l'API C vers la base
         DBPROCESS * dbproc;
 
+        /// Initialise la structure : aloue la mémoire pour les structures
+        void init();
+        /// Initialise à partir d'une procédure stockée
+        void init(const RegisteredProcedure & proc);
+
+        private:
+        /// Desaloue les structuers
+        void cleanup();
         public:
         /// Il faut passer le DBPROCESS à lire pour construire une réponse
         Result(DBPROCESS * dbproc);
+        /// Constructeur à partir d'un RegisteredProcedure
+        Result(const RegisteredProcedure & proc);
+        /// Constructeur par copie
+        Result & operator=(const Result & result);
+        /// Constructeur à partir d'un RegisteredProcedure
+        Result & operator=(const RegisteredProcedure & proc);
         /// Nombre de colonnes retournées par la requete
         int num_cols() const {return ncols;};
         /// Nom des colonnes
         StringVect col_labels() const;
-        /// Itérateur sur le début des réponses. Attention ! Ne peut être appelé qu'une seule fois !
+        /// Itérateur sur le début des réponses. Attention ! Ne peut être appelé qu'une seule fois !
         iterator begin(){return iterator(this);};
         /// Itérateur indiquant qu'il n'y a plus de réponses
         iterator end(){return iterator();};
@@ -86,18 +128,38 @@ namespace Sql {
         ~Result();
     };
 
-    /** Connecteur vers une base Microsoft SQL Server */
+    /** Connecteur vers une base Microsoft SQL Server 
+     *
+     * Très important : modifier le fichier /etc/freetds/freetds.conf pour avoir :
+     * [global]
+     * tds version = 8.0
+     *
+     * Merci de regarder l'API de sybase et venez nous remercier après...
+     * Tutoriel pour les curieux http://www.freetds.org/userguide/samplecode.htm
+     * */
     class MSSql {
         LOGINREC *login;
         DBPROCESS *dbproc;
-        private:
-        /// Retourne 
-        StringVect get_headers();
         public:
-        /// Se connecte au serveur et à la base donnée
+        /** Se connecte au serveur et à la base donnée
+         * De préférence utiliser le même connecteur tout le long de l'application et non pas une fois par requête
+         * 60 octets de fuite de mémoire lié à la lib pour chaque instance.
+         *
+         * Ce connecteur n'est pas thread-safe. Créer un connecteur par thread
+         */
         MSSql(const std::string & host, const std::string & name, const std::string & passwd, const std::string & db);
         ~MSSql();
         /// Exécute une requête, attention à échapper les paramètres !
         Result exec(const std::string & query);
+        /** Execute une procédure stockée.
+         *
+         * Affecter les resultat dans une structure Result
+         * Rajouter des paramètres avec la fonction arg
+         * Ne pas essayer de garder RegisteredProcedure (on vous aura prévenu)
+         *
+         * Ex: Result res = conn.exec_proc("ma_procedure_stockee").arg(42).arg("foo");
+         */
+        RegisteredProcedure exec_proc(char * procedure_name);
     };
+
 };
