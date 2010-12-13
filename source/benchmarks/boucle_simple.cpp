d58a605548dd486898f78f6353507bd7f734aeea
TGR <TGR@a734f7ea-df10-47d3-a172-9f42111c9061>
2010-12-13 14:38:52 +0000

    Rajout de benchmarks
    Permet de comparer :
    windows/linux
    mingw/visual studio
    isapi/fastcgi
    
    git-svn-id: https://ctp-gesconf.canaltp.fr/svn/NAViTiACpp/trunk@26 a734f7ea-df10-47d3-a172-9f42111c9061

---
+++ source/benchmarks/boucle_simple.cpp
@@ -0,0 +1,48 @@
+﻿// dllmain.cpp : Définit le point d'entrée pour l'application DLL.
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+#define LENGTH 17000000
+#define BUFFER_LENGTH 300000
+#define RESULT_LENGTH 100000
+void treatment(char*, long unsigned int);
+static int data[LENGTH];
+
+
+
+void treatment(char* p_result, long  unsigned int result_length)
+{
+	int *buffer = (int*) calloc(BUFFER_LENGTH, sizeof(int));
+    char *current = NULL;
+    long unsigned int index;
+    long unsigned int counter = 0;
+	int value = (rand() % 2) + 1 ;
+    for(index=0; index<LENGTH; index++){
+        if(data[index] % value == 0){
+			buffer[counter % BUFFER_LENGTH] = data[index];
+            counter++;
+        }
+    }
+    current = p_result;
+    for(index=0; (index<counter && index<result_length); index++){
+        *current = 32 + (buffer[index]%223);
+        current++;
+        //la plage de caractére imprimable ascii s'étend entre 32 et 255
+	}
+	free(buffer);
+}
+
+int main(int, char**){
+  std::cout <<"Démarage" << std::endl;
+  char response[RESULT_LENGTH];
+  std::cout <<"C'est parti" << std::endl;
+  for(int i = 0; i< LENGTH; i++){
+    data[i] = i;
+  }
+  
+  std::cout <<"Début du traitemment" << std::endl;
+  for(int i=0; i<1000;i++){
+    treatment(response, RESULT_LENGTH);
+    if(i%100 == 0) std::cout<<  "Itération"  <<i<< std::endl;
+  }
+}
---
+++ source/benchmarks/boucle_simple_ws.cpp
@@ -0,0 +1,55 @@
+#include "threadpool.h"
+#include "data_structures.h"
+
+#define LENGTH 17000000
+#define BUFFER_LENGTH 300000
+#define RESULT_LENGTH 100000
+
+using namespace webservice;
+struct Data {
+  int nb_threads;
+  int * data;//[LENGTH];
+  Data() : nb_threads(8) {
+      data = (int*) calloc(LENGTH, sizeof(int));
+    for(int i = 0; i< LENGTH; i++){
+      data[i] = i;
+    }
+  }
+};
+
+struct Worker {
+  int *buffer;
+  char *p_result;
+  Worker() {
+    buffer = (int*) calloc(BUFFER_LENGTH, sizeof(int));
+    p_result = (char*) calloc(RESULT_LENGTH, sizeof(char));
+  }
+  
+  ResponseData operator()(const RequestData & data, Data & d){
+    char *current = NULL;
+    long unsigned int index;
+    long unsigned int counter = 0;
+    int value = (rand() % 2) + 1 ;
+    for(index=0; index<LENGTH; index++){
+        if(d.data[index] % value == 0){
+          buffer[counter % BUFFER_LENGTH] = d.data[index];
+            counter++;
+        }
+    }
+    current = p_result;
+    for(index=0; (index<counter && index<RESULT_LENGTH); index++){
+        *current = 32 + (buffer[index]%223);
+        current++;
+        //la plage de caractére imprimable ascii s'étend entre 32 et 255
+    }
+
+    ResponseData rd;
+    p_result[RESULT_LENGTH -1] = '\0';
+    rd.response = p_result + (RESULT_LENGTH*3/4); // On retourne juste le quart pour ne pas être limité par le réseau
+    rd.content_type = "text/html";
+    rd.status_code = 200;
+    return rd;
+  }
+};
+
+MAKE_WEBSERVICE(Data, Worker)
