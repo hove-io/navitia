f01bf09225fbee3be11fb005ef29b9c610faf206
ajn <ajn@a734f7ea-df10-47d3-a172-9f42111c9061>
2011-01-04 07:05:54 +0000

    ajout de la classe contenant les structures de données
    git-svn-id: https://ctp-gesconf.canaltp.fr/svn/NAViTiACpp/trunk@44 a734f7ea-df10-47d3-a172-9f42111c9061

---
+++ source/type/data.h
@@ -0,0 +1,27 @@
+#pragma once
+#include "type.h"
+
+class Data{
+
+    public:
+    Container<ValidityPattern> validity_patterns;
+
+    Container<Line> lines;
+
+    Container<Route> routes;
+
+    Container<VehicleJourney> vehicle_journeys;
+
+    Container<StopPoint> stop_points;
+
+    Container<StopArea> stop_areas;
+
+    std::vector<StopTime> stop_times;
+
+    public:
+    template<class Archive> void serialize(Archive & ar, const unsigned int ) {
+        ar & validity_patterns & lines & routes & vehicle_journeys & stop_points & stop_areas & stop_times;
+    }
+};
+
+
