9ccbf5286f3d4e4b811a0a818fa621c3189d7d3f
ajn <ajn@a734f7ea-df10-47d3-a172-9f42111c9061>
2011-01-17 15:11:43 +0000

    Suppression des exemples à base d'employés
    git-svn-id: https://ctp-gesconf.canaltp.fr/svn/NAViTiACpp/trunk@62 a734f7ea-df10-47d3-a172-9f42111c9061

--- source/type/examples.cpp
+++ source/type/examples.cpp
@@ -31,40 +31,13 @@ struct LineStops {
     LineStops(int stop_id, const std::string & line_name, int order): stop_id(stop_id), line_name(line_name), order(order) {}
 };
 
-struct Job {
-    int job_id;
-    std::string label;
-    float salary;
-    Job(int job_id, const std::string & label, float salary) : job_id(job_id), label(label), salary(salary) {}
-};
-
-struct Employee {
-    std::string name;
-    int age;
-    bool is_manager;
-    int job_idx;
-    Employee(const std::string & name, int age, bool is_manager, int job_idx) :
-            name(name), age(age), is_manager(is_manager), job_idx(job_idx) {}
-};
-
 using namespace boost::assign;
 
 int main(int, char**) {
-    std::vector<Job> jobs;
-    std::vector<Employee> employees;
     std::vector<Stop> stops;
     std::vector<Line> lines;
     std::vector<City> cities;
     std::vector<LineStops> line_stops;
-
-    jobs += Job(0, "Developper", 42),
-            Job(1, "Trainee", 3.14),
-            Job(2, "Manager", 66);
-
-    employees += Employee("Alice", 32, false, 2),
-                 Employee("Bob", 44, true, 1),
-                 Employee("Charlie", 12, false, 0),
-                 Employee("Deb", 51, false, 0);
 
     stops += Stop(0, "Gare de l'Est", "Paris"),
              Stop(1, "Gare d'Austeriltz", "Paris"),
@@ -93,33 +66,6 @@ int main(int, char**) {
                   LineStops(0, "Metro 5", 1),
                   LineStops(1, "Metro 5", 2);
 
-
-
-
-    std::cout << "Salaries per job" << std::endl;
-    BOOST_FOREACH(Job j, jobs){
-        std::cout << j.label << ": " << j.salary << "€" << std::endl;
-    }
-    std::cout << "-------------------\n" << std::endl;
-
-    std::cout << "Salaries per job sorted by jobname" << std::endl;
-    BOOST_FOREACH(Job j, order(jobs, &Job::label)){
-        std::cout << j.label << ": " << j.salary << "€" << std::endl;
-    }
-    std::cout << "-------------------\n" << std::endl;
-
-    std::cout << "List of managers" << std::endl;
-    BOOST_FOREACH(Employee e, filter(employees, &Employee::is_manager, true)){
-        std::cout << e.name << std::endl;
-    }
-    std::cout << "-------------------\n" << std::endl;
-
-    std::cout << "List of employees and job" << std::endl;
-    auto employees_job = make_join(employees, jobs, attribute_equals(&Employee::job_idx, &Job::job_id));
-    BOOST_FOREACH(auto ej, employees_job){
-        std::cout << join_get<0>(ej).name << ": " << join_get<1>(ej).label << std::endl;
-    }
-    std::cout << "-------------------\n" << std::endl;
 
     std::cout << "=== Cities ordered by name ===" << std::endl;
     BOOST_FOREACH(auto city, order(cities, &City::name)){
