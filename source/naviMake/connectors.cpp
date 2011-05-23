1e212badaf36eecf0f10d692f08cd87cbf75f9a8
ajn <ajn@a734f7ea-df10-47d3-a172-9f42111c9061>
2011-05-23 13:14:39 +0000

    remplacement des [] par at()
    git-svn-id: https://ctp-gesconf.canaltp.fr/svn/NAViTiACpp/trunk@168 a734f7ea-df10-47d3-a172-9f42111c9061

--- source/naviMake/connectors.cpp
+++ source/naviMake/connectors.cpp
@@ -33,16 +33,16 @@ void CsvFusio::fill_networks(navimake::Data& data){
         }
         if(counter != 0){
             navimake::types::Network* network = new navimake::types::Network();
-            network->id = boost::lexical_cast<int>(row[0]);
-            network->name = row[2];
-            network->external_code = row[3];
-            network->address_name = row[5];
-            network->address_number = row[6];
-            network->address_type_name = row[7];
-            network->phone_number = row[7];
-            network->mail = row[8];
-            network->website = row[9];
-            network->fax = row[1];
+            network->id = boost::lexical_cast<int>(row.at(0));
+            network->name = row.at(2);
+            network->external_code = row.at(3);
+            network->address_name = row.at(5);
+            network->address_number = row.at(6);
+            network->address_type_name = row.at(7);
+            network->phone_number = row.at(7);
+            network->mail = row.at(8);
+            network->website = row.at(9);
+            network->fax = row.at(1);
             data.networks.push_back(network);
             network_map[network->id] = network; 
         }
@@ -63,9 +63,9 @@ void CsvFusio::fill_modes_type(navimake::Data& data){
         }
         if(counter != 0){
             navimake::types::ModeType* mode_type = new navimake::types::ModeType();
-            mode_type->id = boost::lexical_cast<int>(row[0]);
-            mode_type->name = row[1];
-            mode_type->external_code = row[2];
+            mode_type->id = boost::lexical_cast<int>(row.at(0));
+            mode_type->name = row.at(1);
+            mode_type->external_code = row.at(2);
             data.mode_types.push_back(mode_type);
             mode_type_map[mode_type->id] = mode_type; 
         }
@@ -85,11 +85,11 @@ void CsvFusio::fill_modes(navimake::Data& data){
         }
         if(counter != 0){
             navimake::types::Mode* mode = new navimake::types::Mode();
-            mode->id = boost::lexical_cast<int>(row[0]);
-            mode->name = row[2];
-            mode->external_code = row[3];
+            mode->id = boost::lexical_cast<int>(row.at(0));
+            mode->name = row.at(2);
+            mode->external_code = row.at(3);
 
-            int mode_type_id = boost::lexical_cast<int>(row[1]);
+            int mode_type_id = boost::lexical_cast<int>(row.at(1));
             mode->mode_type = this->find(mode_type_map, mode_type_id);
 
             data.modes.push_back(mode);
@@ -112,15 +112,15 @@ void CsvFusio::fill_lines(navimake::Data& data){
         }
         if(counter != 0){
             navimake::types::Line* line = new navimake::types::Line();
-            line->id = boost::lexical_cast<int>(row[0]);
-            line->name = row[4];
-            line->external_code = row[3];
-            line->code = row[2];
+            line->id = boost::lexical_cast<int>(row.at(0));
+            line->name = row.at(4);
+            line->external_code = row.at(3);
+            line->code = row.at(2);
 
-            int network_id = boost::lexical_cast<int>(row[5]);
+            int network_id = boost::lexical_cast<int>(row.at(5));
             line->network = this->find(network_map, network_id);
 
-            int mode_type_id = boost::lexical_cast<int>(row[1]);
+            int mode_type_id = boost::lexical_cast<int>(row.at(1));
             line->mode_type = this->find(mode_type_map, mode_type_id);
 
             data.lines.push_back(line);
@@ -143,13 +143,13 @@ void CsvFusio::fill_cities(navimake::Data& data){
         if(counter != 0){
             navimake::types::City* city = new navimake::types::City();
             city->id = counter;//boost::lexical_cast<int>(row[0]);
-            city->external_code = row[1];
-            city->name = row[2];
+            city->external_code = row.at(1);
+            city->name = row.at(2);
 
-            city->coord.x = boost::lexical_cast<double>(row[3]);
-            city->coord.y = boost::lexical_cast<double>(row[4]);
+            city->coord.x = boost::lexical_cast<double>(row.at(3));
+            city->coord.y = boost::lexical_cast<double>(row.at(4));
 
-            city->main_postal_code = row[5];
+            city->main_postal_code = row.at(5);
 
             data.cities.push_back(city);
             city_map[city->external_code] = city;
@@ -171,18 +171,18 @@ void CsvFusio::fill_stop_areas(navimake::Data& data){
         }
         if(counter != 0){
             navimake::types::StopArea* stop_area = new navimake::types::StopArea();
-            stop_area->id = boost::lexical_cast<int>(row[0]);
-            stop_area->external_code = row[2];
-            stop_area->name = row[1];
+            stop_area->id = boost::lexical_cast<int>(row.at(0));
+            stop_area->external_code = row.at(2);
+            stop_area->name = row.at(1);
 
-            if(row[4] == "True") stop_area->main_stop_area = true;
-            if(row[5] == "True") stop_area->main_connection = true;
+            if(row.at(4) == "True") stop_area->main_stop_area = true;
+            if(row.at(5) == "True") stop_area->main_connection = true;
 
-            stop_area->coord.x = boost::lexical_cast<double>(row[6]);
-            stop_area->coord.y = boost::lexical_cast<double>(row[7]);
+            stop_area->coord.x = boost::lexical_cast<double>(row.at(6));
+            stop_area->coord.y = boost::lexical_cast<double>(row.at(7));
 
 
-            std::string city_external_code = row[3];
+            std::string city_external_code = row.at(3);
             navimake::types::City* city = this->find(city_map, city_external_code);
             if(city != NULL){
                 city->stop_area_list.push_back(stop_area);
@@ -208,25 +208,25 @@ void CsvFusio::fill_stop_points(navimake::Data& data){
         }
         if(counter != 0){
             navimake::types::StopPoint* stop_point = new navimake::types::StopPoint();
-            stop_point->id = boost::lexical_cast<int>(row[0]);
-            stop_point->external_code = row[2];
-            stop_point->name = row[3];
+            stop_point->id = boost::lexical_cast<int>(row.at(0));
+            stop_point->external_code = row.at(2);
+            stop_point->name = row.at(3);
 
-            stop_point->address_name = row[6];
-            stop_point->address_number = row[7];
-            stop_point->address_type_name = row[8];
+            stop_point->address_name = row.at(6);
+            stop_point->address_number = row.at(7);
+            stop_point->address_type_name = row.at(8);
 
-            stop_point->coord.x = boost::lexical_cast<double>(row[12]);
-            stop_point->coord.y = boost::lexical_cast<double>(row[13]);
-            stop_point->fare_zone = boost::lexical_cast<int>(row[14]);
+            stop_point->coord.x = boost::lexical_cast<double>(row.at(12));
+            stop_point->coord.y = boost::lexical_cast<double>(row.at(13));
+            stop_point->fare_zone = boost::lexical_cast<int>(row.at(14));
 
-            int stop_area_id = boost::lexical_cast<int>(row[4]);
+            int stop_area_id = boost::lexical_cast<int>(row.at(4));
             stop_point->stop_area = this->find(stop_area_map, stop_area_id);
 
-            std::string city_external_code = row[5];
+            std::string city_external_code = row.at(5);
             stop_point->city = this->find(city_map, city_external_code);
 
-            int mode_id = boost::lexical_cast<int>(row[11]);
+            int mode_id = boost::lexical_cast<int>(row.at(11));
             stop_point->mode = this->find(mode_map, mode_id);
 
             data.stop_points.push_back(stop_point);
@@ -249,17 +249,17 @@ void CsvFusio::fill_routes(navimake::Data& data){
         }
         if(counter != 0){
             navimake::types::Route* route = new navimake::types::Route();
-            route->id = boost::lexical_cast<int>(row[0]);
-            route->external_code = row[3];
-            route->name = row[2];
+            route->id = boost::lexical_cast<int>(row.at(0));
+            route->external_code = row.at(3);
+            route->name = row.at(2);
 
-            if(row[1] == "True") route->is_frequence = true;
-            if(row[5] == "True") route->is_forward = true;
+            if(row.at(1) == "True") route->is_frequence = true;
+            if(row.at(5) == "True") route->is_forward = true;
 
-            int line_id = boost::lexical_cast<int>(row[4]);
+            int line_id = boost::lexical_cast<int>(row.at(4));
             route->line = this->find(line_map, line_id);
 
-            int mode_id = boost::lexical_cast<int>(row[8]);
+            int mode_id = boost::lexical_cast<int>(row.at(8));
             route->mode = this->find(mode_map, mode_id);
 
             data.routes.push_back(route);
@@ -281,16 +281,16 @@ void CsvFusio::fill_vehicle_journeys(navimake::Data& data){
         }
         if(counter != 0){
             navimake::types::VehicleJourney* vehicle_journey = new navimake::types::VehicleJourney();
-            vehicle_journey->id = boost::lexical_cast<int>(row[0]);
-            vehicle_journey->external_code = row[8];
-            vehicle_journey->name = row[7];
+            vehicle_journey->id = boost::lexical_cast<int>(row.at(0));
+            vehicle_journey->external_code = row.at(8);
+            vehicle_journey->name = row.at(7);
 
-            if(row[12] == "True") vehicle_journey->is_adapted = true;
+            if(row.at(12) == "True") vehicle_journey->is_adapted = true;
 
-            int route_id = boost::lexical_cast<int>(row[2]);
+            int route_id = boost::lexical_cast<int>(row.at(2));
             vehicle_journey->route = this->find(route_map, route_id);
 
-            int mode_id = boost::lexical_cast<int>(row[10]);
+            int mode_id = boost::lexical_cast<int>(row.at(10));
             vehicle_journey->mode = this->find(mode_map, mode_id);
 
             data.vehicle_journeys.push_back(vehicle_journey);
@@ -312,19 +312,19 @@ void CsvFusio::fill_stops(navimake::Data& data){
         }
         if(counter != 0){
             navimake::types::StopTime* stop = new navimake::types::StopTime();
-            stop->id = boost::lexical_cast<int>(row[0]);
-            stop->zone = boost::lexical_cast<int>(row[1]);
+            stop->id = boost::lexical_cast<int>(row.at(0));
+            stop->zone = boost::lexical_cast<int>(row.at(1));
 
-            stop->arrival_time = boost::lexical_cast<int>(row[2]);
-            stop->departure_time = boost::lexical_cast<int>(row[3]);
+            stop->arrival_time = boost::lexical_cast<int>(row.at(2));
+            stop->departure_time = boost::lexical_cast<int>(row.at(3));
 
-            stop->order = boost::lexical_cast<int>(8);
+            stop->order = boost::lexical_cast<int>(row.at(8));
 
 
-            int vehicle_journey_id = boost::lexical_cast<int>(row[4]);
+            int vehicle_journey_id = boost::lexical_cast<int>(row.at(4));
             stop->vehicle_journey = this->find(vehicle_journey_map, vehicle_journey_id);
 
-            int stop_point_id = boost::lexical_cast<int>(row[7]);
+            int stop_point_id = boost::lexical_cast<int>(row.at(7));
             stop->stop_point = this->find(stop_point_map, stop_point_id);
 
             data.stops.push_back(stop);
@@ -344,13 +344,13 @@ void CsvFusio::fill_connections(navimake::Data& data){
         }
         if(counter != 0){
             navimake::types::Connection* connection = new navimake::types::Connection();
-            connection->id = boost::lexical_cast<int>(row[0]);
+            connection->id = boost::lexical_cast<int>(row.at(0));
 
-            int departure = boost::lexical_cast<int>(row[2]);
+            int departure = boost::lexical_cast<int>(row.at(2));
             connection->departure_stop_point = this->find(stop_point_map, departure);
 
 
-            int destination = boost::lexical_cast<int>(row[3]);
+            int destination = boost::lexical_cast<int>(row.at(3));
             connection->destination_stop_point = this->find(stop_point_map, destination);
 
             data.connections.push_back(connection);
