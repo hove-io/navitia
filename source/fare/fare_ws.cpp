6992cf0ac56ff313b6a5527135c3abc476902782
ajn <ajn@a734f7ea-df10-47d3-a172-9f42111c9061>
2011-07-04 15:01:07 +0000

    ajout de log en cas d'erreurs
    git-svn-id: https://ctp-gesconf.canaltp.fr/svn/NAViTiACpp/trunk@220 a734f7ea-df10-47d3-a172-9f42111c9061

--- source/fare/fare_ws.cpp
+++ source/fare/fare_ws.cpp
@@ -5,24 +5,35 @@
 #include "configuration.h"
 #include "fare.h"
 #include <iostream>
+#include <fstream>
+#include <boost/date_time/posix_time/posix_time.hpp>
 using namespace webservice;
 
 /** Structure de données globale au webservice
  *  N'est instancié qu'une seule fois au chargement
  */
 struct Data{
-  int nb_threads; /// Nombre de threads. IMPORTANT ! Sans cette variable, ça ne compile pas
-  boost::mutex mut; /// Un mutex pour protéger ce cout
-  Fare fares; /// Contient tout le graphe capable de calculer les tarifs
-  /// Constructeur par défaut, il est appelé au chargement du webservice
-  Data() : nb_threads(8){
-	 Configuration * conf = Configuration::get();
-	conf->load_ini(conf->get_string("path") + "fare.ini");
-	fares.init(conf->ini["files"]["grammar"], conf->ini["files"]["prices"]);
-	if(conf->ini["files"]["stif"] != "")
-		fares.load_od_stif(conf->ini["files"]["stif"]);
-    std::cout << "chargement terminé avec " << boost::num_vertices(fares.g) << " nœuds et " << boost::num_edges(fares.g) << " arcs" << std::endl;
-  }
+    int nb_threads; /// Nombre de threads. IMPORTANT ! Sans cette variable, ça ne compile pas
+    boost::mutex mut; /// Un mutex pour protéger ce cout
+    Fare fares; /// Contient tout le graphe capable de calculer les tarifs
+    /// Constructeur par défaut, il est appelé au chargement du webservice
+    Data() : nb_threads(8){
+    Configuration * conf = Configuration::get();
+        try{
+            conf->load_ini(conf->get_string("path") + "fare.ini");
+            fares.init(conf->ini["files"]["grammar"], conf->ini["files"]["prices"]);
+            if(conf->ini["files"]["stif"] != "")
+                fares.load_od_stif(conf->ini["files"]["stif"]);
+        }catch(std::exception e){
+            boost::posix_time::ptime date(boost::posix_time::microsec_clock::local_time());
+            std::ofstream f(conf->get_string("path") + "error.log", std::ios_base::app);
+            f << boost::posix_time::to_simple_string(date) << " - " << e.what() << " - " << __FILE__ << ":" << __LINE__ << std::endl;
+        }catch(...){
+            boost::posix_time::ptime date(boost::posix_time::microsec_clock::local_time());
+            std::ofstream f(conf->get_string("path") + "error.log", std::ios_base::app);
+            f << boost::posix_time::to_simple_string(date) << " - error - " << __FILE__ << ":" <<__LINE__ << std::endl;
+        }
+    }
 };
 
 /// Classe associée à chaque thread
@@ -52,9 +63,9 @@ class Worker : public BaseWorker<Data> {
 
     public:
     /** Constructeur par défaut
-      *
-      * On y enregistre toutes les api qu'on souhaite exposer
-      */
+     *
+     * On y enregistre toutes les api qu'on souhaite exposer
+     */
     Worker(Data &) {
         register_api("/journeyfare",boost::bind(&Worker::count, this, _1, _2), "Effectue le calcul de tarif");
         add_default_api();
