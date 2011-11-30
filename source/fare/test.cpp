af6f00a4386b114e30c27cd9c19b63d7ac52463b
TGR <TGR@a734f7ea-df10-47d3-a172-9f42111c9061>
2011-11-30 16:03:15 +0000

    Adaptatino des tests aux dernières fixtures du stif
    git-svn-id: https://ctp-gesconf.canaltp.fr/svn/NAViTiACpp/trunk@367 a734f7ea-df10-47d3-a172-9f42111c9061

--- source/fare/test.cpp
+++ source/fare/test.cpp
@@ -108,13 +108,13 @@ BOOST_AUTO_TEST_CASE(test_computation) {
     keys.push_back("ratp;8711388;8775890;FILGATO-2;2011|07|01;04|40;04|50;4;1;rapidtransit");
     res = f.compute(keys);
     BOOST_CHECK_EQUAL(res.size() , 1);
-    BOOST_CHECK_EQUAL(res.at(0).value,370);
+    BOOST_CHECK_EQUAL(res.at(0).value,395);//code 140
 
     // Le métro doit être gratuit après
     keys.push_back("ratp;paris;FILNav31;FILGATO-2;2011|07|01;04|40;04|50;1;1;metro");
     res = f.compute(keys);
     BOOST_CHECK_EQUAL(res.size() , 1);
-    BOOST_CHECK_EQUAL(res.at(0).value,370);
+    BOOST_CHECK_EQUAL(res.at(0).value,395);
 
     // Et le Tramway bien évidemment payant !
     // Le métro doit être gratuit après
@@ -161,7 +161,7 @@ BOOST_AUTO_TEST_CASE(test_computation) {
     keys.push_back("ratp;8739300;FILGATO-2;8775499;2011|12|01;04|40;04|50;4;1;rapidtransit");
     res = f.compute(keys);
     BOOST_CHECK_EQUAL(res.size() , 1);
-    BOOST_CHECK_EQUAL(res.at(0).value,215);
+    BOOST_CHECK_EQUAL(res.at(0).value,960); // 230 + 120 + 610 [codes 30;100;44]
     BOOST_CHECK_EQUAL(res.at(0).sections.size() , 1);
 
     // Cas avec deux RER
@@ -170,7 +170,7 @@ BOOST_AUTO_TEST_CASE(test_computation) {
     keys.push_back("ratp;8775890;FILGATO-2;8775499;2011|12|01;04|40;04|50;1;5;rapidtransit");
     res = f.compute(keys);
     BOOST_CHECK_EQUAL(res.size() , 1);
-    BOOST_CHECK_EQUAL(res.at(0).value,215);
+    BOOST_CHECK_EQUAL(res.at(0).value,960);
     BOOST_CHECK_EQUAL(res.at(0).sections.size() , 2);
 
     // Cas avec un RER, un changement en métro
@@ -179,7 +179,7 @@ BOOST_AUTO_TEST_CASE(test_computation) {
     keys.push_back("ratp;nation;montparnasse;FILGATO-2;2011|12|01;04|40;04|50;1;1;metro");
     res = f.compute(keys);
     BOOST_CHECK_EQUAL(res.size() , 1);
-    BOOST_CHECK_EQUAL(res.at(0).value,295);
+    BOOST_CHECK_EQUAL(res.at(0).value, 320); // code 130
     BOOST_CHECK_EQUAL(res.at(0).sections.size() , 2);
 
 
@@ -190,7 +190,7 @@ BOOST_AUTO_TEST_CASE(test_computation) {
     keys.push_back("ratp;8775890;FILGATO-2;8775499;2011|12|01;04|40;04|50;1;5;rapidtransit");
     res = f.compute(keys);
     BOOST_CHECK_EQUAL(res.size() , 1);
-    BOOST_CHECK_EQUAL(res.at(0).value,215);
+    BOOST_CHECK_EQUAL(res.at(0).value,960);
     BOOST_CHECK_EQUAL(res.at(0).sections.size() , 3);
 
 
@@ -201,11 +201,11 @@ BOOST_AUTO_TEST_CASE(test_computation) {
     keys.push_back("ratp;8775890;FILGATO-2;8775499;2011|12|01;04|40;04|50;1;5;rapidtransit");
     res = f.compute(keys);
     BOOST_CHECK_EQUAL(res.size() , 3);
-    BOOST_CHECK_EQUAL(res.at(0).value,295);
+    BOOST_CHECK_EQUAL(res.at(0).value,320); // code 140
     BOOST_CHECK_EQUAL(res.at(0).sections.size() , 1);
     BOOST_CHECK_EQUAL(res.at(1).value,170);
     BOOST_CHECK_EQUAL(res.at(1).sections.size() , 1);
-    BOOST_CHECK_EQUAL(res.at(2).value,655);
+    BOOST_CHECK_EQUAL(res.at(2).value,700); // code 144
     BOOST_CHECK_EQUAL(res.at(2).sections.size() , 1);
 
     keys.clear();
@@ -213,7 +213,7 @@ BOOST_AUTO_TEST_CASE(test_computation) {
     keys.push_back("ratp;8775890;FILGATO-2;8775499;2011|12|01;04|40;04|50;1;5;rapidtransit");
     res = f.compute(keys);
     BOOST_CHECK_EQUAL(res.size() , 1);
-    BOOST_CHECK_EQUAL(res.at(0).value,655);
+    BOOST_CHECK_EQUAL(res.at(0).value, 700);
     BOOST_CHECK_EQUAL(res.at(0).sections.size() , 2);
 
     keys.clear();
@@ -222,17 +222,17 @@ BOOST_AUTO_TEST_CASE(test_computation) {
     keys.push_back("ratp;bliabal;FILGATO-2;8775499;2011|12|01;04|40;04|50;3;5;rapidtransit");
     res = f.compute(keys);
     BOOST_CHECK_EQUAL(res.size() , 1);
-    BOOST_CHECK_EQUAL(res.at(0).value,655);
+    BOOST_CHECK_EQUAL(res.at(0).value,700);
     BOOST_CHECK_EQUAL(res.at(0).sections.size() , 3);
 
     keys.clear();
     keys.push_back("5604:127;11:120;050050023:23;8727622;2011|07|31;09|28;09|39;4;4;Bus");
-    keys.push_back(";8727622;800:D;8775860;2011|07|31;09|47;10|09;4;1;RapidTransit");
+    keys.push_back(";8727622;800:D;8775890;2011|07|31;09|47;10|09;4;1;RapidTransit");
     keys.push_back(";8775860;100110007:7;R_0007;2011|07|31;10|20;10|21;1;1;Metro");
     res = f.compute(keys);
     BOOST_CHECK_EQUAL(res.size() , 2);
     BOOST_CHECK_EQUAL(res.at(0).value,170);
-    BOOST_CHECK_EQUAL(res.at(1).value , 370);
+    BOOST_CHECK_EQUAL(res.at(1).value , 395); // code 140
     BOOST_CHECK_EQUAL(res.at(0).sections.size() , 1);
     BOOST_CHECK_EQUAL(res.at(0).sections.at(0).section , keys.at(0));
     BOOST_CHECK_EQUAL(res.at(1).sections.size(), 2);
@@ -252,7 +252,7 @@ BOOST_AUTO_TEST_CASE(test_computation) {
     keys.push_back(";8727141;RER B;8770870;2011|07|31;09|28;09|39;4;4;RapidTransit"); // Aulnay -> CDG
     res = f.compute(keys);
     BOOST_CHECK_EQUAL(res.size(), 1);
-    BOOST_CHECK_EQUAL(res.at(0).value, 280);
+    BOOST_CHECK_EQUAL(res.at(0).value, 545); // code 13 et 84 => 305 + 240
 
     keys.clear();
     keys.push_back(";bled_paumé;bus_magique;8711388;2011|07|31;09|28;09|39;4;4;Bus"); // Bled Paumé -> L'Abbaye
@@ -261,7 +261,7 @@ BOOST_AUTO_TEST_CASE(test_computation) {
     res = f.compute(keys);
     BOOST_CHECK_EQUAL(res.size(), 2);
     BOOST_CHECK_EQUAL(res.at(0).value, 170);
-    BOOST_CHECK_EQUAL(res.at(1).value, 215);
+    BOOST_CHECK_EQUAL(res.at(1).value, 470); // code 12+84 => 230 + 240
 
     // On teste les lignes à tarif exclusif
     keys.clear();
