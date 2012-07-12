83c3300bc88d0a2f1110d8b6452ac00f3cff717a
Vincent Lara <vincent.lara@canaltp.fr>
2012-07-12 18:07:36 +0200

    gateway : Ajout du module planner

--- source/gateway_mod/gateway.cpp
+++ source/gateway_mod/gateway.cpp
@@ -11,14 +11,14 @@
 
 
 Worker::Worker(Pool &){
-    register_api("query", boost::bind(&Worker::handle, this, _1, _2), "traite les requétes");
+    register_api("/", boost::bind(&Worker::handle, this, _1, _2), "traite les requétes");
     register_api("firstletter", boost::bind(&Worker::handle, this, _1, _2), "traite les requètes");
     register_api("streetnetwork", boost::bind(&Worker::handle, this, _1, _2), "traite les requètes");
+    register_api("planner", boost::bind(&Worker::handle, this, _1, _2), "planne les requêtes");
     register_api("load", boost::bind(&Worker::load, this, _1, _2), "traite les requétes");
     register_api("register", boost::bind(&Worker::register_navitia, this, _1, _2), "ajout d'un NAViTiA au pool");
     register_api("status", boost::bind(&Worker::status, this, _1, _2), "status");
     register_api("unregister", boost::bind(&Worker::unregister_navitia, this, _1, _2), "suppression d'un NAViTiA du pool");
-
 }
 
 
