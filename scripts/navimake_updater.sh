4c026d99bfaa8b0d7adb60bb865d49d46c4e0cf6
Alexandre Jacquin <alexandre.jacquin@canaltp.fr>
2012-09-04 11:58:55 +0200

    ajout d'un script pour la mise en place de la binarisation automatique

---
+++ scripts/navimake_updater.sh
@@ -0,0 +1,96 @@
+#!/bin/sh
+
+#si le fichier de lock existe, un traitement est deja en cours, on arréte
+lock_file="$0.lock"
+if [ -f $lock_file ]
+then
+    echo "binarisation deja en cours"
+    exit 0
+fi
+
+config_file="$0-config.sh"
+
+
+if [ ! -r $config_file ]
+then
+    echo "$config_file n'existe  pas ou n'est pas lisible"
+    exit 1
+fi
+
+#equivalent à "source" mais portable sur sh
+. $config_file
+
+if [ ! -d $source_dir ] 
+then
+    echo "source_dir ($source_dir) n'est pas un répertoire"
+    exit 2
+fi
+
+if [ ! -d $destination_dir ]
+then
+    echo "destination_dir ($destination_dir) n'est pas un répertoire"
+    exit 2
+fi
+
+if [ ! -d $error_dir ]
+then
+    echo "error_dir ($error_dir) n'est pas un répertoire"
+    exit 2
+fi
+
+if [ ! -d $data_dir ]
+then
+    echo "data_dir ($data_dir) n'est pas un répertoire"
+    exit 2
+fi
+
+if [ ! -x $navimake ]
+then
+    echo "navimake ($navimake) n'est pas executable"
+    exit 2
+fi
+
+#création du fichier de lock
+touch $lock_file
+
+
+for file in `find $source_dir -name '*.zip'`
+do
+    temp_dir=$destination_dir/`date +%Y%m%d-%H%M%S`
+    mkdir $temp_dir
+
+    echo "binarisation de $file dans $temp_dir "
+
+    unzip $file -d $temp_dir >> "$temp_dir/bina.log" 2>&1
+    if [ $? -ne 0 ]
+    then
+        mv $file $error_dir
+        rm -f $lock_file
+        echo "impossible de dézipper $file"
+        exit 4
+    fi
+
+    if [ -d "$bd_topo" ]
+    then
+        navimake_options=" --topo $bd_topo "
+    else
+        echo "bd_topo ($bd_topo) non trouvé"
+    fi
+    
+
+    `$navimake -t gtfs -i $temp_dir -o $data_dir/$data_filename $navimake_options >> "$temp_dir/bina.log" 2>&1`
+    if [ $? -ne 0 ]
+    then
+        mv $file $error_dir
+        rm -f $lock_file
+        echo "erreurs lors de la binarisation"
+        exit 3
+    fi
+    echo "generation du checksum"
+    echo `md5sum $data_dir/$data_filename | cut -d' ' -f1` > "$data_dir/$data_filename.md5"
+    rm -f $file
+
+
+done
+
+rm -f $lock_file
---
+++ scripts/navimake_updater.sh-config.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+source_dir=/tmp/source_dir
+
+destination_dir=/tmp/destination_dir
+
+error_dir=/tmp/error_dir
+
+navimake=/home/kinou/workspace/navitia/release/naviMake/navimake
+
+data_dir=/tmp/data
+
+data_filename=data.nav.lz4
+
+bd_topo='/home/kinou/workspace/navitia/data/IdF/BD_TOPO_STIF'
