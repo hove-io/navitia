13e3f01dd98b723c5383221479b58885c9f6a64b
Vincent Lara <vincent.lara@canaltp.fr>
2012-11-13 16:30:12 +0100

    ptref : Ajout de test pour les requetes imbriquées

--- source/ptreferential/tests/ptret_test.cpp
+++ source/ptreferential/tests/ptret_test.cpp
@@ -43,6 +43,20 @@ BOOST_AUTO_TEST_CASE(many_filter){
     BOOST_CHECK_EQUAL(filters[1].op, EQ);
 }
 
+BOOST_AUTO_TEST_CASE(having) {
+    std::vector<Filter> filters = parse("stop_areas HAVING (line.external_code=1 and stop_area.external_code=3) and line.external_code>=2");
+    BOOST_REQUIRE_EQUAL(filters.size(), 2);
+    BOOST_CHECK_EQUAL(filters[0].object, "stop_areas");
+    BOOST_CHECK_EQUAL(filters[0].attribute, "");
+    BOOST_CHECK_EQUAL(filters[0].value, "line.external_code=1 and stop_area.external_code=3");
+    BOOST_CHECK_EQUAL(filters[0].op, HAVING);
+
+    BOOST_CHECK_EQUAL(filters[1].object, "line");
+    BOOST_CHECK_EQUAL(filters[1].attribute, "external_code");
+    BOOST_CHECK_EQUAL(filters[1].value, "2");
+    BOOST_CHECK_EQUAL(filters[1].op,  GEQ);
+}
+
 BOOST_AUTO_TEST_CASE(exception){
     BOOST_CHECK_THROW(parse("mouuuhh bliiii"), ptref_parsing_error);
 }
