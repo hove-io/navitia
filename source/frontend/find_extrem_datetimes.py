b46da58b216ce24e14f79eb605a3c73b72b3deec
Vincent Lara <vincent.lara@canaltp.fr>
2013-02-07 14:31:13 +0100

    frontend : Correction de find extrems

--- source/frontend/find_extrem_datetimes.py
+++ source/frontend/find_extrem_datetimes.py
@@ -6,10 +6,10 @@ def extremes(resp, request) :
     if resp.planner != None:
         if resp.planner != None:
             for journey in resp.planner.journey:
-                if journey.departure_date_time < earlier:
-                    earlier = journey.departure_date_time
-                if journey.arrival_date_time > later:
-                    later = journey.arrival_date_time
+                if journey.arrival_date_time< earlier:
+                    earlier = journey.arrival_date_time
+                if journey.departure_date_time > later:
+                    later = journey.departure_date_time
 
             
             tmp = ""
@@ -19,14 +19,13 @@ def extremes(resp, request) :
 
             minute = timedelta(minutes = 1)
             if earlier != "22221101T0101" :
-                d = datetime.strptime(earlier, "%Y%m%dT%H%M%S")
+                d = datetime.strptime(later, "%Y%m%dT%H%M%S")
                 datetime_after = d + minute
                 response["after"] = tmp + "clockwise=true&datetime="+datetime_after.strftime("%Y%m%dT%H%M%S")
 
             if later != "20000101T000000":
-                d = datetime.strptime(later, "%Y%m%dT%H%M%S") 
+                d = datetime.strptime(earlier, "%Y%m%dT%H%M%S") 
                 print minute
-                print "later : " + d.strftime("%Y%m%dT%H%M%S")
                 datetime_before = d - minute
                 response["before"] = tmp +"clockwise=false&datetime="+datetime_before.strftime("%Y%m%dT%H%M%S")
     return response
