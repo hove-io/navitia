f2e75de00b057f7b8e0864d1f1ca5df6d79a0811
Vincent Lara <vincent.lara@canaltp.fr>
2013-02-07 11:33:56 +0100

    frontend : Ajout de find_extrem_datetimes.py

---
+++ source/frontend/find_extrem_datetimes.py
@@ -0,0 +1,32 @@
+from datetime import datetime, timedelta
+def extremes(resp, request) : 
+    response = {"before" : "", "after":""}
+    earlier = "22221101T010159"
+    later = "20000101T000000"
+    if resp.planner != None:
+        if resp.planner != None:
+            for journey in resp.planner.journey:
+                if journey.departure_date_time < earlier:
+                    earlier = journey.departure_date_time
+                if journey.arrival_date_time > later:
+                    later = journey.arrival_date_time
+
+            
+            tmp = ""
+            for key, value in request.iteritems():
+                if key != "datetime" and key != "clockwise":
+                    tmp += key + "=" +str(value) + "&"
+
+            minute = timedelta(minutes = 1)
+            if earlier != "22221101T0101" :
+                print earlier
+                d = datetime.strptime(earlier, "%Y%m%dT%H%M%S")
+                datetime_before = d - minute
+                response["before"] = tmp +"clockwise=true&datetime="+datetime_before.strftime("%Y%m%dT%H%M%S")
+
+            if later != "20000101T000000":
+                d = datetime.strptime(later, "%Y%m%dT%H%M%S")
+                datetime_after = d + minute
+                response["after"] = tmp + "clockwise=false&datetime="+datetime_after.strftime("%Y%m%dT%H%M%S")
+    print response
+    return response
