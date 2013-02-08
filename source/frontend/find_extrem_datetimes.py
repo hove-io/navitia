2aedb32522a152ffdf029c00c6dd6f6981c075d6
Tristram Gräbener <tristram.grabener@canaltp.fr>
2013-02-08 15:45:47 +0100

    Frontend: correction de bug des avant/après
    On les virait lorsqu'il y avait un trajet pure piéton

--- source/frontend/find_extrem_datetimes.py
+++ source/frontend/find_extrem_datetimes.py
@@ -4,19 +4,22 @@ def extremes(resp, request):
     before = None
     after = None
 
-    if resp.planner and len(resp.planner.journey) > 0:
-        asap_journey = min(resp.planner.journey, key=attrgetter('arrival_date_time'))
-        query_args = ""
-        for key, value in request.iteritems():
-            if key != "datetime" and key != "clockwise":
-                query_args += key + "=" +str(value) + "&"
+    try:
+        asap_journey = min([journey for journey in resp.planner.journey if resp.planner.journey.arrival_date_time != ''], key=attrgetter('arrival_date_time'))
+    except:
+        return (None, None)
 
-        if asap_journey.arrival_date_time and asap_journey.departure_date_time:
-            minute = timedelta(minutes = 1)
-            datetime_after = datetime.strptime(asap_journey.departure_date_time, "%Y%m%dT%H%M%S") + minute
-            before = query_args + "clockwise=true&datetime="+datetime_after.strftime("%Y%m%dT%H%M%S")
-    
-            datetime_before = datetime.strptime(asap_journey.arrival_date_time, "%Y%m%dT%H%M%S") - minute
-            after = query_args +"clockwise=false&datetime="+datetime_before.strftime("%Y%m%dT%H%M%S")
+    query_args = ""
+    for key, value in request.iteritems():
+        if key != "datetime" and key != "clockwise":
+            query_args += key + "=" +str(value) + "&"
+
+    if asap_journey.arrival_date_time and asap_journey.departure_date_time:
+        minute = timedelta(minutes = 1)
+        datetime_after = datetime.strptime(asap_journey.departure_date_time, "%Y%m%dT%H%M%S") + minute
+        before = query_args + "clockwise=true&datetime="+datetime_after.strftime("%Y%m%dT%H%M%S")
+
+        datetime_before = datetime.strptime(asap_journey.arrival_date_time, "%Y%m%dT%H%M%S") - minute
+        after = query_args +"clockwise=false&datetime="+datetime_before.strftime("%Y%m%dT%H%M%S")
 
     return (before,after)
