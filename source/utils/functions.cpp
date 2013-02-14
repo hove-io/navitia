17fc248da30b13b67375b61d9da38aadcf6c1134
Tristram Gräbener <tristram.grabener@canaltp.fr>
2013-02-14 12:22:59 +0100

    Utils : suppression d'un std::cout
    dans les fonctions de conversion string vers int/double

--- source/utils/functions.cpp
+++ source/utils/functions.cpp
@@ -11,20 +11,18 @@ double str_to_double(std::string str){
         boost::replace_all(str, ",", ".");
         return boost::lexical_cast<double>(str);
     } catch(...){        
-        std::cout << "Str_To_Double : Impossible de convertir [" << str << "] en double.  " << std::endl;
         return -1;
     }
 }
 
 int str_to_int(std::string str){    
     boost::trim(str);
+    if (str.empty()){
+        return -1;
+    }
     try{
-        if (str.empty()){
-            return -1;
-        }
-        else{ return boost::lexical_cast<int>(str);}
+        return boost::lexical_cast<int>(str);
     } catch(...){
-        std::cout << "str_to_int : Impossible de convertir [" << str << "] en entier.  " << std::endl;
         return -1;
     }
 }
