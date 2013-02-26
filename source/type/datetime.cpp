b6f04cf1ef8261d7aa16df448042d64ea99e9421
Vincent Lara <vincent.lara@canaltp.fr>
2013-02-26 10:04:31 +0100

    Datetime : Ajout des fichiers

---
+++ source/type/datetime.cpp
@@ -0,0 +1,36 @@
+#include "datetime.h"
+#include "type/data.h"
+
+namespace navitia { namespace type {
+
+DateTime DateTime::inf = DateTime::infinity();
+DateTime DateTime::min = DateTime::minimity();
+
+std::ostream & operator<<(std::ostream & os, const DateTime & dt){
+    os << "D=" << dt.date() << " " << dt.hour()/(3600) << ":";
+    if((dt.hour()%3600)/60 < 10)
+        os << "0" << (dt.hour()%3600)/60;
+    else
+        os << (dt.hour()%3600)/60;
+    return os;
+}
+
+
+std::string iso_string(int date, int hour, const Data & d){
+    auto tmp = DateTime(date, hour);
+    return iso_string(tmp, d);
+}
+
+std::string iso_string(DateTime &datetime, const Data &d){
+    return boost::posix_time::to_iso_string(to_posix_time(datetime, d));
+}
+
+
+boost::posix_time::ptime to_posix_time(DateTime &datetime, const Data &d) {
+    boost::posix_time::ptime date_time(d.meta.production_date.begin() + boost::gregorian::days(datetime.date()));
+    date_time += boost::posix_time::seconds(datetime.hour());
+    return date_time;
+}
+
+
+} }
