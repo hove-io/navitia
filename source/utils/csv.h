7e0fd4a5b608b8f1f355bbdaf959bf903e74e361
Vincent Lara <vincent.lara@canaltp.fr>
2013-03-04 18:27:53 +0100

    Csv Reader : Amélioration

--- source/utils/csv.h
+++ source/utils/csv.h
@@ -5,6 +5,7 @@
 #include <string>
 #include <fstream>
 #include <vector>
+#include <unordered_map>
 #include <boost/tokenizer.hpp>
 #ifdef HAVE_ICONV_H
 #include "utils/encoding_converter.h"
@@ -16,21 +17,24 @@
  */
 class CsvReader {
     public:
-        CsvReader(const std::string& filename, char separator=';', std::string encoding="UTF-8");
+        CsvReader(const std::string& filename, char separator=';', bool read_headers = false, std::string encoding="UTF-8");
         ~CsvReader();
         std::vector<std::string> next();
-        int get_pos_col(const std::string&, std::map<std::string, int>&);
+        int get_pos_col(const std::string&);
         bool eof() const;
         void close();
+        bool is_open();
+        bool validate(const std::vector<std::string> &mandatory_headers);
+        std::string missing_headers(const std::vector<std::string> &mandatory_headers);
     private:
 
 
-        typedef boost::tokenizer<boost::escaped_list_separator<char> > Tokenizer;
         std::string filename;
         std::string line;
-        std::ifstream file;
+        std::fstream file;
         bool closed;
         boost::escaped_list_separator<char> functor;
+        std::unordered_map<std::string, int> headers;
 #ifdef HAVE_ICONV_H
         EncodingConverter* converter;
 #endif
