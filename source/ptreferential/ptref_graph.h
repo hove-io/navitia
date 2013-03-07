fc080f99af8497ac2d262c336913bd8e3a5950b3
Tristram Gräbener <tristram.grabener@canaltp.fr>
2013-03-07 16:21:24 +0100

    PTRef Graph : suppression du type node inutilisé
    Le graph porte directement un Type_e comme type de nœud

--- source/ptreferential/ptref_graph.h
+++ source/ptreferential/ptref_graph.h
@@ -12,11 +12,6 @@ struct Edge {
     Edge() : weight(1){}
 };
 
-struct Node {
-    Type_e type;
-    Node(Type_e type) : type(type) {}
-};
-
 typedef boost::adjacency_list<boost::listS, boost::vecS, boost::directedS, Type_e, Edge > Graph;
 typedef boost::graph_traits<Graph>::vertex_descriptor vertex_t;
 typedef boost::graph_traits<Graph>::edge_descriptor edge_t;
