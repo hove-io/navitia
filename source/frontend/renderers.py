40f4f76815798cd6cbe594abbe59cc3c2373e9f2
Tristram Gräbener <tristram.grabener@canaltp.fr>
2013-03-13 18:23:12 +0100

    Jmeter : correction de l'ordre de création des reps

--- source/frontend/renderers.py
+++ source/frontend/renderers.py
@@ -12,15 +12,15 @@ def render(dico, format, callback):
         if callback == '' or callback == None:
             result = Response(json_str, mimetype='application/json')
         else:
-            result = Response(callback + '(' + json_str + ')', mimetype='application/json')
+            result = Response(callback + '(' + json_str + ')', mimetype='application/json;charset=utf-8')
     elif format == 'txt':
-        result = Response(json.dumps(dico, ensure_ascii=False, indent=4), mimetype='text/plain')
+        result = Response(json.dumps(dico, ensure_ascii=False, indent=4), mimetype='text/plain;charset=utf-8')
     elif format == 'xml':
-        result = Response('<?xml version="1.0" encoding="UTF-8"?>\n'+ dict2xml.dict2xml(dico, wrap="Response"), mimetype='application/xml')
+        result = Response('<?xml version="1.0" encoding="UTF-8"?>\n'+ dict2xml.dict2xml(dico, wrap="Response"), mimetype='application/xml;charset=utf-8')
     elif format == 'pb':
         result = Response('Protocol buffer not supported for this request', status=404)
     else:
-        result = Response("Unknown file format format. Please choose .json, .txt, .xml or .pb", mimetype='text/plain', status=404)
+        result = Response("Unknown file format format. Please choose .json, .txt, .xml or .pb", mimetype='text/plain;charset=utf-8', status=404)
     result.headers.add('Access-Control-Allow-Origin', '*')
     return result
 
