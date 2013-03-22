6f7f3ad6046f329e387fc008ab9b9026215cd4fc
krishna <krishna.adhikari@canaltp.fr>
2013-03-22 12:27:18 +0100

    POI : Connecteur POI ajouté

---
+++ source/naviMake/poi_parser.cpp
@@ -0,0 +1,178 @@
+#include "poi_parser.h"
+#include "type/data.h"
+#include <boost/lexical_cast.hpp>
+#include "type/data.h"
+#include "georef/georef.h"
+#include "utils/csv.h"
+#include "utils/configuration.h"
+#include <boost/algorithm/string.hpp>
+
+
+
+namespace navitia{ namespace georef{
+PoiParser::PoiParser(const std::string & path): path(path){
+        init_logger();
+        logger = log4cplus::Logger::getInstance("log");
+}
+
+
+void PoiParser::fill_poi_type(GeoRef & georef_to_fill)
+{
+    // Verification des entêtes:
+    CsvReader csv(path + "/" + "poi_type.txt", ',', true);
+    std::vector<std::string> mandatory_headers = {"poi_type_id" , "poi_type_name"};
+    if(!csv.validate(mandatory_headers)) {
+        LOG4CPLUS_FATAL(logger, "Erreur lors du parsing de " + csv.filename +" . Il manque les colonnes : " + csv.missing_headers(mandatory_headers));
+    }
+
+    int id_c = csv.get_pos_col("poi_type_id"), name_c = csv.get_pos_col("poi_type_name");
+
+    int id = 1;
+    while(!csv.eof()){
+
+        auto row = csv.next();
+        if (!row.empty()){
+            georef::POIType ptype;
+            ptype.idx = id-1;
+            if (id_c != -1 && row[id_c]!= "")
+                ptype.uri = row[id_c];
+            else
+                ptype.uri = boost::lexical_cast<std::string>(id);
+            ptype.name = row[name_c];
+            georef_to_fill.poitypes.push_back(ptype);
+            ++id;
+        }
+    }
+
+    //Chargement de la liste poitype_map
+    georef_to_fill.build_poitypes();
+
+
+}
+
+void PoiParser::fill_poi(GeoRef & georef_to_fill)
+{
+    // Verification des entêtes:
+    CsvReader csv(path + "/" + "poi.txt", ',', true);
+    std::vector<std::string> mandatory_headers = {"poi_id", "poi_name", "poi_lat", "poi_lon"};
+    if(!csv.validate(mandatory_headers)) {
+        LOG4CPLUS_FATAL(logger, "Erreur lors du parsing de " + csv.filename +" . Il manque les colonnes : " + csv.missing_headers(mandatory_headers));
+    }
+    int id_c = csv.get_pos_col("poi_id"), name_c = csv.get_pos_col("poi_name"), weight_c = csv.get_pos_col("poi_weight"),
+            lat_c = csv.get_pos_col("poi_lat"), lon_c = csv.get_pos_col("poi_lon"), type_c = csv.get_pos_col("poi_type_id");
+
+    int id = 1;
+    while (!csv.eof()){
+        auto row = csv.next();
+        if (!row.empty()){
+            georef::POI poi;
+            poi.idx = id-1;
+            if (id_c != -1 && row[id_c] != "")
+                poi.uri = row[id_c];
+            else
+                poi.uri = boost::lexical_cast<std::string>(id);
+            poi.name = row[name_c];
+            if (weight_c != -1 && row[weight_c] != "")
+                poi.weight = boost::lexical_cast<int>(row[weight_c]);
+            else
+                poi.weight = 0;
+
+            try{
+                poi.coord.set_lon(boost::lexical_cast<double>(row[lon_c]));
+                poi.coord.set_lat(boost::lexical_cast<double>(row[lat_c]));
+            }
+            catch(boost::bad_lexical_cast ) {
+                std::cout << "Impossible de parser les coordonnées pour "
+                          << row[id_c] << " " << row[name_c];
+            }
+
+            // Lire la référence du type de POI
+            poi.poitype = row[type_c];
+            auto ptype = georef_to_fill.poitype_map.find(row[type_c]);
+            if (ptype == georef_to_fill.poitype_map.end()){
+                // ajouter l'Index par défaut ??
+                poi.poitype_idx = -1;
+            }
+            else
+                poi.poitype_idx = ptype->second;
+            georef_to_fill.pois.push_back(poi);
+            ++id;
+        }
+    }
+    //Chargement de la liste poitype_map
+    georef_to_fill.build_pois();
+
+}
+
+void PoiParser::fill(GeoRef & georef_to_fill)
+{
+    // Lire les fichiers poi-type.txt
+    fill_poi_type(georef_to_fill);
+    LOG4CPLUS_TRACE(logger, "On a  " + boost::lexical_cast<std::string>(georef_to_fill.poitypes.size())+ " POITypes");
+
+    // Lire les fichiers poi.txt
+    fill_poi(georef_to_fill);
+    LOG4CPLUS_TRACE(logger, "On a  " + boost::lexical_cast<std::string>(georef_to_fill.pois.size())+ " POIs");
+}
+
+void PoiParser::fill_alias(GeoRef & georef_to_fill){
+    // Verification des entêtes:
+    std::string key, value;
+    CsvReader csv(path + "/" + "alias.txt", '=', true);
+    std::vector<std::string> mandatory_headers = {"key" , "value"};
+    if(!csv.validate(mandatory_headers)) {
+        LOG4CPLUS_FATAL(logger, "Erreur lors du parsing de " + csv.filename +" . Il manque les colonnes : " + csv.missing_headers(mandatory_headers));
+    }
+
+    int key_c = csv.get_pos_col("key"), value_c = csv.get_pos_col("value");
+    while(!csv.eof()){
+        auto row = csv.next();
+        if (!row.empty()){
+            if (key_c != -1){
+                key = row[key_c];
+                value = row[value_c];
+                boost::to_lower(key);
+                boost::to_lower(value);
+                georef_to_fill.alias[key]= value;
+            }
+        }
+    }
+}
+
+void PoiParser::fill_synonymes(GeoRef &georef_to_fill)
+{
+    std::string key, value;
+    CsvReader csv(path + "/" + "synonyme.txt", '=', true);
+    std::vector<std::string> mandatory_headers = {"key" , "value"};
+    if(!csv.validate(mandatory_headers)) {
+        LOG4CPLUS_FATAL(logger, "Erreur lors du parsing de " + csv.filename +" . Il manque les colonnes : " + csv.missing_headers(mandatory_headers));
+    }
+
+    int key_c = csv.get_pos_col("key"), value_c = csv.get_pos_col("value");
+    while(!csv.eof()){
+        auto row = csv.next();
+        if (!row.empty()){
+            if (key_c != -1){
+                key = row[key_c];
+                value = row[value_c];
+                boost::to_lower(key);
+                boost::to_lower(value);
+                georef_to_fill.synonymes[key]=value;
+            }
+        }
+    }
+}
+
+void PoiParser::fill_alias_synonyme(GeoRef &georef_to_fill)
+{
+    // Lire les fichiers poi-type.txt
+    fill_alias(georef_to_fill);
+    LOG4CPLUS_TRACE(logger, "On a  " + boost::lexical_cast<std::string>(georef_to_fill.alias.size())+ " alias");
+
+    // Lire les fichiers poi-type.txt
+    fill_synonymes(georef_to_fill);
+    LOG4CPLUS_TRACE(logger, "On a  " + boost::lexical_cast<std::string>(georef_to_fill.synonymes.size())+ " synonymes");
+}
+
+
+}}
---
+++ source/naviMake/poi_parser.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <string>
+#include "type/data.h"
+#include <utils/logger.h>
+
+
+
+namespace navitia{ namespace georef{
+class PoiParser
+{
+private:
+    std::string path;///< Chemin vers les fichiers
+    log4cplus::Logger logger;
+
+public:
+    PoiParser(const std::string & path);
+    PoiParser(): path(){}
+
+    void fill_poi_type(GeoRef & georef_to_fill);
+    void fill_poi(GeoRef & georef_to_fill);
+    void fill(GeoRef & georef_to_fill);
+    void fill_alias(GeoRef & georef_to_fill);
+    void fill_synonymes(GeoRef & georef_to_fill);
+    void fill_alias_synonyme(GeoRef & georef_to_fill);
+};
+}}
+
+
