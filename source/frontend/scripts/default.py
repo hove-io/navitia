5138a6c145c89b6262f23ebd384a0866122ae522
Vincent Lara <vincent.lara@canaltp.fr>
2013-04-15 14:51:56 +0200

    Autocomplete : L'argument de requetage est maintenant q
    L'argument était précédemment nommé name.

--- source/frontend/scripts/default.py
+++ source/frontend/scripts/default.py
@@ -82,7 +82,7 @@ class Script:
     def autocomplete(self, request_args, version, region):
         req = request_pb2.Request()
         req.requested_api = type_pb2.AUTOCOMPLETE
-        req.autocomplete.name = self.v.arguments['name']
+        req.autocomplete.q = self.v.arguments['q']
         req.autocomplete.depth = self.v.arguments['depth']
         req.autocomplete.nbmax = self.v.arguments['nbmax']
         for object_type in self.v.arguments["object_type[]"]:
