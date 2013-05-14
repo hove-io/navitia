22a1cf204b2653a9920b029aa3201eb28ba5cc0b
Tristram Gräbener <tristram.grabener@canaltp.fr>
2013-05-14 14:10:08 +0200

    Transverse : extraction de utils dans dépôt séparé
    Ces fonctions seront partagées entre différents projets

--- source/utils/logger.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <log4cplus/logger.h>
-
-/** Crée une configuration par défaut pour le logger */
-void init_logger();
-
-/** Configure le logger à partir du fichier de configuration */
-void init_logger(const std::string & config_file);
