f47305773e8c7d8d7e8bbb664e290fff65c86ae0
Vincent Lara <vincent.lara@canaltp.fr>
2013-09-09 18:22:35 +0200

    Scripts : Modification configuration update_db
    On demande maintenant fichier de configuration

--- source/scripts/update_db.sh
+++ source/scripts/update_db.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
+#You have to be logged as postgres to execute this script
 
+if [ $# -ne 1 ]
+then
+    echo 'fichier de conf requis en paramétre'
+    exit 1
+fi
+
+if [ ! -r $1 ]
+then
+    echo "$1 n'est pas lisible"
+    exit 2
+fi
 #en cas d'erreur on s'arrete
 set -e
 
-. settings.sh
-
+source $1
 cd $sql_dir
 
 tempfile=$(mktemp)
