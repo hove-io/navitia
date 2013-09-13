045819f721d66011d2fb9e5fdd114bd31306023d
Abderrahim Azime <abderrahim.azime@canaltp.fr>
2013-09-13 11:11:54 +0200

    Journeys : suppression des données de From, To
    suppression de date_taime de From et To

--- source/jormungandr/interfaces/v1/Journeys.py
+++ source/jormungandr/interfaces/v1/Journeys.py
@@ -92,19 +92,6 @@ class section_type(enum_type):
             pass
         return super(section_type, self).output(key, obj)
 
-class get_temp_date_time():
-    def output(self, key, obj):
-        result = {}
-        enum = obj.DESCRIPTOR.fields_by_name['type'].enum_type.values_by_name
-        if obj.type == enum['STREET_NETWORK'].number:
-            result = {"departure_date_time": obj.begin_date_time, "arrival_date_time": obj.end_date_time}
-        else:
-            stop_date_times = getattr(obj, "stop_date_times")
-            result = {"departure_date_time": stop_date_times[0].departure_date_time, "arrival_date_time": stop_date_times[-1].arrival_date_time}
-
-
-        return result
-
 class section_place(PbField):
     def output(self, key, obj):
         enum_t = obj.DESCRIPTOR.fields_by_name['type'].enum_type.values_by_name
@@ -128,8 +115,8 @@ section = {
                          attribute="street_network.path_items"),
     "transfer_type" : enum_type(),
     "stop_date_times" : NonNullList(NonNullNested(stop_date_time)),
-    #temp_date_time : une variable temporaire
-    "temp_date_time" : get_temp_date_time()
+    "departure_date_time" : fields.String(attribute="begin_date_time"),
+    "arrival_date_time" : fields.String(attribute="end_date_time")
 }
 
 
@@ -189,23 +176,6 @@ class add_journey_href(object):
             return objects
         return wrapper
 
-class add_date_time_from_to(object):
-    def __call__(self, f):
-        @wraps(f)
-        def wrapper(*args, **kwargs):
-            objects = f(*args, **kwargs)
-            if not "journeys" in objects[0].keys():
-                return objects
-            for journey in objects[0]['journeys']:
-                if "sections" in journey.keys():
-                    for section in journey["sections"]:
-                        if "temp_date_time" in section.keys():
-                            section["from"]["date_time"] = section["temp_date_time"]["departure_date_time"]
-                            section["to"]["date_time"] = section["temp_date_time"]["arrival_date_time"]
-                            del section["temp_date_time"]
-            return objects
-        return wrapper
-
 class Journeys(ResourceUri):
     def __init__(self):
         modes = ["walking", "car", "bike", "br"]
@@ -240,7 +210,6 @@ class Journeys(ResourceUri):
     @clean_links()
     @add_id_links()
     @add_journey_href()
-    @add_date_time_from_to()
     @marshal_with(journeys)
     def get(self, region=None, lon=None, lat=None, uri=None):
         args = self.parser.parse_args()
