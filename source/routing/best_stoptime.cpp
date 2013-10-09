5e3f4a8b998a70502841d512c1c3d580f210be72
Vincent Lara <vincent.lara@canaltp.fr>
2013-10-09 17:59:25 +0200

    Kraken : Best stop time renvoie maintenant l'heure d'arrivee ou de départ

--- source/routing/best_stoptime.cpp
+++ source/routing/best_stoptime.cpp
@@ -4,14 +4,13 @@ namespace navitia { namespace routing {
 
 std::pair<const type::StopTime*, uint32_t>
 best_stop_time(const type::JourneyPatternPoint* jpp,
-               const DateTime &dt,
-               /*const type::Properties &required_properties*/
+               const DateTime dt,
                const type::AccessibiliteParams & accessibilite_params,
                const bool clockwise, const type::Data &data, bool reconstructing_path) {
     if(clockwise)
-        return earliest_stop_time(jpp, dt, data, reconstructing_path, accessibilite_params/*required_properties*/);
+        return earliest_stop_time(jpp, dt, data, reconstructing_path, accessibilite_params);
     else
-        return tardiest_stop_time(jpp, dt, data, reconstructing_path, accessibilite_params/*required_properties*/);
+        return tardiest_stop_time(jpp, dt, data, reconstructing_path, accessibilite_params);
 }
 
 
@@ -19,8 +18,7 @@ best_stop_time(const type::JourneyPatternPoint* jpp,
 /** Which is the first valid stop_time in this range ?
  *  Returns invalid_idx is none is
  */
-const type::StopTime*
-valid_pick_up(type::idx_t idx, type::idx_t end, uint32_t date,
+const type::StopTime* valid_pick_up(type::idx_t idx, type::idx_t end, uint32_t date,
               uint32_t hour, const type::Data &data, bool reconstructing_path,
               const type::VehicleProperties &required_vehicle_properties){
     for(; idx < end; ++idx) {
@@ -35,10 +33,9 @@ valid_pick_up(type::idx_t idx, type::idx_t end, uint32_t date,
     return nullptr;
 }
 
-const type::StopTime*
-valid_drop_off(type::idx_t idx, type::idx_t end, uint32_t date,
-               uint32_t hour, const type::Data &data,
-               bool reconstructing_path, const type::VehicleProperties &required_vehicle_properties){
+const type::StopTime* valid_drop_off(type::idx_t idx, type::idx_t end, uint32_t date,
+               uint32_t hour, const type::Data &data, bool reconstructing_path,
+               const type::VehicleProperties &required_vehicle_properties){
     for(; idx < end; ++idx) {
         const type::StopTime* st = data.dataRaptor.st_idx_backward[idx];
         if (st->arrival_validity_pattern->check(date)) {
@@ -53,40 +50,52 @@ valid_drop_off(type::idx_t idx, type::idx_t end, uint32_t date,
 
 std::pair<const type::StopTime*, uint32_t>
 earliest_stop_time(const type::JourneyPatternPoint* jpp,
-                   const DateTime &dt, const type::Data &data,
+                   const DateTime dt, const type::Data &data,
                    bool reconstructing_path,
-                   const type::AccessibiliteParams & accessibilite_params
-                   /*const type::Properties &required_properties*/) {
+                   const type::AccessibiliteParams & accessibilite_params) {
 
-    // If the stop_point doesn’t match the required properties, we don’t bother looking further
-    if(!jpp->stop_point->accessible(accessibilite_params.properties/*required_properties*/))
+    // If the stop_point doesn’t match the required properties,
+    // we don’t bother looking further
+    if(!jpp->stop_point->accessible(accessibilite_params.properties))
         return std::make_pair(nullptr, 0);
 
 
     //On cherche le plus petit stop time de la journey_pattern >= dt.hour()
-    std::vector<uint32_t>::const_iterator begin = data.dataRaptor.departure_times.begin() +
+    auto begin = data.dataRaptor.departure_times.begin() +
             data.dataRaptor.first_stop_time[jpp->journey_pattern->idx] +
             jpp->order * data.dataRaptor.nb_trips[jpp->journey_pattern->idx];
-    std::vector<uint32_t>::const_iterator end = begin + data.dataRaptor.nb_trips[jpp->journey_pattern->idx];
-
+    auto end = begin + data.dataRaptor.nb_trips[jpp->journey_pattern->idx];
+    const auto bound_predicate = [](uint32_t departure_time, uint32_t hour){
+                               return departure_time < hour;};
     auto it = std::lower_bound(begin, end, DateTimeUtils::hour(dt),
-                               [](uint32_t departure_time, uint32_t hour){
-                               return departure_time < hour;});
+                               bound_predicate);
 
     type::idx_t idx = it - data.dataRaptor.departure_times.begin();
-    type::idx_t end_idx = (begin - data.dataRaptor.departure_times.begin()) +  data.dataRaptor.nb_trips[jpp->journey_pattern->idx];
+    type::idx_t end_idx = (begin - data.dataRaptor.departure_times.begin()) +
+                           data.dataRaptor.nb_trips[jpp->journey_pattern->idx];
 
     //On renvoie le premier trip valide
-    const type::StopTime* first_st = valid_pick_up(idx, end_idx, DateTimeUtils::date(dt), DateTimeUtils::hour(dt), data, reconstructing_path, accessibilite_params.vehicle_properties/*required_properties*/);
-
-    // If no trip was found, we look for the next day
-    if(first_st == nullptr){
+    const type::StopTime* first_st = valid_pick_up(idx, end_idx,
+            DateTimeUtils::date(dt), DateTimeUtils::hour(dt), data,
+            reconstructing_path, accessibilite_params.vehicle_properties);
+    auto working_dt = dt;
+    // If no trip was found, we look for one the day after
+    if(first_st == nullptr) {
         idx = begin - data.dataRaptor.departure_times.begin();
-        first_st = valid_pick_up(idx, end_idx, DateTimeUtils::date(dt) + 1, 0, data, reconstructing_path, accessibilite_params.vehicle_properties/*required_properties*/);
+        working_dt = DateTimeUtils::set(DateTimeUtils::date(dt)+1, 0);
+        first_st = valid_pick_up(idx, end_idx, DateTimeUtils::date(working_dt), 0,
+            data, reconstructing_path, accessibilite_params.vehicle_properties);
     }
 
-    if(first_st != nullptr){
-        return std::make_pair(first_st, !first_st->is_frequency() ? 0 : compute_gap(DateTimeUtils::hour(dt), first_st->start_time, first_st->headway_secs, true));
+    if(first_st != nullptr) {
+        if(!first_st->is_frequency()) {
+            DateTimeUtils::update(working_dt, first_st->departure_time);
+        } else {
+            working_dt = dt;
+            const DateTime tmp_dt = f_departure_time(DateTimeUtils::hour(working_dt), first_st);
+            DateTimeUtils::update(working_dt, DateTimeUtils::hour(tmp_dt));
+        }
+        return std::make_pair(first_st, working_dt);
     }
 
     //Cette journey_pattern ne comporte aucun trip compatible
@@ -96,36 +105,48 @@ earliest_stop_time(const type::JourneyPatternPoint* jpp,
 
 std::pair<const type::StopTime*, uint32_t>
 tardiest_stop_time(const type::JourneyPatternPoint* jpp,
-                   const DateTime &dt, const type::Data &data,
+                   const DateTime dt, const type::Data &data,
                    bool reconstructing_path,
-                   const type::AccessibiliteParams & accessibilite_params
-                   /*const type::Properties &required_properties*/) {
-    if(!jpp->stop_point->accessible(accessibilite_params.properties/*required_properties*/))
+                   const type::AccessibiliteParams & accessibilite_params) {
+    if(!jpp->stop_point->accessible(accessibilite_params.properties))
         return std::make_pair(nullptr, 0);
     //On cherche le plus grand stop time de la journey_pattern <= dt.hour()
     const auto begin = data.dataRaptor.arrival_times.begin() +
                        data.dataRaptor.first_stop_time[jpp->journey_pattern->idx] +
                        jpp->order * data.dataRaptor.nb_trips[jpp->journey_pattern->idx];
     const auto end = begin + data.dataRaptor.nb_trips[jpp->journey_pattern->idx];
-    auto tmp_vec = std::vector<uint32_t>(begin, end);
-    auto it = std::lower_bound(begin, end, DateTimeUtils::hour(dt),
-                               [](uint32_t arrival_time, uint32_t hour){
-                                  return arrival_time > hour;}
-                              );
+    const auto bound_predicate = [](uint32_t arrival_time, uint32_t hour){
+                                  return arrival_time > hour;};
+    auto it = std::lower_bound(begin, end, DateTimeUtils::hour(dt), bound_predicate);
 
     type::idx_t idx = it - data.dataRaptor.arrival_times.begin();
-    type::idx_t end_idx = (begin - data.dataRaptor.arrival_times.begin()) +  data.dataRaptor.nb_trips[jpp->journey_pattern->idx];
+    type::idx_t end_idx = (begin - data.dataRaptor.arrival_times.begin()) +
+                           data.dataRaptor.nb_trips[jpp->journey_pattern->idx];
 
-    const type::StopTime* first_st = valid_drop_off(idx, end_idx, DateTimeUtils::date(dt), DateTimeUtils::hour(dt), data, reconstructing_path, accessibilite_params.vehicle_properties/*required_properties*/);
+    const type::StopTime* first_st = valid_drop_off(idx, end_idx,
+            DateTimeUtils::date(dt), DateTimeUtils::hour(dt), data,
+            reconstructing_path, accessibilite_params.vehicle_properties);
 
-    // If no trip was found, we look for the next day
+    auto working_dt = dt;
+    // If no trip was found, we look for one the day before
     if(first_st == nullptr && DateTimeUtils::date(dt) > 0){
         idx = begin - data.dataRaptor.arrival_times.begin();
-        first_st = valid_drop_off(idx, end_idx, DateTimeUtils::date(dt) -1, DateTimeUtils::SECONDS_PER_DAY, data, reconstructing_path, accessibilite_params.vehicle_properties/*required_properties*/);
+        working_dt = DateTimeUtils::set(DateTimeUtils::date(working_dt) - 1,
+                                        DateTimeUtils::SECONDS_PER_DAY - 1);
+        first_st = valid_drop_off(idx, end_idx, DateTimeUtils::date(working_dt),
+                DateTimeUtils::SECONDS_PER_DAY - 1, data, reconstructing_path,
+                accessibilite_params.vehicle_properties);
     }
 
     if(first_st != nullptr){
-        return std::make_pair(first_st, !first_st->is_frequency() ? 0 : compute_gap(DateTimeUtils::hour(dt), first_st->start_time, first_st->headway_secs, false));
+        if(!first_st->is_frequency()) {
+            DateTimeUtils::update(working_dt, DateTimeUtils::hour(first_st->arrival_time), false);
+        } else {
+            working_dt = dt;
+            const DateTime tmp_dt = f_arrival_time(DateTimeUtils::hour(working_dt), first_st);
+            DateTimeUtils::update(working_dt, DateTimeUtils::hour(tmp_dt), false);
+        }
+        return std::make_pair(first_st, working_dt);
     }
 
     //Cette journey_pattern ne comporte aucun trip compatible
