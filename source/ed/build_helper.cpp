bea40488c79925fcb669c5f69dc8cfec8b04fb2b
Vincent Lara <vincent.lara@canaltp.fr>
2013-10-11 11:18:53 +0200

    Ed : Builder : Erreur de merge

--- source/ed/build_helper.cpp
+++ source/ed/build_helper.cpp
@@ -299,13 +299,4 @@ void builder::connection(const std::string & name1, const std::string & name2, f
          vj->stop_time_list.back()->set_pick_up_allowed(false);
      }
  }
-
-
-void builder::finish() {
-    for(auto vj : this->data.pt_data.vehicle_journeys) {
-        vj->stop_time_list.front()->set_drop_off_allowed(false);
-        vj->stop_time_list.back()->set_pick_up_allowed(false);
-    }
-}
-
 }
