f6637e97bff51f38d475b672e4494b6cec57cf4a
Vincent Lara <vincent.lara@canaltp.fr>
2013-10-17 18:08:26 +0200

    Jormungandr : Implémentation représentation jsonp

--- source/jormungandr/jormungandr.py
+++ source/jormungandr/jormungandr.py
@@ -7,6 +7,7 @@ from conf import base_url
 from instance_manager import NavitiaManager, RegionNotFound
 from flask import Flask, url_for, make_response
 from flask.ext.restful import Api
+from flask.ext.restful.representations import json
 from interfaces.v0_routing import v0_routing
 from interfaces.v1_routing import v1_routing
 from interfaces.documentation import v0_documentation, v1_documentation
@@ -22,12 +23,20 @@ def output_xml(data, code, headers=None):
     resp.headers.extend(headers or {})
     return resp
 
+def output_jsonp(data, code, headers=None):
+    resp = json.output_json(data, code, headers)
+    callback = request.args.get('callback', False)
+    if callback:
+        resp.data = str(callback)+'(' + resp.data + ')'
+    return resp
+
 app = Flask(__name__)
 app.debug = True
 app.config.update(PROPAGATE_EXCEPTIONS=True)
 api = Api(app, catch_all_404s=True)
 api.representations['application/xml'] = output_xml
-api = Api(app)
+api.representations['application/jsonp'] = output_jsonp
+api.representations['text/jsonp'] = output_jsonp
 
 v1_routing(api)
 v0_routing(api)
@@ -37,11 +46,10 @@ v1_documentation(api)
 def region_not_found(error):
     return {"error" : error.value}, 404
 
-
 def kill_thread(signal, frame):
     NavitiaManager().stop()
     sys.exit(0)
-config_file = 'Jormungandr.ini' if not os.environ.has_key('JORMUNGANDR_CONFIG_FILE')\
+config_file = '/home/vlara/navitiagit/source/jormungandr/Jormungandr.ini' if not os.environ.has_key('JORMUNGANDR_CONFIG_FILE')\
                                 else os.environ['JORMUNGANDR_CONFIG_FILE']
 if __name__ == '__main__':
     signal.signal(signal.SIGINT, kill_thread)
