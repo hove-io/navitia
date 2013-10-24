e737164741f48e9ce4568d1b5013080633fbdead
Alexandre Jacquin <alexandre.jacquin@canaltp.fr>
2013-10-24 18:48:58 +0200

    jormungandr: ajout de placesNeraby au déploiement

--- source/jormungandr/default_settings.py
+++ source/jormungandr/default_settings.py
@@ -3,8 +3,7 @@
 INSTANCES_DIR='/etc/jormungandr.d'
 
 #chaine de connnection à postgresql pour la base jormungandr
-#PG_CONNECTION_STRING='postgresql://navitia:navitia@localhost/jormun'
-PG_CONNECTION_STRING=''
+PG_CONNECTION_STRING='postgresql://navitia:navitia@localhost/jormun'
 
 #désactivation de l'authentification
 PUBLIC=True
