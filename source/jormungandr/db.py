3cd396a0c66fd96d05e95f801aae3b79f3e5d930
Alexandre Jacquin <alexandre.jacquin@canaltp.fr>
2013-10-24 09:42:05 +0200

    jormungandr: ajout de l'authentification via la base de données
    Toutes les API qui prennent une région ou des coordonnées sont couvertes

---
+++ source/jormungandr/db.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Table, MetaData, select, create_engine
+from app import app
+
+__ALL__ = ['engine', 'meta', 'key', 'user', 'instance', 'api', 'authorization']
+
+#TODO
+app.config['PG_CONNECTION_STRING'] = 'postgresql://navitia:navitia@localhost/jormun'
+
+engine = create_engine(app.config['PG_CONNECTION_STRING'])
+meta = MetaData(engine)
+key = Table('key', meta, autoload=True, schema='jormungandr')
+user = Table('user', meta, autoload=True, schema='jormungandr')
+instance = Table('instance', meta, autoload=True, schema='jormungandr')
+api = Table('api', meta, autoload=True, schema='jormungandr')
+authorization = Table('authorization', meta, autoload=True,
+        schema='jormungandr')
+
