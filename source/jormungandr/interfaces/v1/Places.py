186010f0716ec6996cbded3798c449cbd342e3c6
Vincent Lara <vincent.lara@canaltp.fr>
2013-10-28 17:06:58 +0100

    Jormungandr : Gestion de places dans places_nearby

--- source/jormungandr/interfaces/v1/Places.py
+++ source/jormungandr/interfaces/v1/Places.py
@@ -93,7 +93,7 @@ class PlacesNearby(ResourceUri):
             if uri[-1] == '/':
                 uri = uri[:-1]
             uris = uri.split("/")
-            if len(uris) > 1 and uris[0] != "coord" and uris[0] != "addresses":
+            if len(uris) > 1 and uris[-1].count(";") == 0:
                 args["uri"] = uris[-1]
             else:
                 coord = uris[-1].split(";")
