06f3c5f2ace882ba75938397964cca3dc33c4717
Vincent Lara <vincent.lara@canaltp.fr>
2013-10-29 16:04:54 +0100

    Jormungandr : On met du logging

--- source/jormungandr/default_settings.py
+++ source/jormungandr/default_settings.py
@@ -1,10 +1,13 @@
 #encoding: utf-8
 #emplacement ou charger les fichier de configuration par instances
-INSTANCES_DIR='/etc/jormungandr.d'
+INSTANCES_DIR = '/etc/jormungandr.d'
 
 #chaine de connnection à postgresql pour la base jormungandr
-PG_CONNECTION_STRING='postgresql://navitia:navitia@localhost/jormun'
+PG_CONNECTION_STRING = 'postgresql://navitia:navitia@localhost/jormun'
 
 #désactivation de l'authentification
-PUBLIC=True
-PROPAGATE_EXCEPTIONS=True
+PUBLIC = True
+
+ERROR_HANDLER_FILE = 'jormungandr.log'
+ERROR_HANDLER_TYPE = 'rotating' # can be timedrotating
+ERROR_HANDLER_PARAMS = {'maxBytes' : 20000000, 'backupCount' : 5}
--- source/jormungandr/jormungandr_exceptions.py
+++ source/jormungandr/jormungandr_exceptions.py
@@ -1,3 +1,5 @@
+from flask import current_app, request
+
 __all__ = ["RegionNotFound", "DeadSocketException", "ApiNotFound",
             "InvalidArguments"]
 
@@ -17,6 +19,8 @@ class RegionNotFound(Exception):
         else:
             self.data['message'] = "Unable to parse region"
         self.code = 404
+        current_app.logger.error('RegionNotFound : ' + self.data['message']+\
+                " "+request.url)
 
     def __str__(self):
         return repr(self.data['message'])
@@ -26,15 +30,21 @@ class DeadSocketException(Exception):
     def __init__(self, region, path):
         self.data['message'] = "The region " + region + " is dead"
         self.code = 503
+        current_app.logger.error('DeadSocketException : ' + self.data['message']+\
+                " "+request.url)
 
 
 class ApiNotFound(Exception):
     def __init__(self, api):
         self.data['message'] = "The api " + api + " doesn't exist"
         self.code = 404
+        current_app.logger.error('ApiNotFound : ' + self.data['message']+\
+                " "+request.url)
 
 
 class InvalidArguments(Exception):
     def __init__(self, arg):
         self.data['message'] = "Invalid arguments " + arg
         self.code = 400
+        current_app.logger.error('InvalidArguments : ' + self.data['message']+\
+                " "+request.url)
