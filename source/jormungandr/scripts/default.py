f8f14577383efd08a5a205dd8f9225aacadd7818
Vincent Lara <vincent.lara@canaltp.fr>
2013-11-04 17:20:57 +0100

    Schedules : Changement stop_date_time en date_time

--- source/jormungandr/scripts/default.py
+++ source/jormungandr/scripts/default.py
@@ -129,7 +129,7 @@ class Script(object):
         st.count = 10 if not "count" in request.keys() else request["count"]
         st.start_page = 0 if not "start_page" in request.keys()\
                            else request["start_page"]
-        st.max_stop_date_times = 10 if not "max_stop_date_times" in request.keys() else request["max_stop_date_times"]
+        st.max_stop_date_times = 10 if not "max_date_times" in request.keys() else request["max_date_times"]
         resp = NavitiaManager().send_and_receive(req, region)
         return resp
 
