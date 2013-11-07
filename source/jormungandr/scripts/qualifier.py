8b871a82e669af8220ddfe1949de2d9fcbfc25e6
Alexandre Jacquin <alexandre.jacquin@canaltp.fr>
2013-11-07 11:51:45 +0100

    Jormungandr: fix qualification of journeys and add a few tests on it

--- source/jormungandr/scripts/qualifier.py
+++ source/jormungandr/scripts/qualifier.py
@@ -1,7 +1,6 @@
 # coding=utf-8
 import type_pb2
 import response_pb2
-from instance_manager import NavitiaManager
 
 def get_rabattement_duration(journey):
     sections = journey.sections
@@ -36,27 +35,28 @@ def qualifier_one(journeys):
         index = -1
         for journey in journeys :
             index = index + 1
-            if journey.duration > 1.2*journeys[trip_type["rapid"]].duration and \
-                journey.nb_transfers < journeys[trip_type["rapid"]].nb_transfers :
+            if (journey.duration < (1.2*journeys[trip_type["rapid"]].duration)) and \
+                    (journey.nb_transfers < journeys[trip_type["rapid"]].nb_transfers) :
                 trip_type["rapid_plus"] = index
 
         if trip_type["rapid_plus"] > -1 :
             trip_type["rapid"] = trip_type["rapid_plus"]
         index = -1
         rapid_duration = get_rabattement_duration(journeys[trip_type["rapid"]])
-        for journey in journeys :
+        for journey in journeys:
             index = index + 1
-            if journey.duration > 1.5*journeys[trip_type["rapid"]].duration :
-                current_duration = get_rabattement_duration(journey)
-                sections = getattr(journey, "sections")
-                if (not is_car(sections[0])) \
-                        and (not is_car(sections[-1])) \
-                        and current_duration > journey.duration * 0.1:
-                    trip_type["healthy"] = index
-                if journey.nb_transfers < journeys[trip_type["rapid"]].nb_transfers or \
-                        current_duration < rapid_duration:
-                    trip_type["comfort"] = index
+            if index == trip_type['rapid']:
+                continue
 
+            if journey.duration < 1.5*journeys[trip_type["rapid"]].duration :
+                current_duration = get_rabattement_duration(journey)
+                sections = journey.sections
+                if (not is_car(sections[0])) and (not is_car(sections[-1])) \
+                        and current_duration > (rapid_duration * 1.1):
+                    trip_type["healthy"] = index
+                if journey.nb_transfers < journeys[trip_type["rapid"]].nb_transfers \
+                        or current_duration < rapid_duration:
+                    trip_type["comfort"] = index
         if trip_type["rapid"] > -1 :
             journeys[trip_type["rapid"]].type =  "rapid"
         if trip_type["comfort"] > -1 :
