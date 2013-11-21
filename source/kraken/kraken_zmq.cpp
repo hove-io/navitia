c3f26c51bafb3520903c8802657213eb3ecfe106
Alexandre Jacquin <alexandre.jacquin@canaltp.fr>
2013-11-21 10:43:57 +0100

    Kraken: remove old dead code
    it was needed for build with SimpleAmqpClient before we fix it the lib

--- source/kraken/kraken_zmq.cpp
+++ source/kraken/kraken_zmq.cpp
@@ -1,5 +1,3 @@
-//on inclue simpleAmqpClient dés le début, sinon probléme avec la compatibilité C++ 11
-#include <SimpleAmqpClient/SimpleAmqpClient.h>
 #include "worker.h"
 #include "maintenance_worker.h"
 #include "type/type.pb.h"
