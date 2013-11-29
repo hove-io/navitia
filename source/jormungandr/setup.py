112f1f63dcb84e0ea7c7841cf426f964992e8ed9
Vincent Lara <vincent.lara@canaltp.fr>
2013-11-29 16:41:49 +0100

    Jormungandr: Addition of a setup.py

---
+++ source/jormungandr/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='jormungandr',
+        version='0.10.0',
+        description='webservice d'exposition en http de kraken',
+        author='CanalTP',
+        author_email='vincent.lara@canaltp.fr',
+        url='www.navitia.io',
+        packages=['jormungandr']
+)
