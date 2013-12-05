d38c9af3038f90368e5963ba62c861c38611369b
Vincent Lara <lara.vincent@gmail.com>
2013-12-05 18:36:26 +0100

    Kraken: We use the new argument stop_point_connection_list

--- source/routing/raptor_path.cpp
+++ source/routing/raptor_path.cpp
@@ -62,15 +62,15 @@ makePath(type::idx_t destination_idx, unsigned int countb, bool clockwise,
             auto departure = raptor_.data.pt_data.journey_pattern_points[current_jpp_idx]->stop_point;
             auto destination_jpp = raptor_.data.pt_data.journey_pattern_points[raptor_.get_boarding_jpp(countb, current_jpp_idx)->idx];
             auto destination = destination_jpp->stop_point;
-            auto connections_idx = departure->get(type::Type_e::Connection, raptor_.data.pt_data);
+            auto connections = departure->stop_point_connection_list;
             l = raptor_.labels[countb][current_jpp_idx];
             auto find_predicate = [&](type::idx_t idx)->bool {
                 const auto connection = raptor_.data.pt_data.stop_point_connections[idx];
                 return destination->idx == connection->destination->idx;
             };
 
-            auto it = std::find_if(connections_idx.begin(), connections_idx.end(), find_predicate);
-            if(it == connections_idx.end()) {
+            auto it = std::find_if(connections.begin(), connections.end(), find_predicate);
+            if(it == connections.end()) {
                 auto r2 = raptor_.labels[countb][raptor_.get_boarding_jpp(countb, current_jpp_idx)->idx];
                 if(clockwise) {
                    item = PathItem(r2, l);
@@ -78,7 +78,7 @@ makePath(type::idx_t destination_idx, unsigned int countb, bool clockwise,
                    item = PathItem(l, r2);
                 }
             } else {
-                const auto stop_point_connection = raptor_.data.pt_data.stop_point_connections[*it];
+                const auto stop_point_connection = *it;
                 if(clockwise) {
                     item = PathItem(l - stop_point_connection->display_duration, l);
                 } else {
