849e37bbd385605c830530770fd14742efcfaf3a
Abderrahim Azime <abderrahim.azime@canaltp.fr>
2013-12-09 15:27:21 +0100

    ed : connection à redis
    lors de la construction de l'objet extcode2uri

--- source/ed/connectors/extcode2uri.cpp
+++ source/ed/connectors/extcode2uri.cpp
@@ -6,14 +6,14 @@ namespace ed{ namespace connectors{
 ExtCode2Uri::ExtCode2Uri(const std::string& redis_connection):redis(redis_connection){
     init_logger();
     logger = log4cplus::Logger::getInstance("log");
-}
-
-void ExtCode2Uri::to_redis(const ed::Data & data){
     try{
         this->redis.redis_connect_with_timeout();
     }catch(const navitia::exception& ne){
         throw navitia::exception(ne.what());
     }
+}
+
+void ExtCode2Uri::to_redis(const ed::Data & data){
     // StopArea
     ed::add_redis(data.stop_areas, *this);
     // StopPoint
