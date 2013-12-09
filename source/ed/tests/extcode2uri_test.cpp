ff725131f3f116981d7e5efaad7a274509338b48
Abderrahim Azime <abderrahim.azime@canaltp.fr>
2013-12-09 15:25:02 +0100

    ed : usage de fixture pour les tests unitaires

--- source/ed/tests/extcode2uri_test.cpp
+++ source/ed/tests/extcode2uri_test.cpp
@@ -3,64 +3,57 @@
 #define BOOST_TEST_MODULE test_extcode2uri
 #include <boost/test/unit_test.hpp>
 
+class Params{
+public:
+    std::string host;
+    std::string db;
+    std::string password;
+    std::string port;
+    std::string timeout;
+    Params(){
+        this->host = "localhost";
+        this->db = "0";
+        this->password = "";
+        this->port = "6379";
+        this->timeout = "2";
 
-BOOST_AUTO_TEST_CASE(exception) {
+        if (getenv("REDISHOST") != nullptr){
+            this->host = getenv("REDISHOST");
+        }
+        if (getenv("REDISDB") != nullptr){
+            this->db = getenv("REDISDB");
+        }
+        if (getenv("REDISPASSWORD") != nullptr){
+            this->password = getenv("REDISPASSWORD");
+        }
+        if (getenv("REDISPORT") != nullptr){
+            this->host = getenv("REDISPORT");
+        }
+        if (getenv("REDISTIMEOUT") != nullptr){
+            this->timeout = getenv("REDISTIMEOUT");
+        }
+    }
+};
 
-    std::string host("localhost");
-    std::string db("0");
-    std::string password("password");
-    std::string port("6379");
-    std::string timeout("2");
-
-    if (getenv("REDISHOST") != nullptr){
-        host = getenv("REDISHOST");
-    }
-    if (getenv("REDISDB") != nullptr){
-        db = getenv("REDISDB");
-    }
-    if (getenv("REDISPASSWORD") != nullptr){
-        password = getenv("REDISPASSWORD");
-    }
-    if (getenv("REDISPORT") != nullptr){
-        host = getenv("REDISPORT");
-    }
-    if (getenv("REDISTIMEOUT") != nullptr){
-        timeout = getenv("REDISTIMEOUT");
-    }
+BOOST_FIXTURE_TEST_CASE(exception, Params) {
     host = "AAAAAAA";
-    ed::connectors::ExtCode2Uri redis_test("host=" + host + " db=" + db + " password=" + password + " port=" + port + " timeout=" + timeout);    
-    BOOST_REQUIRE_THROW(redis_test.redis.redis_connect_with_timeout(), navitia::exception);
-
+    bool test = true;
+    try{
+        ed::connectors::ExtCode2Uri redis_test("host=" + host + " db=" + db + " password=" + password
+                                           + " port=" + port + " timeout=" + timeout);
+    }catch(const navitia::exception& ne){
+        test = false;
+    }
+    BOOST_REQUIRE_EQUAL(test, false);
 }
-BOOST_AUTO_TEST_CASE(test1) {
 
-    std::string host("localhost");
-    std::string db("0");
-    std::string password("password");
-    std::string port("6379");
-    std::string timeout("2");
+BOOST_FIXTURE_TEST_CASE(test1,Params) {
 
-    if (getenv("REDISHOST") != nullptr){
-        host = getenv("REDISHOST");
-    }
-    if (getenv("REDISDB") != nullptr){
-        db = getenv("REDISDB");
-    }
-    if (getenv("REDISPASSWORD") != nullptr){
-        password = getenv("REDISPASSWORD");
-    }
-    if (getenv("REDISPORT") != nullptr){
-        host = getenv("REDISPORT");
-    }
-    if (getenv("REDISTIMEOUT") != nullptr){
-        timeout = getenv("REDISTIMEOUT");
-    }
-
-    ed::connectors::ExtCode2Uri redis_test("host=" + host + " db=" + db + " password=" + password + " port=" + port + " timeout=" + timeout);
+    ed::connectors::ExtCode2Uri redis_test("host=" + host + " db=" + db + " password=" + password
+                                           + " port=" + port + " timeout=" + timeout);
     redis_test.redis.redis_connect_with_timeout();
     redis_test.redis.set("t1", "test1");
     redis_test.redis.set("t2", "test2");
     BOOST_REQUIRE_EQUAL(redis_test.redis.get("t1"), "test1");
     BOOST_REQUIRE_EQUAL(redis_test.redis.get("t2"), "test2");
-
 }
