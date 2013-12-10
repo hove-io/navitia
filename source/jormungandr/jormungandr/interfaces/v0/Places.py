6bbc362fe7b5766baab24566a0fdee11cecb3d5a
Vincent Lara <lara.vincent@gmail.com>
2013-12-10 16:35:54 +0100

    Jormungandr: v0/interfaces/Places.py pep8 compliance

--- source/jormungandr/jormungandr/interfaces/v0/Places.py
+++ source/jormungandr/jormungandr/interfaces/v0/Places.py
@@ -2,33 +2,45 @@
 from flask import Flask
 from flask.ext.restful import Resource, fields, abort
 from jormungandr.instance_manager import InstanceManager
-from protobuf_to_dict import protobuf_to_dict
+from .protobuf_to_dict import protobuf_to_dict
 from flask.ext.restful import reqparse
 from jormungandr.interfaces.parsers import depth_argument
 from jormungandr.interfaces.argument import ArgumentDoc
 from jormungandr.authentification import authentification_required
 
+
 class Places(Resource):
+
     """Retreives places"""
     method_decorators = [authentification_required]
+
     def __init__(self):
         self.parsers = {}
-        self.parsers["get"] = reqparse.RequestParser(argument_class=ArgumentDoc)
+        self.parsers["get"] = reqparse.RequestParser(
+            argument_class=ArgumentDoc)
         self.parsers["get"].add_argument("q", type=unicode, required=True,
-                description="The data to search")
+                                         description="The data to search")
         self.parsers["get"].add_argument("type[]", type=str, action="append",
-                                 default=["stop_area","address",
-                                          "poi", "administrative_region"],
-                description="The type of data to search")
-        self.parsers["get"].add_argument("count", type=int,  default=10,
-                description="The maximum number of places returned")
+                                         default=["stop_area", "address",
+                                                  "poi",
+                                                  "administrative_region"],
+                                         description="The type of data to\
+                                         search")
+        self.parsers["get"].add_argument("count", type=int, default=10,
+                                         description="The maximum number of\
+                                         places returned")
         self.parsers["get"].add_argument("search_type", type=int, default=0,
-                description="Type of search: firstletter or typeerror")
-        self.parsers["get"].add_argument("admin_uri[]", type=str, action="append",
-                description="""If filled, will restrained the search within the
+                                         description="Type of search:\
+                                         firstletter or typeerror")
+        self.parsers["get"].add_argument("admin_uri[]", type=str,
+                                         action="append",
+                                         description="""If filled, will\
+                                         restrained the search within the
                     ²           given admin uris""")
-        self.parsers["get"].add_argument("depth", type=depth_argument, default=1,
-                description="The depth of the objects")
+        self.parsers["get"].add_argument("depth", type=depth_argument,
+                                         default=1,
+                                         description="The depth of the\
+                                         objects")
 
     def get(self, region):
         args = self.parsers["get"].parse_args()
