2ec5272c30be954b66a9a23c1440eb66e22438ef
Vincent Lara <lara.vincent@gmail.com>
2013-12-10 14:02:18 +0100

    Jormungandr: v1/make_links.py pep8 compliant

--- source/jormungandr/jormungandr/interfaces/v1/make_links.py
+++ source/jormungandr/jormungandr/interfaces/v1/make_links.py
@@ -12,7 +12,7 @@ class generate_links(object):
             objects = objects[0]
         if not "links" in objects.keys():
             objects["links"] = []
-        elif hasattr(self, "collections") :
+        elif hasattr(self, "collections"):
             for link in objects["links"]:
                 if "type" in link.keys():
                     self.collections.remove(link["type"])
@@ -26,6 +26,7 @@ class generate_links(object):
         if "uri" in kwargs:
             del kwargs["uri"]
         return kwargs
+
 
 class add_pagination_links(object):
     def __call__(self, f):
@@ -46,62 +47,64 @@ class add_pagination_links(object):
                 elif key == "pagination":
                     pagination = value
                 elif key in collections_to_resource_type.keys():
-                    endpoint = "v1."+key+"."
+                    endpoint = "v1." + key + "."
                     endpoint += "id" if "id" in kwargs.keys() else "collection"
                 elif key in ["journeys", "stop_schedules", "route_schedules",
                              "departures", "arrivals", "places_nearby"]:
-                    endpoint = "v1."+key
+                    endpoint = "v1." + key
             if pagination and endpoint:
                 pagination = data["pagination"]
                 if "start_page" in pagination.keys() and \
                     "items_on_page" in pagination.keys() and \
                     "items_per_page" in pagination.keys() and \
-                    "total_result" in pagination.keys() :
-                        if not "links" in data.keys():
-                            data["links"] = []
-                        start_page = int(pagination["start_page"])
-                        items_per_page = int(pagination["items_per_page"])
-                        items_on_page = int(pagination["items_on_page"])
-                        total_result = int(pagination["total_result"])
-                        kwargs["_external"] = True
+                    "total_result" in pagination.keys():
+                    if not "links" in data.keys():
+                        data["links"] = []
+                    start_page = int(pagination["start_page"])
+                    items_per_page = int(pagination["items_per_page"])
+                    items_on_page = int(pagination["items_on_page"])
+                    total_result = int(pagination["total_result"])
+                    kwargs["_external"] = True
 
-                        if start_page > 0:
-                            kwargs["start_page"] = start_page - 1
+                    if start_page > 0:
+                        kwargs["start_page"] = start_page - 1
+                        data["links"].append({
+                            "href": url_for(endpoint, **kwargs),
+                            "type": "previous",
+                            "templated": False
+                        })
+                    nb_next_page = items_per_page * start_page
+                    nb_next_page += items_on_page
+                    if total_result > nb_next_page:
+                        kwargs["start_page"] = start_page + 1
+                        data["links"].append({
+                            "href": url_for(endpoint, **kwargs),
+                            "type": "next",
+                            "templated": False
+                        })
+                        if items_per_page == 0 or total_result == 0:
+                            kwargs["start_page"] = 0
+                        else:
+                            nb_last_page = total_result - 1
+                            nb_last_page = nb_last_page / items_per_page
+                            kwargs["start_page"] = nb_last_page
                             data["links"].append({
                                 "href": url_for(endpoint, **kwargs),
-                                "type": "previous",
-                                "templated" : False
-                                })
-
-                        if total_result > (items_per_page * start_page + items_on_page):
-                            kwargs["start_page"] = start_page + 1
-                            data["links"].append({
-                                "href": url_for(endpoint, **kwargs),
-                                "type": "next",
-                                "templated" : False
+                                "type": "last",
+                                "templated": False
                             })
-                        kwargs["start_page"] = 0 if items_per_page == 0 or total_result == 0 \
-                                              else (total_result-1) / items_per_page
-                        data["links"].append({
-                            "href": url_for(endpoint, **kwargs),
-                            "type": "last",
-                            "templated" : False
-                        })
 
-                        del kwargs["start_page"]
-                        data["links"].append({
-                            "href": url_for(endpoint, **kwargs),
-                            "type": "first",
-                            "templated" : False
-                        })
+                    del kwargs["start_page"]
+                    data["links"].append({
+                        "href": url_for(endpoint, **kwargs),
+                        "type": "first",
+                        "templated": False
+                    })
             if isinstance(objects, tuple):
                 return data, code, header
             else:
                 return data
         return wrapper
-
-
-
 
 
 class add_coverage_link(generate_links):
@@ -117,10 +120,10 @@ class add_coverage_link(generate_links):
                 data = objects
             if isinstance(data, OrderedDict):
                 data = self.prepare_objetcs(data)
-                kwargs  = self.prepare_kwargs(kwargs, data)
+                kwargs = self.prepare_kwargs(kwargs, data)
                 url = url_for("v1.coverage", _external=True, **kwargs)
-                data["links"].append({"href":url, "rel" : "related",
-                                             "templated":True})
+                data["links"].append({"href": url, "rel": "related",
+                                             "templated": True})
             if isinstance(objects, tuple):
                 return data, code, header
             else:
@@ -144,12 +147,12 @@ class add_collection_links(generate_links):
                 data = objects
             if isinstance(data, OrderedDict):
                 data = self.prepare_objetcs(objects, True)
-                kwargs  = self.prepare_kwargs(kwargs, data)
+                kwargs = self.prepare_kwargs(kwargs, data)
                 for collection in self.collections:
-                    url = url_for("v1."+collection+".collection",
+                    url = url_for("v1." + collection + ".collection",
                                     _external=True, **kwargs)
-                    data["links"].append({"href":url, "rel" : collection,
-                                             "templated":True})
+                    data["links"].append({"href": url, "rel": collection,
+                                             "templated": True})
             if isinstance(objects, tuple):
                 return data, code, header
             else:
@@ -173,12 +176,12 @@ class add_id_links(generate_links):
                 data = objects
             self.get_objets(data)
             data = self.prepare_objetcs(objects, True)
-            kwargs  = self.prepare_kwargs(kwargs, data)
+            kwargs = self.prepare_kwargs(kwargs, data)
             uri_id = None
             if "id" in kwargs.keys() and\
                "collection" in kwargs.keys() and \
                kwargs["collection"] in data.keys():
-                   uri_id = kwargs["id"]
+                uri_id = kwargs["id"]
             for obj in self.data:
                 if obj in resource_type_to_collection.keys():
                     kwargs["collection"] = resource_type_to_collection[obj]
@@ -186,14 +189,15 @@ class add_id_links(generate_links):
                     kwargs["collection"] = obj
                 if kwargs["collection"] in collections_to_resource_type.keys():
                     if not uri_id:
-                        kwargs["id"] = "{"+obj+".id}"
-                    endpoint = "v1."+kwargs["collection"]+"."
-                    endpoint += "id" if "region" in kwargs.keys() or "lon" in kwargs.keys()\
+                        kwargs["id"] = "{" + obj + ".id}"
+                    endpoint = "v1." + kwargs["collection"] + "."
+                    endpoint += "id" if "region" in kwargs.keys() or\
+                                            "lon" in kwargs.keys()\
                                         else "redirect"
                     del kwargs["collection"]
                     url = url_for(endpoint, _external=True, **kwargs)
-                    data["links"].append({"href":url, "rel" : obj,
-                                             "templated":True})
+                    data["links"].append({"href": url, "rel": obj,
+                                             "templated": True})
             if isinstance(objects, tuple):
                 return data, code, header
             else:
@@ -206,7 +210,7 @@ class add_id_links(generate_links):
             if "id" in data.keys() \
                and (not "href" in data.keys()) \
                and collection_name:
-                   self.data.add(collection_name)
+                self.data.add(collection_name)
             for key, value in data.iteritems():
                 c_name = key
                 self.get_objets(value, c_name)
@@ -222,7 +226,7 @@ class clean_links(object):
             response = f(*args, **kwargs)
             if isinstance(response, tuple):
                 data, code, header = unpack(response)
-                if code!=200:
+                if code != 200:
                     return data, code, header
             else:
                 data = response
