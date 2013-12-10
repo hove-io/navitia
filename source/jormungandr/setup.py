320ab9fc731cac57880367326c6329abdbce0f65
Vincent Lara <lara.vincent@gmail.com>
2013-12-10 11:25:33 +0100

    Jormungandr: Really fixing setup.py

--- source/jormungandr/setup.py
+++ source/jormungandr/setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(name='jormungandr',
         version='0.10.0',
-        description='webservice d'\exposition en http de kraken',
+        description='webservice d\'exposition en http de kraken',
         author='CanalTP',
         author_email='vincent.lara@canaltp.fr',
         url='www.navitia.io',
