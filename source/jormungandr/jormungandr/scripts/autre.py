4aa0a8467c498a417b8b50256e6dff1708bf5d76
Vincent Lara <lara.vincent@gmail.com>
2013-12-10 12:32:02 +0100

    Jormungandr: scripts/autre.py is now pep8 compliant

--- source/jormungandr/jormungandr/scripts/autre.py
+++ source/jormungandr/jormungandr/scripts/autre.py
@@ -4,11 +4,13 @@ import request_pb2
 import response_pb2
 from protobuf_to_dict import protobuf_to_dict
 
-from instance_manager import InstanceManager, DeadSocketException, RegionNotFound
+from instance_manager import InstanceManager, DeadSocketException,\
+                             RegionNotFound
 from renderers import render, render_from_protobuf
 from werkzeug.wrappers import Response
 from find_extrem_datetimes import *
 
+
 class Script(default.Script):
-    def on_index(requestion, version = None, region = None):
+    def on_index(requestion, version=None, region=None):
         return Response('Mooooooooooooooo')
