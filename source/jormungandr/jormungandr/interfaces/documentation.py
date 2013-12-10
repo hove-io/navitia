3f90319a2215aedd7d728d9c1e034d3c94cf8886
Vincent Lara <lara.vincent@gmail.com>
2013-12-10 16:47:07 +0100

    Jormungandr: interfaces/documentation.py pep8 compliance

--- source/jormungandr/jormungandr/interfaces/documentation.py
+++ source/jormungandr/jormungandr/interfaces/documentation.py
@@ -2,8 +2,12 @@ from flask.ext.restful import Resource
 from flask import current_app, request, abort, url_for
 import inspect
 import re
+
+
 class Documentation(Resource):
+
     """ Make the documentation """
+
     def __init__(self, prefix="", api_version=0, *args, **kwargs):
         super(Documentation, self).__init__(*args, **kwargs)
         self.prefix = prefix
@@ -12,42 +16,42 @@ class Documentation(Resource):
         self.base_path = None
         self.method_decorators = []
 
-
     def get(self, api=None):
         if api is None:
             return self.index()
         else:
             return self.get_api(api)
-        return {"endpoint" : api}
-
+        return {"endpoint": api}
 
     def get_api(self, api):
         response = {
-            "apiVersion" : repr(self.api_version),
+            "apiVersion": repr(self.api_version),
             "swaggerVersion": "1.2",
-            "basePath" : request.url_root,
-            "resourcePath" : "/" + api,
-            "apis" : []
+            "basePath": request.url_root,
+            "resourcePath": "/" + api,
+            "apis": []
         }
         rule_list = []
-        for endpoint, rules in current_app.url_map._rules_by_endpoint.iteritems():
+        rules = current_app.url_map._rules_by_endpoint
+        for endpoint, rules in rules.iteritems():
             splitted_endpoint = endpoint.split(".")
-            if (not self.prefix or\
+            if (not self.prefix or
                     (splitted_endpoint[0] == self.prefix)) and\
                     splitted_endpoint[1] == str(api) and\
                     (len(splitted_endpoint) < 3 or
-                    splitted_endpoint[2] != 'redirect'):
+                     splitted_endpoint[2] != 'redirect'):
                 for rule in rules:
                     rule_list.append((rule, endpoint))
         if len(rule_list) == 0:
-            abort(404, {"message" : "api not found"})
+            abort(404, {"message": "api not found"})
         response["apis"] = self.get_endpoint(rule_list)
         return response
 
     def get_path(self, rule):
         path = rule.rule
         for argument in rule.arguments:
-            path = re.sub(r'<(\w+):'+argument+'>', '{'+argument+'}', path)
+            path = re.sub(r'<(\w+):' + argument + '>', '{' + argument + '}',
+                          path)
         return path
 
     def get_parameters(self, rule, parser):
@@ -60,42 +64,40 @@ class Documentation(Resource):
             if 'type_' in dir(converter):
                 type_ = converter.type_
             parameters.append({
-                "paramType" : "path",
-                "name" : name,
-                "description" : description,
-                "type" : type_,
-                "required" : True,
+                "paramType": "path",
+                "name": name,
+                "description": description,
+                "type": type_,
+                "required": True,
                 "dataType": "string"
-                            })
+            })
         for argument in parser.args:
             if "description" in dir(argument) and\
                     argument.description and\
                     not argument.hidden:
-                param = { "paramType": "query",
-                          "name" : argument.name,
-                          "dataType" : "string",
-                          "required" : argument.required,
-                          "allowMultiple" : argument.action=="append",
-                          "description" : argument.description,
-                        }
+                param = {"paramType": "query",
+                         "name": argument.name,
+                         "dataType": "string",
+                         "required": argument.required,
+                         "allowMultiple": argument.action == "append",
+                         "description": argument.description,
+                         }
                 if len(argument.choices) > 0:
                     values = [list(i) for i in argument.choices]
                     param["allowableValues"] = {
                         "valueType": "LIST",
-                        "values" : values
+                        "values": values
                     }
                 parameters.append(param)
         return parameters
-
-
 
     def get_endpoint(self, rules):
         apis = []
         for (rule, endpoint) in rules:
             path = self.get_path(rule)
-            api = {"path" : path,
-                    "operations" : []
-            }
+            api = {"path": path,
+                   "operations": []
+                   }
             for method in rule.methods:
                 if not method in ['OPTIONS', 'HEAD']:
                     view_func = current_app.view_functions[endpoint]
@@ -111,24 +113,23 @@ class Documentation(Resource):
                     if func.__doc__:
                         doc = func.__doc__.splitlines()
                         summary = doc[0]
-                        notes = "" if len(doc)==1\
-                                   else func.__doc__[len(summary)+1:]
+                        notes = "" if len(doc) == 1\
+                            else func.__doc__[len(summary) + 1:]
                     api["operations"].append({
-                        "httpMethod" : method.upper(),
-                        "summary" : summary,
-                        "notes" : notes,
-                        "nickname" : endpoint.replace(".", "_")+method,
-                        "parameters" : parameters
-                        })
+                        "httpMethod": method.upper(),
+                        "summary": summary,
+                        "notes": notes,
+                        "nickname": endpoint.replace(".", "_") + method,
+                        "parameters": parameters
+                    })
             apis.append(api)
         return apis
 
-
     def index(self):
         response = {
-            "apiVersion" : repr(self.api_version),
+            "apiVersion": repr(self.api_version),
             "swaggerVersion": "1.2",
-            "apis" : []
+            "apis": []
         }
         apis_names = []
         for rule in current_app.url_map.iter_rules():
@@ -138,18 +139,18 @@ class Documentation(Resource):
             api_name = splitted_endpoint[1]
             if rule.rule[:len(request.url_rule.rule)] != request.url_rule.rule:
                 splitted_endpoint = rule.endpoint.split(".")
-                if not self.prefix or(\
+                if not self.prefix or(
                    splitted_endpoint[0] == self.prefix) and\
-                   not api_name in apis_names:
+                        not api_name in apis_names:
                     apis_names.append(api_name)
                     func = current_app.view_functions[rule.endpoint]
                     description = None
                     if "view_class" in func.func_dict.keys() and\
-                       func.func_dict['view_class'].__doc__ != None:
+                       func.func_dict['view_class'].__doc__ is not None:
                         description = func.func_dict['view_class'].__doc__
                         description = description.splitlines()[0]
                     url = "/" + splitted_endpoint[1]
-                    api = { "path" : url}
+                    api = {"path": url}
                     if description:
                         api["description"] = description
                     response["apis"].append(api)
@@ -157,10 +158,12 @@ class Documentation(Resource):
 
 
 class V0Documentation(Documentation):
-    def __init__(self, prefix= None, *args, **kwargs):
+
+    def __init__(self, prefix=None, *args, **kwargs):
         super(V0Documentation, self).__init__(*args, **kwargs)
         self.prefix = "v0"
         self.api_version = 0
+
 
 def v0_documentation(api):
     api.add_resource(V0Documentation,
@@ -170,10 +173,12 @@ def v0_documentation(api):
 
 
 class V1Documentation(Documentation):
-    def __init__(self, prefix= None, *args, **kwargs):
+
+    def __init__(self, prefix=None, *args, **kwargs):
         super(V1Documentation, self).__init__(*args, **kwargs)
         self.prefix = "v1"
         self.api_version = 1
+
 
 def v1_documentation(api):
     api.add_resource(V1Documentation,
