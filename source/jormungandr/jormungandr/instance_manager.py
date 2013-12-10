af28776b286edf4a1bfa9c718000a6b454a8018f
Vincent Lara <lara.vincent@gmail.com>
2013-12-10 17:01:04 +0100

    Jormungandr: instance_manager.py pep8 compliance

--- source/jormungandr/jormungandr/instance_manager.py
+++ source/jormungandr/jormungandr/instance_manager.py
@@ -3,29 +3,31 @@ from shapely import geometry, geos, wkt
 import ConfigParser
 import zmq
 from threading import Thread, Event
-import type_pb2
-import request_pb2
+from . import type_pb2
+from . import request_pb2
 import glob
-from singleton import singleton
-import importlib
+from .singleton import singleton
+from importlib import import_module
 import logging
-from renderers import protobuf_to_dict
+from .renderers import protobuf_to_dict
 from jormungandr.exceptions import ApiNotFound, RegionNotFound
-from app import app
-from instance import Instance
+from .app import app
+from .instance import Instance
+
 
 @singleton
 class InstanceManager(object):
+
     """ Permet de coordonner les différents NAViTiA gérés par le front-end
     Un identifiant correspond à une socket ZMQ Navitia où se connecter
     Plusieurs identifiants peuvent se connecter à une même socket
 
-    De plus il est possible de définir la zone géographique (au format WKT) couverte par un identifiant
+    De plus il est possible de définir la zone géographique (au format WKT)
+    couverte par un identifiant
     """
 
     def __init__(self):
         pass
-
 
     def initialisation(self, start_ping=True):
         """ Charge la configuration à partir d'un fichier ini indiquant
@@ -46,19 +48,20 @@ class InstanceManager(object):
             logging.info("Initialisation, reading file : " + file_name)
             conf = ConfigParser.ConfigParser()
             conf.read(file_name)
-            instance = Instance(self.context, conf.get('instance' , 'key'))
-            instance.socket_path = conf.get('instance' , 'socket')
-            if conf.has_option('instance', 'script') :
-                instance.script = importlib.import_module(conf.get('instance','script')).Script()
+            instance = Instance(self.context, conf.get('instance', 'key'))
+            instance.socket_path = conf.get('instance', 'socket')
+            if conf.has_option('instance', 'script'):
+                module = import_module(conf.get('instance', 'script'))
+                instance.script = module.Script()
             else:
-                instance.script = importlib.import_module("jormungandr.scripts.default").Script()
+                module = import_module("jormungandr.scripts.default")
+                instance.script = module.Script()
 
-            self.instances[conf.get('instance' , 'key')] = instance
+            self.instances[conf.get('instance', 'key')] = instance
         self.thread_event = Event()
-        self.thread = Thread(target = self.thread_ping)
+        self.thread = Thread(target=self.thread_ping)
         if start_ping:
             self.thread.start()
-
 
     def dispatch(self, arguments, region, api, request=None):
         if region in self.instances:
@@ -69,7 +72,6 @@ class InstanceManager(object):
                 raise ApiNotFound(api)
         else:
             raise RegionNotFound(region)
-
 
     def thread_ping(self, timer=1):
         req = request_pb2.Request()
@@ -87,17 +89,15 @@ class InstanceManager(object):
                     instance.geom = None
             self.thread_event.wait(timer)
 
-
-    def stop(self) :
+    def stop(self):
         self.thread_event.set()
-
 
     def key_of_id(self, object_id):
         """ Retrieves the key of the region of a given id
             if it's a coord calls key_of_coord
             Return the region key, or None if it doesn't exists
         """
-#Il s'agit d'une coordonnée
+# Il s'agit d'une coordonnée
         if object_id.count(";") == 1 or object_id[:6] == "coord:":
             if object_id.count(";") == 1:
                 lon, lat = object_id.split(";")
@@ -120,19 +120,18 @@ class InstanceManager(object):
                 return contributor
         raise RegionNotFound(object_id=object_id)
 
-
     def key_of_coord(self, lon, lat):
-        """ Étant donné une coordonnée, retourne à quelle clef NAViTiA cela correspond
+        """ Étant donné une coordonnée, retourne à quelle clef NAViTiA cela
+        correspond
 
         Retourne None si on a rien trouvé
         """
-        p = geometry.Point(lon,lat)
+        p = geometry.Point(lon, lat)
         for key, instance in self.instances.iteritems():
             if instance.geom and instance.geom.contains(p):
                 return key
 
         raise RegionNotFound(lon=lon, lat=lat)
-
 
     def is_region_exists(self, region_str):
         if (region_str in self.instances.keys()):
@@ -140,8 +139,7 @@ class InstanceManager(object):
         else:
             raise RegionNotFound(region=region_str)
 
-
-    def get_region(self, region_str=None, lon=None, lat=None, object_id = None):
+    def get_region(self, region_str=None, lon=None, lat=None, object_id=None):
         if region_str and self.is_region_exists(region_str):
             return region_str
         elif lon and lat:
@@ -152,18 +150,19 @@ class InstanceManager(object):
             raise RegionNotFound(region=region_str, lon=lon, lat=lat,
                                  object_id=object_id)
 
-
     def regions(self, region=None, lon=None, lat=None):
         response = {'regions': []}
         regions = []
         if region or lon or lat:
-            regions.append(self.get_region(region_str=region, lon=lon, lat=lat))
+            regions.append(self.get_region(region_str=region, lon=lon,
+                                           lat=lat))
         else:
             regions = self.instances.keys()
-        for key_region in regions :
+        for key_region in regions:
             req = request_pb2.Request()
             req.requested_api = type_pb2.METADATAS
-            resp = self.instances[key_region].send_and_receive(req, timeout=2000)
+            resp = self.instances[key_region].send_and_receive(req,
+                                                               timeout=2000)
             resp_dict = protobuf_to_dict(resp)
             if 'metadatas' in resp_dict.keys():
                 resp_dict['metadatas']['region_id'] = key_region
