db00ba7955552c7f67e3ef5c1ac09ef2597c58d0
Vincent Lara <lara.vincent@gmail.com>
2013-12-10 17:13:02 +0100

    Jormungandr: app.py pep8 compliance

--- source/jormungandr/jormungandr/app.py
+++ source/jormungandr/jormungandr/app.py
@@ -1,26 +1,26 @@
-#encoding: utf-8
+# encoding: utf-8
 from flask import Flask, got_request_exception
 from flask.ext.restful import Api
 from logging.handlers import RotatingFileHandler, TimedRotatingFileHandler
-from exceptions import log_exception
+from .exceptions import log_exception
 app = Flask(__name__)
 app.config.from_object('jormungandr.default_settings')
 app.config.from_envvar('JORMUNGANDR_CONFIG_FILE')
 
-if app.config.has_key('ERROR_HANDLER_TYPE') and\
-        app.config.has_key('ERROR_HANDLER_FILE'):
+if 'ERROR_HANDLER_TYPE' in app.config and\
+        'ERROR_HANDLER_FILE' in app.config:
     handler = None
     params = {} if not 'ERROR_HANDLER_PARAMS' in app.config\
-                else app.config['ERROR_HANDLER_PARAMS']
+        else app.config['ERROR_HANDLER_PARAMS']
     file_ = app.config['ERROR_HANDLER_FILE']
     if app.config['ERROR_HANDLER_TYPE'] == 'rotating':
-        handler = RotatingFileHandler(file_ , **params)
+        handler = RotatingFileHandler(file_, **params)
     if app.config['ERROR_HANDLER_TYPE'] == 'timedrotating':
         handler = TimedRotatingFileHandler(file_, **params)
     elif app.config['ERROR_HANDLER_FILE'] == 'file':
         handler = FileHandler(file_, **params)
     if handler:
-        if app.config.has_key('ERROR_HANDLER_LEVEL'):
+        if 'ERROR_HANDLER_LEVEL' in app.config:
             handler.setLevel(app.config['ERROR_HANDLER_LEVEL'])
         app.logger.addHandler(handler)
 
