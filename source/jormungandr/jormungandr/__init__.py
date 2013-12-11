24b86a55474c5c5ec1feb9344f935063fc7ddfea
Vincent Lara <lara.vincent@gmail.com>
2013-12-11 13:51:35 +0100

    Jormungandr: we now import app in jormungandr

--- source/jormungandr/jormungandr/__init__.py
+++ source/jormungandr/jormungandr/__init__.py
@@ -1,5 +1,5 @@
 #__all__ = ['interfaces', 'instance_manager', 'jormungandr_exceptions',
 #           'request_pb2']
 
-#__all__ = ['instance_manager']
 from jormungandr.instance_manager import InstanceManager
+from jormungandr.app import app
