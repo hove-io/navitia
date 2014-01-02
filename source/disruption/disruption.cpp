2c52a19a2da041c0c2932d90981d3029b46de2b5
Abderrahim Azime <abderrahim.azime@canaltp.fr>
2014-01-02 09:48:31 +0100

    kraken : amélioration de recherche
    des lignes et réseaux ayant des messages

--- source/disruption/disruption.cpp
+++ source/disruption/disruption.cpp
@@ -11,12 +11,6 @@ network_line disruptions_list(const std::vector<type::idx_t>& line_idx, const ty
         auto find_predicate = [&](pair_network_line ntw_list) {
             return ntw_list.first->idx == line->network->idx;
         };
-        if (line->network->has_applicable_message(now, action_period)){
-            auto it = std::find_if(response.begin(), response.end(), find_predicate);
-            if(it == response.end()){
-                response.push_back(std::make_pair(line->network, std::vector<const navitia::type::Line*> {}));
-            }
-        }
         if (line->has_applicable_message(now, action_period)){
             auto it = std::find_if(response.begin(), response.end(), find_predicate);
             if(it == response.end()){
@@ -24,6 +18,13 @@ network_line disruptions_list(const std::vector<type::idx_t>& line_idx, const ty
             }else{
                     it->second.push_back(line);
                 }
+        }else{
+            if (line->network->has_applicable_message(now, action_period)){
+                auto it = std::find_if(response.begin(), response.end(), find_predicate);
+                if(it == response.end()){
+                    response.push_back(std::make_pair(line->network, std::vector<const navitia::type::Line*> {}));
+                }
+            }
         }
     }
     return response;
