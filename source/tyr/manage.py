1e4ffbf3bf080464559d573aee65e05265132f27
Alexandre Jacquin <alexandre.jacquin@canaltp.fr>
2014-01-13 08:01:16 +0100

    Tyr: add migration with flask-migrate (based on alembic)
    addition of a requirements.txt for the dependancies

--- source/tyr/manage.py
+++ source/tyr/manage.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python
 # coding=utf-8
-from tyr import app
+from tyr import app, db
 import sys
 from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 
 
 manager = Manager(app)
+
+migrate = Migrate(app, db)
+manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
     app.config['DEBUG'] = True
