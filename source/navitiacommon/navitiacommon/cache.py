fb4830f7bc87184e36b40d7a6d25bde122bfb1ee
Vincent Lara <lara.vincent@gmail.com>
2014-02-10 12:39:37 +0100

    Navitiacommon: add prefix to cache

--- source/navitiacommon/navitiacommon/cache.py
+++ source/navitiacommon/navitiacommon/cache.py
@@ -1,37 +1,37 @@
 # encoding: utf-8
 
-from redis import Redis
 import cPickle
-
 
 
 class Cache(object):
 
     def __init__(self, host='localhost', port=6379, db=0,
-                 password=None, disabled=False):
+                 password=None, disabled=False, separator="."):
         self._disabled = disabled
+        self.separator = separator
         if not disabled:
+            from redis import Redis
             self._redis = Redis(host=host, port=port, db=db, password=password)
         else:
             self._redis = None
 
-    def get(self, key):
+    def get(self, prefix, key):
         if self._disabled:
             return None
 
-        cached = self._redis.get(key)
+        cached = self._redis.get("%s%s%s" % (prefix, self.separator, key))
         if cached:
             return cPickle.loads(cached)
         else:
             return None
 
-    def set(self, key, obj, ttl=None):
+    def set(self, prefix, key, obj, ttl=None):
+        complete_key = "%s%s%s" % (prefix, self.separator, key)
         if self._disabled:
             return
-
-        self._redis.set(key, cPickle.dumps(obj))
+        self._redis.set(complete_key, cPickle.dumps(obj))
         if ttl:
-            self._redis.expire(key, ttl)
+            self._redis.expire(complete_key, ttl)
 
 
 cache = Cache(disabled=True)
