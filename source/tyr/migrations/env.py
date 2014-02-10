963ffaa090c8ae245cabefe5f2d85bd830bc144b
Vincent Lara <lara.vincent@gmail.com>
2014-02-10 12:38:19 +0100

    Alembic: some config to exclude tables from migration

--- source/tyr/migrations/env.py
+++ source/tyr/migrations/env.py
@@ -24,6 +24,26 @@ target_metadata = current_app.extensions['migrate'].db.metadata
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
 
+
+def exclude_tables_from_config(config_):
+    tables_ = config_.get("tables", None)
+    if tables_ is not None:
+        tables = tables_.split(",")
+    return tables
+
+exclude_tables = exclude_tables_from_config(config.get_section('alembic:exclude'))
+
+def include_object(object, name, type_, reflected, compare_to):
+    if type_ == "table" and name in exclude_tables:
+        return False
+    else:
+        return True
+
+def include_object(object, name, type_, reflected, compare_to):
+    if type_ == "table" and name in exclude_tables:
+        return False
+    else:
+        return True
 def run_migrations_offline():
     """Run migrations in 'offline' mode.
 
@@ -57,7 +77,8 @@ def run_migrations_online():
     connection = engine.connect()
     context.configure(
                 connection=connection,
-                target_metadata=target_metadata
+                target_metadata=target_metadata,
+                include_object=include_object
                 )
 
     try:
@@ -70,4 +91,3 @@ if context.is_offline_mode():
     run_migrations_offline()
 else:
     run_migrations_online()
-
