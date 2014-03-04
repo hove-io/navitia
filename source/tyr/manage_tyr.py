bf5e598f777d596ef5cd90c4b7fa6a579858a499
Vincent Lara <lara.vincent@gmail.com>
2014-03-04 14:44:38 +0100

    Tyr: add aggregate_places command
    Launch aggregate places.
    It can take a name argument to specify on which instance we want
    to aggregate places.

--- source/tyr/manage_tyr.py
+++ source/tyr/manage_tyr.py
@@ -4,12 +4,13 @@ from tyr import app, db
 import sys
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
-
+from tyr.aggregate_places import AggregatePlacesCommand
 
 manager = Manager(app)
 
 migrate = Migrate(app, db)
 manager.add_command('db', MigrateCommand)
+manager.add_command('aggregate_places', AggregatePlacesCommand())
 
 if __name__ == '__main__':
     app.config['DEBUG'] = True
