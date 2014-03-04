83147a39d3f7394c1df3187eb2f2413633d87d8b
Vincent Lara <lara.vincent@gmail.com>
2014-03-04 16:34:55 +0100

    Kraken: speed of a bike is now 20km/h (i.e 5.6m/s)

--- source/georef/georef.h
+++ source/georef/georef.h
@@ -22,9 +22,9 @@ namespace navitia { namespace georef {
 const flat_enum_map<nt::Mode_e, float> default_speed {
                                                     {{
                                                         1.38f, //nt::Mode_e::Walking
-                                                        8.8f, //nt::Mode_e::Bike
+                                                        5.6f, //nt::Mode_e::Bike
                                                         16.8, //nt::Mode_e::Car
-                                                        8.8f //nt::Mode_e::Vls
+                                                        5.6f //nt::Mode_e::Vls
                                                     }}
                                                     };
 
