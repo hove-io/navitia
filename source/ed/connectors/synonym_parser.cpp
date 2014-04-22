549bd6fc83fae50570b03f6ec83053b5a79e381e
Abderrahim Azime <abderrahim.azime@canaltp.fr>
2014-04-22 12:08:08 +0200

    ed : add synonym2ed connector

---
+++ source/ed/connectors/synonym_parser.cpp
@@ -0,0 +1,44 @@
+#include "synonym_parser.h"
+#include <boost/lexical_cast.hpp>
+#include "utils/csv.h"
+#include "utils/configuration.h"
+#include <boost/algorithm/string.hpp>
+
+
+namespace ed{ namespace connectors{
+
+SynonymParser::SynonymParser(const std::string & path): path(path){
+        logger = log4cplus::Logger::getInstance("log");
+}
+
+void SynonymParser::fill_synonyms(){
+    std::string key, value;
+    CsvReader reader(this->path + "/synonyms.txt", ',', true, true);
+    if(!reader.is_open()) {
+       throw SynonymParserException("Cannot open file : " + reader.filename);
+    }
+    std::vector<std::string> mandatory_headers = {"key" , "value"};
+    if(!reader.validate(mandatory_headers)) {
+        throw SynonymParserException("Impossible to parse file "
+                                    + reader.filename + " . Not find column : "
+                                    + reader.missing_headers(mandatory_headers));
+    }
+
+    int key_c = reader.get_pos_col("key"), value_c = reader.get_pos_col("value");
+    while(!reader.eof()) {
+        auto row = reader.next();
+        if (reader.is_valid(key_c, row) && reader.has_col(value_c, row)){
+                key = row[key_c];
+                value = row[value_c];
+                boost::to_lower(key);
+                boost::to_lower(value);
+                this->synonym_map[key]=value;
+            }
+    }
+    LOG4CPLUS_TRACE(logger, "synonyms : " + this->synonym_map.size());
+}
+
+void SynonymParser::fill(){
+    this->fill_synonyms();
+}
+}}
---
+++ source/ed/connectors/synonym_parser.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <string>
+#include <utils/logger.h>
+#include <utils/exception.h>
+
+namespace ed{ namespace connectors{
+struct SynonymParserException: public navitia::exception{
+    SynonymParserException(const std::string& message): navitia::exception(message){};
+};
+
+class SynonymParser{
+private:
+    log4cplus::Logger logger;
+    std::string path;
+    void fill_synonyms();
+
+public:
+    std::map<std::string, std::string> synonym_map;
+
+    SynonymParser(const std::string & path);
+
+    void fill();
+};
+}}
