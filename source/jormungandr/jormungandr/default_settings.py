d1ada7f0cb677499425be2765623f622b489cc3b
kadhikari <krishna.adhikari@canaltp.fr>
2014-05-12 11:03:39 +0200

    jormungandr: SAVE_STAT deactivated.

--- source/jormungandr/jormungandr/default_settings.py
+++ source/jormungandr/jormungandr/default_settings.py
@@ -57,7 +57,7 @@ LOGGER = {
 }
 
 #Paramètres pour les statistiques
-SAVE_STAT = True
+SAVE_STAT = False
 BROKER_URL = 'amqp://guest:guest@localhost:5672//'
 EXCHANGE_NAME='stat_persistor_exchange'
 SQLALCHEMY_DATABASE_STAT = 'postgresql://navitia:navitia@localhost/navitia'
