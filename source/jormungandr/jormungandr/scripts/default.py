a787c8d38c7bfba31dbe360e00f2a0b4c0a2fa5d
Vincent Lara <lara.vincent@gmail.com>
2014-05-13 16:48:05 +0200

    Jormungandr: Update comment on choose best

--- source/jormungandr/jormungandr/scripts/default.py
+++ source/jormungandr/jormungandr/scripts/default.py
@@ -514,7 +514,7 @@ class Script(object):
     def choose_best(self, resp):
         """
         prerequisite: Journeys has to be sorted before
-        the best journey, is the first rapid, ie the rapid without additional tags ('prev' or 'next')
+        the best journey is the first rapid
         """
         for j in resp.journeys:
             if j.type == 'rapid':
