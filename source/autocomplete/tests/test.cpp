e020d70f45e06a25c53f7da5681419afca3e047f
kadhikari <krishna.adhikari@canaltp.fr>
2014-05-15 15:25:15 +0200

    autocomplete: tests added for duplicate words

--- source/autocomplete/tests/test.cpp
+++ source/autocomplete/tests/test.cpp
@@ -187,18 +187,18 @@ BOOST_AUTO_TEST_CASE(regex_toknize_tests){
     //synonyme : "cc" = "centre commercial" / synonym : de = ""
     //"cc Carré de Soie" -> "centre commercial carré de soie"
     vec = ac.tokenize("cc Carré de Soie", synonyms);
-    BOOST_CHECK_EQUAL(vec[0], "centre");
-    BOOST_CHECK_EQUAL(vec[1], "commercial");
-    BOOST_CHECK_EQUAL(vec[2], "carre");
+    BOOST_CHECK_EQUAL(vec[0], "carre");
+    BOOST_CHECK_EQUAL(vec[1], "centre");
+    BOOST_CHECK_EQUAL(vec[2], "commercial");
     BOOST_CHECK_EQUAL(vec[3], "soie");
 
     vec.clear();
     //synonyme : "c c"= "centre commercial" / synonym : de = ""
     //"c c Carré de Soie" -> "centre commercial carré de soie"
     vec = ac.tokenize("c c Carré de Soie", synonyms);
-    BOOST_CHECK_EQUAL(vec[0], "centre");
-    BOOST_CHECK_EQUAL(vec[1], "commercial");
-    BOOST_CHECK_EQUAL(vec[2], "carre");
+    BOOST_CHECK_EQUAL(vec[0], "carre");
+    BOOST_CHECK_EQUAL(vec[1], "centre");
+    BOOST_CHECK_EQUAL(vec[2], "commercial");
     BOOST_CHECK_EQUAL(vec[3], "soie");
 }
 
@@ -590,3 +590,106 @@ BOOST_AUTO_TEST_CASE(autocompletesynonym_and_weight_test){
         // Qualité = 100 - (9 + 10) = 81
         BOOST_REQUIRE_EQUAL(res4.at(0).quality, 81);
     }
+
+BOOST_AUTO_TEST_CASE(autocomplete_duplicate_words_and_weight_test){
+
+    autocomplete_map synonyms;
+    std::vector<std::string> admins;
+    std::string admin_uri;
+    int word_weight = 5;
+    int nbmax = 10;
+
+    synonyms["de"]="";
+    synonyms["la"]="";
+    synonyms["le"]="";
+    synonyms["les"]="";
+    synonyms["des"]="";
+    synonyms["d"]="";
+    synonyms["l"]="";
+
+    synonyms["st"]="saint";
+    synonyms["ste"]="sainte";
+    synonyms["cc"]="centre commercial";
+    synonyms["chu"]="hopital";
+    synonyms["chr"]="hopital";
+    synonyms["c.h.u"]="hopital";
+    synonyms["c.h.r"]="hopital";
+    synonyms["all"]="allée";
+    synonyms["allee"]="allée";
+    synonyms["ave"]="avenue";
+    synonyms["av"]="avenue";
+    synonyms["bvd"]="boulevard";
+    synonyms["bld"]="boulevard";
+    synonyms["bd"]="boulevard";
+    synonyms["b"]="boulevard";
+    synonyms["r"]="rue";
+    synonyms["pl"]="place";
+
+    Autocomplete<unsigned int> ac;
+    ac.add_string("gare de Tours Tours", 0, synonyms);
+    ac.add_string("Garennes-sur-Eure", 1, synonyms);
+    ac.add_string("gare d'Orléans Orléans", 2, synonyms);
+    ac.add_string("gare de Bourges Bourges", 3, synonyms);
+    ac.add_string("Gare SNCF Blois", 4, synonyms);
+    ac.add_string("gare de Dreux Dreux", 5, synonyms);
+    ac.add_string("gare de Lucé Lucé", 6, synonyms);
+    ac.add_string("gare de Vierzon Vierzon", 7, synonyms);
+    ac.add_string("Les Sorinières, Les Sorinières", 8, synonyms);
+    ac.add_string("Les Sorinières 44840", 9, synonyms);
+    ac.add_string("Rouet, Les Sorinières", 10, synonyms);
+    ac.add_string("Ecoles, Les Sorinières", 11, synonyms);
+    ac.add_string("Prières, Les Sorinières", 12, synonyms);
+    ac.add_string("Calvaire, Les Sorinières", 13, synonyms);
+    ac.add_string("Corberie, Les Sorinières", 14, synonyms);
+    ac.add_string("Le Pérou, Les Sorinières", 15, synonyms);
+    ac.add_string("Les Sorinières, Villebernier", 16, synonyms);
+    ac.add_string("Les Faulx, Les Sorinières", 17, synonyms);
+    ac.add_string("cimetière, Les Sorinières", 18, synonyms);
+
+    ac.build();
+
+    auto res = ac.find_complete("gare", synonyms, word_weight, nbmax, [](int){return true;});
+    BOOST_REQUIRE_EQUAL(res.size(), 8);
+    BOOST_REQUIRE_EQUAL(res.at(0).quality, 91);
+    BOOST_REQUIRE_EQUAL(res.at(0).idx, 6);//gare de Lucé Lucé
+    BOOST_REQUIRE_EQUAL(res.at(1).idx, 5);//gare de Dreux Dreux
+    BOOST_REQUIRE_EQUAL(res.at(2).idx, 0);//gare de Tours Tours
+    BOOST_REQUIRE_EQUAL(res.at(3).idx, 2);//gare d'Orléans Orléans
+    BOOST_REQUIRE_EQUAL(res.at(4).idx, 3);//gare de Bourges Bourges
+    BOOST_REQUIRE_EQUAL(res.at(5).idx, 7);//gare de Vierzon Vierzon
+    BOOST_REQUIRE_EQUAL(res.at(6).idx, 4);//Gare SNCF Blois
+    BOOST_REQUIRE_EQUAL(res.at(7).idx, 1);//Garennes-sur-Eure
+    BOOST_REQUIRE_EQUAL(res.at(7).quality, 79);
+
+
+    auto res1 = ac.find_complete("gare tours", synonyms, word_weight, nbmax, [](int){return true;});
+    BOOST_REQUIRE_EQUAL(res1.size(), 1);
+    BOOST_REQUIRE_EQUAL(res1.at(0).quality, 100);
+
+    auto res2 = ac.find_complete("gare tours tours", synonyms, word_weight, nbmax, [](int){return true;});
+    BOOST_REQUIRE_EQUAL(res1.size(), 1);
+    BOOST_REQUIRE_EQUAL(res1.at(0).quality, 100);
+
+    auto res3 = ac.find_complete("les Sorinières", synonyms, word_weight, nbmax,[](int){return true;});
+    BOOST_REQUIRE_EQUAL(res3.size(), 10);
+    BOOST_REQUIRE_EQUAL(res3.at(0).quality, 100);
+    BOOST_REQUIRE_EQUAL(res3.at(0).idx, 8); //Les Sorinières, Les Sorinières
+    BOOST_REQUIRE_EQUAL(res3.at(1).quality, 90);
+    BOOST_REQUIRE_EQUAL(res3.at(1).idx, 9); //Les Sorinières 44840
+    BOOST_REQUIRE_EQUAL(res3.at(2).quality, 90);
+    BOOST_REQUIRE_EQUAL(res3.at(2).idx, 15); //Le Pérou, Les Sorinières
+    BOOST_REQUIRE_EQUAL(res3.at(3).quality, 90);
+    BOOST_REQUIRE_EQUAL(res3.at(3).idx, 17); //Les Faulx, Les Sorinières
+    BOOST_REQUIRE_EQUAL(res3.at(4).quality, 90);
+    BOOST_REQUIRE_EQUAL(res3.at(4).idx, 10); //Rouet, Les Sorinières
+    BOOST_REQUIRE_EQUAL(res3.at(5).quality, 89);
+    BOOST_REQUIRE_EQUAL(res3.at(5).idx, 11); //Ecoles, Les Sorinières
+    BOOST_REQUIRE_EQUAL(res3.at(6).quality, 88);
+    BOOST_REQUIRE_EQUAL(res3.at(6).idx, 12); //Prières, Les Sorinières
+    BOOST_REQUIRE_EQUAL(res3.at(7).quality, 87);
+    BOOST_REQUIRE_EQUAL(res3.at(7).idx, 13); //Calvaire, Les Sorinières
+    BOOST_REQUIRE_EQUAL(res3.at(8).quality, 87);
+    BOOST_REQUIRE_EQUAL(res3.at(8).idx, 14);//Corberie, Les Sorinières
+    BOOST_REQUIRE_EQUAL(res3.at(9).quality, 86);
+    BOOST_REQUIRE_EQUAL(res3.at(9).idx, 18);//cimetière, Les Sorinières
+}
