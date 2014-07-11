e76345da6b15b2ba3847addcdd6f831f410650dd
Thomas Koch <thakoch@gmail.com>
2014-07-11 10:03:14 +0200

    Add human readable debug output

--- source/routing/single_run.cpp
+++ source/routing/single_run.cpp
@@ -32,12 +32,14 @@ www.navitia.io
 #include "routing/raptor_api.h"
 #include "type/data.h"
 #include "utils/timer.h"
+#include "type/response.pb.h"
 #include <boost/program_options.hpp>
 
 namespace nr = navitia::routing;
 namespace nt = navitia::type;
 namespace bt = boost::posix_time;
 namespace po = boost::program_options ;
+namespace pb = pbnavitia;
 
 int main(int argc, char** argv){
     po::options_description desc("Options du calculateur simple");
@@ -49,12 +51,14 @@ int main(int argc, char** argv){
             ("start,s", po::value<std::string>(&start), "uri of point to start")
             ("target,t", po::value<std::string>(&target), "uri of point to end")
             ("date,d", po::value<std::string>(&date), "yyyymmddThhmmss")
-            ("clockwise,c", po::value<std::string>(&date), "Clockwise search")
+            ("counterclockwise,c", "Counter-clockwise search")
+            ("protobuf,p", "Full-output")
             ("file,f", po::value<std::string>(&file)->default_value("Path to data.nav.lz4"));
 
     po::variables_map vm;
     po::store(po::parse_command_line(argc, argv, desc), vm);
     po::notify(vm);
+    bool clockwise = !vm.count("counterclockwise");
 
     if (vm.count("help")) {
         std::cout << desc << std::endl;
@@ -74,12 +78,52 @@ int main(int argc, char** argv){
         nt::Type_e destination_type = data.get_type_of_id(start);
         nt::EntryPoint origin(origin_type, start);
         nt::EntryPoint destination(destination_type, target);
+        pb::Response resp = make_response(raptor, origin, destination, {date}, clockwise, navitia::type::AccessibiliteParams()/*false*/, forbidden, sn_worker, false, true);
 
-        pbnavitia::Response resp = make_response(raptor, origin, destination, {date}, true, navitia::type::AccessibiliteParams()/*false*/, forbidden, sn_worker, false, true);
-        std::cout << resp.DebugString() << "\n";
+        if (vm.count("protobuf")){
+            std::cout << resp.DebugString() << "\n";
+        }else{
+            std::cout << "Response type: " <<  pb::ResponseStatus_Name(pb::ResponseStatus(resp.response_type())) << "\n";
+            if (resp.journeys_size() == 0){
+                std::cout << "No solutions found\n";
+            }
+            for (int i = 0; i < resp.journeys_size();i++){
+                auto journey = resp.journeys(i);
+                std::cout << "Journey " << journey.nb_transfers() << " transfers" << "\n";
+                for (int j = 0; j < journey.sections_size(); j++){
+                    auto section = journey.sections(j);
+                    std::cout << std::left << std::setw(18) << pb::SectionType_Name(pb::SectionType(section.type()));
+                    std::cout << std::left << std::setw(16) << section.begin_date_time();
+                    std::cout << std::left << std::setw(16) << section.end_date_time();
+                    if (section.has_transfer_type()){
+                        std::cout << std::left << std::setw(10) << pb::TransferType_Name(pb::TransferType(section.transfer_type()));
+                    }else if (section.has_pt_display_informations()){
+                        std::cout << std::left << std::setw(10) << section.pt_display_informations().physical_mode();
+                    }else{
+                        std::cout << std::left << std::setw(10) << "";
+                    }
+                    if (section.has_pt_display_informations()){
+                        std::cout << std::left << std::setw(10)
+                                  << section.pt_display_informations().code();
+                        std::cout << std::left << std::setw(20)
+                                  << section.pt_display_informations().headsign();
+                        std::cout << std::left << std::setw(10)
+                                  << section.pt_display_informations().network();
+                    }else{
+                        std::cout << std::left << std::setw(40) << "";
+                    }
+                    std::cout << std::left << std::setw(30) << section.origin().name();
+                    std::cout << section.destination().name();
+                    std::cout << std::endl;
+                }
+                std::cout << std::endl;
+            }
+        }
+        std::cout << std::endl;
         return 0;
     } else {
         std::cout << desc << std::endl;
         return 1;
     }
+
 }
