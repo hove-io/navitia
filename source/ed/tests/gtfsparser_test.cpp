31af17eb764d60956112750c0968626fcb0c05c4
Alexandre Jacquin <alexandre.jacquin@canaltp.fr>
2014-08-25 11:47:19 +0200

    Ed: add test for the last day of a validity pattern

--- source/ed/tests/gtfsparser_test.cpp
+++ source/ed/tests/gtfsparser_test.cpp
@@ -1,28 +1,28 @@
 /* Copyright © 2001-2014, Canal TP and/or its affiliates. All rights reserved.
-  
+
 This file is part of Navitia,
     the software to build cool stuff with public transport.
- 
+
 Hope you'll enjoy and contribute to this project,
     powered by Canal TP (www.canaltp.fr).
 Help us simplify mobility and open public transport:
     a non ending quest to the responsive locomotion way of traveling!
-  
+
 LICENCE: This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU Affero General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
-   
+
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 GNU Affero General Public License for more details.
-   
+
 You should have received a copy of the GNU Affero General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
-  
+
 Stay tuned using
-twitter @navitia 
+twitter @navitia
 IRC #navitia on freenode
 https://groups.google.com/d/forum/navitia
 www.navitia.io
@@ -298,6 +298,8 @@ BOOST_AUTO_TEST_CASE(parse_gtfs_no_dst){
     //Calendar
     BOOST_REQUIRE_EQUAL(data.validity_patterns.size(), 2);
     BOOST_CHECK_EQUAL(data.validity_patterns[0]->uri, "FULLW");
+    auto vp = data.validity_patterns[0];
+    BOOST_CHECK_EQUAL(vp->check((boost::gregorian::date(2010, 12, 31) - vp->beginning_date).days()), true);
 
     BOOST_CHECK_EQUAL(data.validity_patterns[1]->uri, "WE");
 
