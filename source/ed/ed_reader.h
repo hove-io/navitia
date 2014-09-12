38960be77ec5ebd03f6010189ad26572075809b0
Guillaume Pinot <texitoi@texitoi.eu>
2014-09-12 18:33:09 +0200

    ed: add support of the cities database in ed2nav to add admins to sp, sa and poi

--- source/ed/ed_reader.h
+++ source/ed/ed_reader.h
@@ -58,6 +58,10 @@ struct EdReader{
 
     void fill(navitia::type::Data& nav_data, const double min_non_connected_graph_ratio);
 
+    //for admin main stop areas, we need this temporary map
+    //(we can't use an index since the link is between georef and navitia, and those modules are loaded separatly)
+    std::unordered_map<std::string, navitia::georef::Admin*> admin_by_insee_code;
+
 private:
     //map d'id en base vers le poiteur de l'objet instancié
     std::unordered_map<idx_t, navitia::type::Network*> network_map;
@@ -82,11 +86,6 @@ private:
     std::unordered_map<uint64_t, navitia::type::Calendar*> calendar_map;
 
     std::unordered_map<uint64_t, uint64_t> node_map;
-
-    //for admin main stop areas, we need this temporary map
-    //(we can't use an index since the link is between georef and navitia, and those modules are loaded separatly)
-    std::unordered_map<std::string, navitia::georef::Admin*> admin_by_insee_code;
-
 
     // ces deux vectors servent pour ne pas charger les graphes secondaires
     std::set<uint64_t> way_to_ignore; //TODO if bottleneck change to flat_set
