20c9a8e03b2027b73a3eff19aca1f33edb391bc4
l-vincent-l <lara.vincent@gmail.com>
2014-10-14 11:01:27 +0200

    Ed: remove a comment

--- source/ed/connectors/gtfs_parser.cpp
+++ source/ed/connectors/gtfs_parser.cpp
@@ -258,8 +258,6 @@ ed::types::Network* AgencyGtfsHandler::handle_line(Data& data, const csv_row& ro
 
     gtfs_data.network_map[network->uri] = network;
 
-    //for the moment we have to deactivate the timezone handling, so we considere all timezone as UTC
-    // (Africa/Abidjan is equivalent to utc since there is no dst and 0 offset from utc)
     std::string timezone_name = row[time_zone_c];
 
     if (gtfs_data.tz.default_timezone.second) {
