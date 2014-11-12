b3770cd6dcab590e703d911646fce715d1b71d8c
antoine-de <antoine.desbordes@gmail.com>
2014-11-12 18:14:47 +0100

    Kraken: do not catch dying worker
    we stop the service, it's useless to catch the dying worker, the rest
    will folow quickly

--- source/kraken/kraken_zmq.h
+++ source/kraken/kraken_zmq.h
@@ -50,62 +50,59 @@ pbnavitia::Response make_internal_error(const navitia::recoverable_exception& e)
 namespace pt = boost::posix_time;
 void doWork(zmq::context_t & context, DataManager<navitia::type::Data>& data_manager, navitia::kraken::Configuration conf) {
     auto logger = log4cplus::Logger::getInstance("worker");
-    try{
-        zmq::socket_t socket (context, ZMQ_REP);
-        socket.connect ("inproc://workers");
-        bool run = true;
-        navitia::Worker w(data_manager, conf);
-        while(run) {
-            zmq::message_t request;
-            try{
-                // Wait for next request from client
-                socket.recv(&request);
-            }catch(zmq::error_t){
-                //on gére le cas du sighup durant un recv
-                continue;
-            }
 
-            pbnavitia::Request pb_req;
-            pbnavitia::Response result;
-            pt::ptime start = pt::microsec_clock::local_time();
-            pbnavitia::API api = pbnavitia::UNKNOWN_API;
-            if(pb_req.ParseFromArray(request.data(), request.size())){
-                api = pb_req.requested_api();
-                if(api != pbnavitia::METADATAS){
-                    LOG4CPLUS_DEBUG(logger, "receive request: " << pb_req.DebugString());
-                }
-                try {
-                    result = w.dispatch(pb_req);
-                    if(api != pbnavitia::METADATAS){
-                       LOG4CPLUS_TRACE(logger, "response: " << result.DebugString());
-                    }
-                } catch (const navitia::recoverable_exception& e) {
-                    //on a recoverable an internal server error is returned
-                    LOG4CPLUS_ERROR(logger, "internal server error: " << e.what());
-                    LOG4CPLUS_ERROR(logger, "on query: " << pb_req.DebugString());
-                    LOG4CPLUS_ERROR(logger, "backtrace: " << e.backtrace());
-                    result = make_internal_error(e);
-                }
-            }else{
-               LOG4CPLUS_WARN(logger, "receive invalid protobuf");
-               result.mutable_error()->set_id(
-                       pbnavitia::Error::invalid_protobuf_request);
-            }
-            if (! data_manager.get_data()->loaded){
-                result.set_publication_date(-1);
-            } else {
-                result.set_publication_date(navitia::to_posix_timestamp(data_manager.get_data()->meta->publication_date));
-            }
-            zmq::message_t reply(result.ByteSize());
-            result.SerializeToArray(reply.data(), result.ByteSize());
-            socket.send(reply);
-
-            if(api != pbnavitia::METADATAS){
-                LOG4CPLUS_DEBUG(logger, "processing time : "
-                        << (pt::microsec_clock::local_time() - start).total_milliseconds());
-            }
+    zmq::socket_t socket (context, ZMQ_REP);
+    socket.connect ("inproc://workers");
+    bool run = true;
+    navitia::Worker w(data_manager, conf);
+    while(run) {
+        zmq::message_t request;
+        try{
+            // Wait for next request from client
+            socket.recv(&request);
+        }catch(zmq::error_t){
+            //on gére le cas du sighup durant un recv
+            continue;
         }
-    }catch(const std::exception& e){
-        LOG4CPLUS_ERROR(logger, "worker die: " << e.what());
+
+        pbnavitia::Request pb_req;
+        pbnavitia::Response result;
+        pt::ptime start = pt::microsec_clock::local_time();
+        pbnavitia::API api = pbnavitia::UNKNOWN_API;
+        if(pb_req.ParseFromArray(request.data(), request.size())){
+            api = pb_req.requested_api();
+            if(api != pbnavitia::METADATAS){
+                LOG4CPLUS_DEBUG(logger, "receive request: " << pb_req.DebugString());
+            }
+            try {
+                result = w.dispatch(pb_req);
+                if(api != pbnavitia::METADATAS){
+                    LOG4CPLUS_TRACE(logger, "response: " << result.DebugString());
+                }
+            } catch (const navitia::recoverable_exception& e) {
+                //on a recoverable an internal server error is returned
+                LOG4CPLUS_ERROR(logger, "internal server error: " << e.what());
+                LOG4CPLUS_ERROR(logger, "on query: " << pb_req.DebugString());
+                LOG4CPLUS_ERROR(logger, "backtrace: " << e.backtrace());
+                result = make_internal_error(e);
+            }
+        }else{
+            LOG4CPLUS_WARN(logger, "receive invalid protobuf");
+            result.mutable_error()->set_id(
+                        pbnavitia::Error::invalid_protobuf_request);
+        }
+        if (! data_manager.get_data()->loaded){
+            result.set_publication_date(-1);
+        } else {
+            result.set_publication_date(navitia::to_posix_timestamp(data_manager.get_data()->meta->publication_date));
+        }
+        zmq::message_t reply(result.ByteSize());
+        result.SerializeToArray(reply.data(), result.ByteSize());
+        socket.send(reply);
+
+        if(api != pbnavitia::METADATAS){
+            LOG4CPLUS_DEBUG(logger, "processing time : "
+                            << (pt::microsec_clock::local_time() - start).total_milliseconds());
+        }
     }
 }
