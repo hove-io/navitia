d3210ae86f3d6df2ae53d59994141d898d9fd0d2
l-vincent-l <lara.vincent@gmail.com>
2015-01-21 15:44:18 +0100

    Kraken: add impacted_journey_patterns on disruptions

--- source/type/message.h
+++ source/type/message.h
@@ -172,6 +172,10 @@ struct Impact {
 
     std::vector<Message> messages;
 
+    // Used when an informed entity is a stop_area
+    // We need to store it, to be able to delete it afterward
+    std::vector<const JourneyPattern*> impacted_journey_patterns;
+
     //link to the parent disruption
     //Note: it is a raw pointer because an Impact is owned by it's disruption
     //(even if the impact is stored as a share_ptr in the disruption to allow for weak_ptr towards it)
