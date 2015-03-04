0ed4429c58a200fe180254c580b1084686f9409d
Xavier RAFFIN <xavier.raffin@tisseo.fr>
2015-03-04 17:39:35 +0100

    Add an optional default region.

--- source/jormungandr/jormungandr/authentication.py
+++ source/jormungandr/jormungandr/authentication.py
@@ -60,6 +60,8 @@ def authentication_required(func):
                                                 lat=kwargs['lat'])
             except RegionNotFound:
                 pass
+        elif current_app.config.get('DEFAULT_REGION'): # if a default region is defined in config
+            region = current_app.config.get('DEFAULT_REGION') # we use it
         user = get_user(token=get_token())
         if not region:
             #we could not find any regions, we abort
