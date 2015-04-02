f709d93eb4ffd3c92060f5b476e757aec4ae5d16
antoine-de <antoine.desbordes@gmail.com>
2015-04-02 18:03:57 +0200

    Jormungandr: those functions were called twice

--- source/jormungandr/jormungandr/interfaces/v1/Journeys.py
+++ source/jormungandr/jormungandr/interfaces/v1/Journeys.py
@@ -580,8 +580,6 @@ class Journeys(ResourceUri, ResourceUtc):
             parser_post.args[index].location = "json"
 
     @add_debug_info()
-    @clean_links()
-    @add_id_links()
     @add_fare_links()
     @add_journey_pagination()
     @add_journey_href()
