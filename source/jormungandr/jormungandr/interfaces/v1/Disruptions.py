fc77455499d719c0cda8fc23e6d0328c7de0e64a
Alexandre Jacquin <alexandre.jacquin@canaltp.fr>
2015-04-28 13:13:26 +0200

    Jormungandr: re introduce the disruption api since it was used...

--- source/jormungandr/jormungandr/interfaces/v1/Disruptions.py
+++ source/jormungandr/jormungandr/interfaces/v1/Disruptions.py
@@ -29,19 +29,21 @@
 # https://groups.google.com/d/forum/navitia
 # www.navitia.io
 
-from flask.ext.restful import marshal_with, reqparse
+from flask.ext.restful import marshal_with, reqparse, fields
 from flask.globals import g
 from jormungandr import i_manager, timezone
-from fields import PbField, error, network, line,\
-    NonNullList, NonNullNested, pagination, stop_area
-from ResourceUri import ResourceUri
+from jormungandr.interfaces.v1.fields import PbField, error, network, line,\
+    NonNullList, NonNullNested, pagination, stop_area, format_datetime
+from jormungandr.interfaces.v1.ResourceUri import ResourceUri
 from jormungandr.interfaces.argument import ArgumentDoc
 from jormungandr.interfaces.parsers import date_time_format
-from errors import ManageError
+from jormungandr.interfaces.v1.errors import ManageError
 from datetime import datetime
 import aniso8601
 from datetime import timedelta
 from jormungandr.interfaces.v1.fields import DisruptionsField
+from copy import deepcopy
+import sys
 
 disruption = {
     "network": PbField(network, attribute='network'),
@@ -57,12 +59,12 @@ traffic = {
 }
 
 
+
 class TrafficReport(ResourceUri):
     def __init__(self):
         ResourceUri.__init__(self)
         self.parsers = {}
-        self.parsers["get"] = reqparse.RequestParser(
-            argument_class=ArgumentDoc)
+        self.parsers["get"] = reqparse.RequestParser(argument_class=ArgumentDoc)
         parser_get = self.parsers["get"]
         parser_get.add_argument("depth", type=int, default=1)
         parser_get.add_argument("count", type=int, default=10,
@@ -97,3 +99,110 @@ class TrafficReport(ResourceUri):
         response = i_manager.dispatch(args, "disruptions", instance_name=self.region)
 
         return response
+
+"""
+Note: the 'generic_messages' are the old 'disruptions'
+
+For the moment we output both object, but we want to remove the generic_message as soon as possible
+"""
+
+class GenericMessage(fields.Raw):
+    """
+    create old message interface from disruption
+    """
+    def output(self, key, obj):
+        res = []
+        for d in obj.disruptions:
+            message = None
+            if d.messages:
+                message = d.messages[0].text
+            min_date = sys.maxint
+            max_date = None
+            for date in d.application_periods:
+                min_date = min(min_date, date.begin)
+                max_date = max(max_date, date.end)
+            level = 'warning'
+            if d.severity.effect == 'NO_SERVICE':
+                level = 'disrupt'
+
+            tz = timezone.get_timezone()
+            generic_message = {
+                "level": level,
+                "value": message,
+                "start_application_date": format_datetime(min_date, tz),
+                "end_application_date": format_datetime(max_date, tz),
+                "start_application_daily_hour": '000000',
+                "end_application_daily_hour": '235959',
+            }
+            res.append(generic_message)
+
+        return res
+
+network_with_messages = deepcopy(network)
+line_with_messages = deepcopy(line)
+stop_area_with_messages = deepcopy(stop_area)
+
+del network_with_messages['links']
+del line_with_messages['links']
+del stop_area_with_messages['links']
+network_with_messages["messages"] = GenericMessage()
+line_with_messages["messages"] = GenericMessage()
+stop_area_with_messages["messages"] = GenericMessage()
+
+disruptions_old = {
+    "network": PbField(network_with_messages, attribute='network'),
+    "lines": NonNullList(NonNullNested(line_with_messages)),
+    "stop_areas": NonNullList(NonNullNested(stop_area_with_messages))
+}
+disruption_api = {"disruptions": NonNullList(NonNullNested(disruptions_old), attribute='disruptions'),
+                }
+
+def duration(param):
+    try:
+        return aniso8601.parse_duration(param)
+    except ValueError as e:
+        raise ValueError("Unable to parse duration {}, {}".format(param, e.message))
+
+
+class Disruptions(ResourceUri):
+    def __init__(self):
+        ResourceUri.__init__(self)
+        self.parsers = {}
+        self.parsers["get"] = reqparse.RequestParser(argument_class=ArgumentDoc)
+        parser_get = self.parsers["get"]
+        parser_get.add_argument("depth", type=int, default=1)
+        parser_get.add_argument("count", type=int, default=10,
+                                description="Number of disruptions per page")
+        parser_get.add_argument("start_page", type=int, default=0, description="The current page")
+        parser_get.add_argument("_current_datetime", type=date_time_format, default=datetime.utcnow(),
+                                description="The datetime we want to publish the disruptions from."
+                                " Default is the current date and it is mainly used for debug.")
+        parser_get.add_argument("forbidden_id[]", type=unicode, description="forbidden ids",
+                                dest="forbidden_uris[]", action="append")
+
+    @marshal_with(disruption_api)
+    @ManageError()
+    def get(self, region=None, lon=None, lat=None, uri=None):
+        self.region = i_manager.get_region(region, lon, lat)
+        timezone.set_request_timezone(self.region)
+        args = self.parsers["get"].parse_args()
+
+        if uri:
+            if uri[-1] == "/":
+                uri = uri[:-1]
+            uris = uri.split("/")
+            args["filter"] = self.get_filter(uris)
+        else:
+            args["filter"] = ""
+
+        self._register_interpreted_parameters(args)
+
+        response = i_manager.dispatch(args, "disruptions", instance_name=self.region)
+
+        # we store in g if we want the disruptions in the old or in the new format
+        # It's not well designed, but it is temporary
+        # DELETE that asap
+
+        return response
+
+
