cb27fc310afc4d4142949da1627db1b33cc47d59
Xavier RAFFIN <xavier.raffin@tisseo.fr>
2015-05-13 08:50:27 +0200

    Add a reference instead of a copy in a loop (TeXitoi style :-)

--- source/ed/ed_persistor.cpp
+++ source/ed/ed_persistor.cpp
@@ -1107,7 +1107,7 @@ void EdPersistor::insert_meta_vj(const std::map<std::string, types::MetaVehicleJ
 
 void EdPersistor::insert_object_properties(const std::vector<types::ObjectProperty>& object_properties) {
     this->lotus.prepare_bulk_insert("navitia.object_properties", {"object_id", "object_type" , "property_name", "property_value"});
-    for (const types::ObjectProperty property: object_properties) {
+    for (const auto& property: object_properties) {
         this->lotus.insert({std::to_string(property.object_with_idx->idx),
                             property.object_type,
                             property.property_name,
