a05f85846f11d45b345cb7caf2e4e7e69f1b4a48
antoine-de <antoine.desbordes@gmail.com>
2015-05-28 11:19:20 +0200

    All: rename kraken_version in project_version
    with the hugin split, we need to better separate utils and navitia

--- source/config.cpp.cmake
+++ source/config.cpp.cmake
@@ -4,6 +4,6 @@ namespace navitia { namespace config {
 
 const char* fixtures_dir = "${FIXTURES_DIR}";
 const char* navitia_build_type = "${CMAKE_BUILD_TYPE}";
-const char* kraken_version = "${GIT_REVISION}";
+const char* project_version = "${GIT_REVISION}";
 
 }}// namespace navitia::config
