3b7ab1d1620274c9e4c97affe0fc859aa97c2427
Abderrahim Azime <abderrahim.azime@canaltp.fr>
2015-06-17 13:33:22 +0200

    kraken: instance_name added in feed_infos

--- source/kraken/maintenance_worker.cpp
+++ source/kraken/maintenance_worker.cpp
@@ -53,6 +53,8 @@ void MaintenanceWorker::load(){
     LOG4CPLUS_INFO(logger, "Loading database from file: " + database);
     if(this->data_manager.load(database, chaos_database, contributors)){
         auto data = data_manager.get_data();
+        data->meta->feed_infos["instance_name"] = conf.instance_name();
+
     }
 }
 
