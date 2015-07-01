e470f8700c6889adde1aa8b20715b45c9feca1f9
Abderrahim Azime <abderrahim.azime@canaltp.fr>
2015-07-01 16:21:32 +0200

    kraken: fill Properties of stop_date_time object in departures API

--- source/time_tables/next_passages.cpp
+++ source/time_tables/next_passages.cpp
@@ -77,6 +77,9 @@ next_passages(const std::string &request,
         auto arrival_date = navitia::to_posix_timestamp(dt_stop_time.first, data);
         passage->mutable_stop_date_time()->set_departure_date_time(departure_date);
         passage->mutable_stop_date_time()->set_arrival_date_time(arrival_date);
+
+        fill_pb_object(dt_stop_time.second, data, passage->mutable_stop_date_time()->mutable_properties(), 0, current_datetime, action_period);
+
         const type::JourneyPatternPoint* jpp = dt_stop_time.second->journey_pattern_point;
         fill_pb_object(jpp->stop_point, data, passage->mutable_stop_point(),
                 depth, current_datetime, action_period);
