be31c80fabf45438c8df8e80c07038932dbdd390
antoine-de <antoine.desbordes@gmail.com>
2015-07-20 10:03:49 +0200

    Ed: working integration tests
    \o/
    
    make and make test do not handle the ed integration tests
    
    to run them call make docker_test

--- source/ed/docker_tests/ed_integration_tests.cpp
+++ source/ed/docker_tests/ed_integration_tests.cpp
@@ -34,6 +34,7 @@ www.navitia.io
 #include "utils/logger.h"
 #include "type/data.h"
 #include "type/pt_data.h"
+#include <boost/algorithm/string.hpp>
 
 struct logger_initialized {
     logger_initialized()   { init_logger(); }
@@ -41,8 +42,32 @@ struct logger_initialized {
 BOOST_GLOBAL_FIXTURE( logger_initialized )
 
 
-BOOST_AUTO_TEST_CASE(fusio_test) {
-    const auto input_file = std::getenv("INPUT_FILE_PATH");
+struct ArgsFixture {
+   ArgsFixture() {
+       auto argc = boost::unit_test::framework::master_test_suite().argc;
+       auto argv = boost::unit_test::framework::master_test_suite().argv;
+
+       //we should have at least more than one arg, the first is the exe name, the rest is the input file paths
+       BOOST_REQUIRE(argc > 1);
+
+       for (int i(1); i < argc; ++i) {
+           std::vector<std::string> arg;
+
+           std::string raw_arg = argv[i];
+           // dumb method, we erase all '-' not to bother with the '--bob=toto' format
+           boost::erase_all(raw_arg, "-");
+           boost::split(arg, raw_arg, boost::is_any_of("="));
+
+           BOOST_REQUIRE_EQUAL(arg.size(), 2);
+           input_file_paths[arg.at(0)] = arg.at(1);
+       }
+   }
+
+   std::map<std::string, std::string> input_file_paths;
+};
+
+BOOST_FIXTURE_TEST_CASE(fusio_test, ArgsFixture) {
+    const auto input_file = input_file_paths.at("ntfs_file");
     navitia::type::Data data;
 
     const auto res = data.load(input_file);
