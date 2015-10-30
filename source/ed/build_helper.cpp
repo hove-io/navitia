1d1f33bba1d905db0104ab9205f5c753ba55c707
xlqian <patrick.qian@canaltp.fr>
2015-10-30 15:20:04 +0100

    minor fix

--- source/ed/build_helper.cpp
+++ source/ed/build_helper.cpp
@@ -64,7 +64,6 @@ VJ::VJ(builder & b, const std::string &line_name, const std::string &validity_pa
         route = it->second->route_list.front();
     }
 
-    nt::MetaVehicleJourney* mvj = nullptr;
     std::string name;
     if (! meta_vj_name.empty()) {
         name = meta_vj_name;
@@ -74,15 +73,13 @@ VJ::VJ(builder & b, const std::string &line_name, const std::string &validity_pa
         auto idx = pt_data.vehicle_journeys.size();
         name = "vehicle_journey " + std::to_string(idx);
     }
-
+    nt::MetaVehicleJourney* mvj = pt_data.meta_vjs.get_or_create(name);
     if (is_frequency) {
         // NOTE: the meta vj name should be the same as the vj's name
-        mvj = pt_data.meta_vjs.get_or_create(name);
         auto f_vj = mvj->create_frequency_vj();
         route->frequency_vehicle_journey_list.push_back(f_vj);
         vj = f_vj;
     } else {
-        mvj = pt_data.meta_vjs.get_or_create(name);
         auto d_vj = mvj->create_discrete_vj();
         route->discrete_vehicle_journey_list.push_back(d_vj);
         vj = d_vj;
