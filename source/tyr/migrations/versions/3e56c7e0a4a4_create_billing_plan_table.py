b40019edd9fd6e07214a94eae48b20117784c3da
Thomas Noury <thomas.noury@canaltp.fr>
2015-11-09 13:34:17 +0100

    Updated migration for default billing plan

--- source/tyr/migrations/versions/3e56c7e0a4a4_create_billing_plan_table.py
+++ source/tyr/migrations/versions/3e56c7e0a4a4_create_billing_plan_table.py
@@ -34,8 +34,8 @@ def upgrade():
         "end_point", ["end_point_id"], ["id"]
     )
 
-    op.execute("INSERT INTO end_point (name, \"default\") SELECT 'sncf',false WHERE NOT EXISTS (SELECT id FROM end_point WHERE id = 2 AND name = 'sncf');")
-    op.execute("INSERT INTO billing_plan (name, max_request_count, max_object_count, \"default\", end_point_id) VALUES ('nav_dev',3000,NULL,false,1),('nav_ent',NULL,NULL,false,1),('nav_ctp',NULL,NULL,true,1),('sncf_dev',3000,60000,true,2),('sncf_ent',NULL,NULL,false,2);")
+    op.execute("INSERT INTO end_point (name, \"default\") SELECT 'sncf',false WHERE NOT EXISTS (SELECT id FROM end_point WHERE name = 'sncf');")
+    op.execute("INSERT INTO billing_plan (name, max_request_count, max_object_count, \"default\", end_point_id) VALUES ('nav_dev',3000,NULL,false,1),('nav_ent',NULL,NULL,false,1),('nav_ctp',NULL,NULL,true,1),('sncf_dev',3000,60000,true,(SELECT id FROM end_point WHERE name='sncf')),('sncf_ent',NULL,NULL,false,(SELECT id FROM end_point WHERE name='sncf'));")
 
     op.add_column(u'user', sa.Column('billing_plan_id', sa.Integer(), nullable=True))
     op.create_foreign_key(
@@ -43,8 +43,8 @@ def upgrade():
         "billing_plan", ["billing_plan_id"], ["id"]
     )
 
-    op.execute('UPDATE public.user SET billing_plan_id = (SELECT id FROM billing_plan b WHERE b.default AND end_point_id=1) WHERE end_point_id=1;')
-    op.execute('UPDATE public.user SET billing_plan_id = (SELECT id FROM billing_plan b WHERE b.default AND end_point_id=2) WHERE end_point_id=2;')
+    op.execute("UPDATE public.user SET billing_plan_id = (SELECT b.id FROM billing_plan b WHERE b.default AND end_point_id=1) WHERE end_point_id=1;")
+    op.execute("UPDATE public.user u SET billing_plan_id = (SELECT b.id FROM billing_plan b INNER JOIN end_point ep ON ep.id = b.end_point_id WHERE b.default AND ep.name='sncf') FROM end_point ep WHERE ep.id = u.end_point_id AND ep.name='sncf';")
 
 def downgrade():
     op.drop_column('user', 'billing_plan_id')
