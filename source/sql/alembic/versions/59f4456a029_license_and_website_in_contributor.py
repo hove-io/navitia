33ffb5e9559006c4edc4d2e6bc59fa1c8fdbbdee
Abderrahim Azime <abderrahim.azime@canaltp.fr>
2015-11-24 12:18:22 +0100

    ed: rebase

--- source/sql/alembic/versions/59f4456a029_license_and_website_in_contributor.py
+++ source/sql/alembic/versions/59f4456a029_license_and_website_in_contributor.py
@@ -1,14 +1,14 @@
 """license and website in contributor
 
 Revision ID: 59f4456a029
-Revises: 4ccd66ece649
+Revises: c3da2283d4a
 Create Date: 2015-11-20 11:18:47.970759
 
 """
 
 # revision identifiers, used by Alembic.
 revision = '59f4456a029'
-down_revision = '4ccd66ece649'
+down_revision = 'c3da2283d4a'
 
 from alembic import op
 import sqlalchemy as sa
