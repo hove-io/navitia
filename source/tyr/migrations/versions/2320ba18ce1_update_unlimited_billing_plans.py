6f93f5205bcab651c3911efeb59a6353930e556d
Rémy Abi-Khalil <remyabi@gmail.com>
2015-12-18 16:31:31 +0100

    Migration updated. (add default value on max_object_count & max_request_count column)

--- source/tyr/migrations/versions/2320ba18ce1_update_unlimited_billing_plans.py
+++ source/tyr/migrations/versions/2320ba18ce1_update_unlimited_billing_plans.py
@@ -17,7 +17,11 @@ import sqlalchemy as sa
 def upgrade():
     op.execute("update billing_plan set max_request_count=0 where max_request_count IS NULL")
     op.execute("update billing_plan set max_object_count=0 where max_object_count IS NULL")
+    op.alter_column('billing_plan', 'max_request_count', server_default='0')
+    op.alter_column('billing_plan', 'max_object_count', server_default='0')
 
 def downgrade():
     op.execute("update billing_plan set max_request_count=NULL where max_request_count=0")
     op.execute("update billing_plan set max_object_count=NULL where max_object_count=0")
+    op.alter_column('billing_plan', 'max_request_count')
+    op.alter_column('billing_plan', 'max_object_count')
