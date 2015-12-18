37a5fe8af7707f0b2c80b838d800e36f68006f67
Rémy Abi-Khalil <remyabi@gmail.com>
2015-12-18 15:41:12 +0100

    Datetime validation updated.

--- source/tyr/tyr/validations.py
+++ source/tyr/tyr/validations.py
@@ -2,7 +2,4 @@ from datetime import datetime
 
 def datetime_format(value):
     """Parse a valid looking date in the format YYYYmmddTHHmmss"""
-    utc_dt = datetime.strptime(value, "%Y%m%dT%H%M%S")
-    if utc_dt.year < 1900:
-        raise ValueError(u"Year must be >= 1900")
-    return utc_dt
+    return datetime.strptime(value, "%Y%m%dT%H%M%S")
