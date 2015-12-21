52ab94909ba2be4b5dba70ac54858e37df1488dd
Rémy Abi-Khalil <remyabi@gmail.com>
2015-12-21 10:50:18 +0100

    datetime type updated to save in database. Timezone UTC added.

--- source/tyr/tyr/validations.py
+++ source/tyr/tyr/validations.py
@@ -1,5 +1,9 @@
 from datetime import datetime
+import pytz
 
 def datetime_format(value):
     """Parse a valid looking date in the format YYYYmmddTHHmmss"""
-    return datetime.strptime(value, "%Y%m%dT%H%M%S")
+    utc=pytz.UTC
+
+    return utc.localize(datetime.strptime(value, "%Y%m%dT%H%M%S"))
+
