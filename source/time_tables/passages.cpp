a0490b9821e878788a3b376b53103c6211456ff5
Guillaume Pinot <texitoi@texitoi.eu>
2016-03-08 18:17:07 +0100

    time table: mutualize next_passages and previous_passages

--- source/time_tables/passages.cpp
+++ source/time_tables/passages.cpp
@@ -28,40 +28,89 @@ https://groups.google.com/d/forum/navitia
 www.navitia.io
 */
 
-#include "next_passages.h"
+#include "passages.h"
 #include "routing/get_stop_times.h"
 #include "request_handle.h"
 #include "type/datetime.h"
 #include "ptreferential/ptreferential.h"
 #include "utils/paginate.h"
 #include "routing/dataraptor.h"
-#include <boost/range/algorithm_ext/erase.hpp>
 #include <boost/date_time/posix_time/posix_time.hpp>
+#include <boost/range/algorithm_ext/erase.hpp>
 
 namespace pt = boost::posix_time;
 using navitia::routing::StopEvent;
 
 namespace navitia { namespace timetables {
 
-template<typename Visitor>
-void next_passages(PbCreator& pb_creator, const std::string &request,
+namespace {
+struct PassagesVisitor {
+    pbnavitia::API api_pb;
+    const type::Data &data;
+    bool clockwise() const {
+        switch (api_pb) {
+        case pbnavitia::NEXT_DEPARTURES:
+        case pbnavitia::NEXT_ARRIVALS:
+            return true;
+        case pbnavitia::PREVIOUS_DEPARTURES:
+        case pbnavitia::PREVIOUS_ARRIVALS:
+            return false;
+        default:
+            throw std::logic_error("bad api_pb in passages");
+        }
+    }
+    StopEvent stop_event() const {
+        switch (api_pb) {
+        case pbnavitia::NEXT_DEPARTURES:
+        case pbnavitia::PREVIOUS_DEPARTURES:
+            return StopEvent::pick_up;
+        case pbnavitia::NEXT_ARRIVALS:
+        case pbnavitia::PREVIOUS_ARRIVALS:
+            return StopEvent::drop_off;
+        default:
+            throw std::logic_error("bad api_pb in passages");
+        }
+    }
+    bool is_useless(const routing::JppIdx& jpp_idx) const {
+        const auto& jpp = data.dataRaptor->jp_container.get(jpp_idx);
+        const auto& jp = data.dataRaptor->jp_container.get(jpp.jp_idx);
+        switch (stop_event()) {
+        case StopEvent::pick_up:
+            if (jp.jpps.empty()) { return false; }
+            return jpp_idx == jp.jpps.back();
+        case StopEvent::drop_off:
+            return jpp.order == 0;
+        }
+    }
+};
+}
+
+
+void passages(PbCreator& pb_creator,
+              const std::string& request,
               const std::vector<std::string>& forbidden_uris,
               const pt::ptime datetime,
-              uint32_t duration, uint32_t nb_stoptimes, const int depth,
-              const type::AccessibiliteParams & accessibilite_params,
-              const type::RTLevel rt_level, Visitor vis, uint32_t count, uint32_t start_page) {
+              const uint32_t duration,
+              const uint32_t nb_stoptimes,
+              const int depth,
+              const type::AccessibiliteParams& accessibilite_params,
+              const type::RTLevel rt_level,
+              const pbnavitia::API api_pb,
+              const uint32_t count,
+              const uint32_t start_page) {
 
-    RequestHandle handler(pb_creator, request, forbidden_uris, datetime, duration, {});
+    const PassagesVisitor vis{api_pb, pb_creator.data};
+    RequestHandle handler(pb_creator, request, forbidden_uris,
+                          datetime, duration, {}, vis.clockwise());
 
     if(pb_creator.has_error()) {
         return;
     }
 
-    boost::remove_erase_if(handler.journey_pattern_points, vis.predicate);
+    boost::remove_erase_if(handler.journey_pattern_points,
+                           [&](const routing::JppIdx& jpp_idx){return vis.is_useless(jpp_idx);});
 
-    const StopEvent stop_event = (vis.api_pb == pbnavitia::NEXT_DEPARTURES) ?
-                                 StopEvent::pick_up : StopEvent::drop_off;
-    auto passages_dt_st = get_stop_times(stop_event, handler.journey_pattern_points,
+    auto passages_dt_st = get_stop_times(vis.stop_event(), handler.journey_pattern_points,
                                          handler.date_time, handler.max_datetime, nb_stoptimes,
                                          pb_creator.data, rt_level, accessibilite_params);
     size_t total_result = passages_dt_st.size();
@@ -69,7 +118,7 @@ void next_passages(PbCreator& pb_creator, const std::string &request,
 
     for (auto dt_stop_time : passages_dt_st) {
         pbnavitia::Passage* passage;
-        if (stop_event == StopEvent::pick_up) {
+        if (vis.stop_event() == StopEvent::pick_up) {
             passage = pb_creator.add_next_departures();
         } else {
             passage = pb_creator.add_next_arrivals();
@@ -82,7 +131,6 @@ void next_passages(PbCreator& pb_creator, const std::string &request,
         passage->mutable_stop_date_time()->set_arrival_date_time(arrival_date);
 
         pb_creator.fill(dt_stop_time.second, passage->mutable_stop_date_time()->mutable_properties(), 0);
-
         pb_creator.fill(dt_stop_time.second->stop_point, passage->mutable_stop_point(), depth);
         const type::VehicleJourney* vj = dt_stop_time.second->vehicle_journey;
         const type::Route* route = vj->route;
@@ -97,59 +145,4 @@ void next_passages(PbCreator& pb_creator, const std::string &request,
     pb_creator.make_paginate(total_result, start_page, count, passages_dt_st.size());
 }
 
-
-void next_departures(PbCreator& pb_creator, const std::string &request,
-        const std::vector<std::string>& forbidden_uris,
-        const pt::ptime datetime, uint32_t duration, uint32_t nb_stoptimes,
-        const int depth, const type::AccessibiliteParams & accessibilite_params,
-        const type::RTLevel rt_level, uint32_t count, uint32_t start_page) {
-
-    struct vis_next_departures {
-        struct predicate_t {
-            const type::Data &data;
-            predicate_t(const type::Data& data) : data(data){}
-            bool operator()(const routing::JppIdx& jpp_idx) const {
-                const auto& jpp = data.dataRaptor->jp_container.get(jpp_idx);
-                const auto& jp = data.dataRaptor->jp_container.get(jpp.jp_idx);
-                if (jp.jpps.empty()) { return false; }
-                return jpp_idx == jp.jpps.back();
-            }
-        };
-        pbnavitia::API api_pb;
-        predicate_t predicate;
-        vis_next_departures(const type::Data& data) :
-            api_pb(pbnavitia::NEXT_DEPARTURES), predicate(data) {}
-    };
-    vis_next_departures vis(pb_creator.data);
-    next_passages(pb_creator, request, forbidden_uris, datetime, duration, nb_stoptimes, depth,
-                         accessibilite_params, rt_level, vis, count, start_page);
-}
-
-
-void next_arrivals(PbCreator& pb_creator, const std::string &request,
-        const std::vector<std::string>& forbidden_uris,
-        const pt::ptime datetime, uint32_t duration, uint32_t nb_stoptimes,
-        const int depth, const type::AccessibiliteParams & accessibilite_params,
-        const type::RTLevel rt_level, uint32_t count, uint32_t start_page) {
-
-    struct vis_next_arrivals {
-        struct predicate_t {
-            const type::Data &data;
-            predicate_t(const type::Data& data) : data(data){}
-            bool operator()(const routing::JppIdx& jpp_idx) const {
-                const auto& jpp = data.dataRaptor->jp_container.get(jpp_idx);
-                return jpp.order == 0;
-            }
-        };
-        pbnavitia::API api_pb;
-        predicate_t predicate;
-        vis_next_arrivals(const type::Data& data) :
-            api_pb(pbnavitia::NEXT_ARRIVALS), predicate(data) {}
-    };
-    vis_next_arrivals vis(pb_creator.data);
-    next_passages(pb_creator, request, forbidden_uris, datetime, duration, nb_stoptimes, depth,
-            accessibilite_params, rt_level, vis, count, start_page);
-}
-
-
-} }
+}}// namespace
