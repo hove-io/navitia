996723f02b48973a4827cdf48e96f1b42133261d
antoine-de <antoine.desbordes@gmail.com>
2016-03-14 16:49:38 +0100

    compatibility fix

--- source/jormungandr/jormungandr/interfaces/v1/errors.py
+++ source/jormungandr/jormungandr/interfaces/v1/errors.py
@@ -79,7 +79,7 @@ class ManageError(object):
                 response_pb2.Error.bad_format: 400,
                 response_pb2.Error.no_solution: 200
             }
-            if response.HasField("error") and\
+            if response.HasField(b"error") and\
                response.error.id in errors:
                 code = errors[response.error.id]
                 if code == 400 and "filter" not in request.args:
@@ -87,6 +87,6 @@ class ManageError(object):
                     code = 404
 
             else:
-                response.ClearField("error")
+                response.ClearField(b"error")
             return response, code
         return wrapper
