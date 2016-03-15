b9153df4e5d53670a79ff4843a4945c8d344dda4
Abderrahim Azime <abderrahim.azime@canaltp.fr>
2016-03-15 11:40:32 +0100

    kraken: comment added

--- source/routing/raptor.cpp
+++ source/routing/raptor.cpp
@@ -212,6 +212,10 @@ struct Dom {
     bool clockwise;
     typedef std::pair<size_t, StartingPointSndPhase> Arg;
     inline bool operator()(const Arg& lhs, const Arg& rhs) const {
+        /*
+         * When multiple arrival with (same walking, same time, same number of sections)
+         * were possible we used to keep only the first discovered
+         */
         if (lhs.second.count == rhs.second.count
                 && lhs.second.end_dt == rhs.second.end_dt
                 && lhs.second.fallback_dur == rhs.second.fallback_dur) {
