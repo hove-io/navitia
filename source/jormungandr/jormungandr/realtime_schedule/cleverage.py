2c3009a741a31dfed120c3881ddc8a2fc2f0cdb8
patoche <patrick.degeilh@canaltp.fr>
2016-04-08 16:25:34 +0200

    add clever age headers

--- source/jormungandr/jormungandr/realtime_schedule/cleverage.py
+++ source/jormungandr/jormungandr/realtime_schedule/cleverage.py
@@ -40,8 +40,9 @@ class Cleverage(RealtimeProxy):
     """
     class managing calls to cleverage external service providing real-time next passages
     """
-    def __init__(self, id, service_url, timezone, object_id_tag=None, timeout=10):
-        self.service_url = service_url
+    def __init__(self, id, service_url, service_args, timezone, object_id_tag=None, timeout=10):
+        self.service_url = service_url if (service_url[-1] == u'/') else (service_url+'/')
+        self.service_args = service_args
         self.timeout = timeout  # timeout in seconds
         self.rt_system_id = id
         self.object_id_tag = object_id_tag if object_id_tag else id
@@ -55,13 +56,16 @@ class Cleverage(RealtimeProxy):
         """
         return self.rt_system_id
 
-    @cache.memoize(app.config['CACHE_CONFIGURATION'].get('CLEVERAGE_SYNTHESE', 30))
-    def _call_synthese(self, url):
+    @cache.memoize(app.config['CACHE_CONFIGURATION'].get('TIMEOUT_CLEVERAGE', 30))
+    def _call_cleverage(self, url):
         """
         http call to cleverage
         """
         try:
-            return self.breaker.call(requests.get, url, timeout=self.timeout)
+            headers = {}
+            for k, v in self.service_args.items() :
+                headers[k] = v
+            return self.breaker.call(requests.get, url, timeout=self.timeout, headers=headers)
         except pybreaker.CircuitBreakerError as e:
             logging.getLogger(__name__).error('Cleverage RT service dead, using base '
                                               'schedule (error: {}'.format(e))
@@ -71,4 +75,36 @@ class Cleverage(RealtimeProxy):
         except:
             logging.getLogger(__name__).exception('Cleverage RT error, using base schedule')
         return None
+
+    def _make_url(self, route_point):
+        """
+        The url returns something like a departure on a stop point
+        """
+
+        stop_id = route_point.fetch_stop_id(self.object_id_tag)
+
+        if not stop_id:
+            # one a the id is missing, we'll not find any realtime
+            logging.getLogger(__name__).debug('missing realtime id for {obj}: stop code={s}'.
+                                              format(obj=route_point, s=stop_id))
+            return None
+
+        url = "{base_url}{stop_id}".format(base_url=self.service_url, stop_id=stop_id)
+
+        return url
+
+    def next_passage_for_route_point(self, route_point):
+        url = self._make_url(route_point)
+        if not url:
+            return None
+
+        r = self._call_cleverage(url)
+        if not r:
+            return None
+
+        if r.status_code != 200:
+            # TODO better error handling, the response might be in 200 but in error
+            logging.getLogger(__name__).error('Cleverage RT service unavailable, impossible to query : {}'
+                                              .format(r.url))
+            return None
 
