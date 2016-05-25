7304f113e1c52e9fe358f4d06931f1068915fcdb
Abderrahim Azime <abderrahim.azime@canaltp.fr>
2016-05-25 15:48:53 +0200

    jormungandr: test

--- source/jormungandr/tests/proxy_realtime_integration_tests.py
+++ source/jormungandr/tests/proxy_realtime_integration_tests.py
@@ -39,11 +39,95 @@ import pytz
 from .check_utils import is_valid_stop_date_time, get_not_null
 from mock import MagicMock
 from navitiacommon import type_pb2
+from jormungandr.realtime_schedule.timeo import Timeo
+
+
+class Obj(object):
+    pass
+
+
+class MockTimeo(Timeo):
+
+    def __init__(self, id, service_url, service_args, timezone,
+        object_id_tag=None, destination_id_tag=None, instance=None, timeout=10):
+        Timeo.__init__(self, id, service_url, service_args, timezone,
+        object_id_tag, destination_id_tag, instance, timeout)
+
+    def _call_timeo(self, url):
+        resp = Obj()
+        resp.status_code = 200
+        json = {}
+        if url == 'S41':
+            json = {
+                "CorrelationID": "AA",
+                "StopTimesResponse": [
+                    {
+                        "StopID": "S41",
+                        "StopTimeoCode": "AAAAA",
+                        "StopLongName": "Malraux",
+                        "StopShortName": "Malraux",
+                        "StopVocalName": "Malraux",
+                        "ReferenceTime": "09:54:53",
+                        "NextStopTimesMessage": {
+                            "LineID": "AAA",
+                            "Way": "A",
+                            "LineMainDirection": "DIRECTION AA",
+                            "NextExpectedStopTime": [
+                                {
+                                    "NextStop": "10:00:52",
+                                    "Destination": "DIRECTION AA"
+                                },
+                                {
+                                    "NextStop": "10:13:52",
+                                    "Destination": "DIRECTION AA"
+                                }
+                            ]
+                        }
+                    }
+                ]
+            }
+        if url == 'S42':
+            json = {
+                "CorrelationID": "AA",
+                "StopTimesResponse": [
+                    {
+                        "StopID": "C:S0",
+                        "StopTimeoCode": "AAAAA",
+                        "StopLongName": "Malraux",
+                        "StopShortName": "Malraux",
+                        "StopVocalName": "Malraux",
+                        "ReferenceTime": "09:54:53",
+                        "NextStopTimesMessage": {
+                            "LineID": "AAA",
+                            "Way": "A",
+                            "LineMainDirection": "DIRECTION AA",
+                            "NextExpectedStopTime": [
+                                {
+                                    "NextStop": "10:00:52",
+                                    "Destination": "DIRECTION AA",
+                                    "Terminus": "KisioDigital_C:S1"
+
+                                },
+                                {
+                                    "NextStop": "10:13:52",
+                                    "Destination": "DIRECTION AA",
+                                    "Terminus": "KisioDigital_C:S1"
+                                }
+                            ]
+                        }
+                    }
+                ]
+            }
+        resp.json = MagicMock(return_value=json)
+        return resp
+
+    def _make_url(self, route_point, count=None, from_dt=None):
+        return route_point.pb_stop_point.uri
 
 
 MOCKED_PROXY_CONF = ('[{ "object_id_tag": "KisioDigital",'
                      '"id": "KisioDigital",'
-                     '"class": "jormungandr.realtime_schedule.timeo.Timeo",'
+                     '"class": "tests.proxy_realtime_integration_tests.MockTimeo",'
                      '"args": {'
                      '"destination_id_tag": "KisioDigital",'
                      '"timezone": "Europe/Paris",'
@@ -54,26 +138,12 @@ MOCKED_PROXY_CONF = ('[{ "object_id_tag": "KisioDigital",'
                      '"EntityID": "XX",'
                      '"Media": "XXX"}}}]')
 
-
-class Obj(object):
-    pass
-
 @dataset({"basic_schedule_test": {"proxy_conf": MOCKED_PROXY_CONF}})
 class TestDepartures(AbstractTestFixture):
     query_template = 'stop_points/{sp}/stop_schedules?from_datetime={dt}&show_codes=true{data_freshness}'
 
     def test_stop_schedule_without_rt(self):
         query = self.query_template.format(sp='C:S0', dt='20160102T1100', data_freshness='')
-        from jormungandr import i_manager
-        instance = i_manager.instances.get('basic_schedule_test')
-        rt_system = instance.realtime_proxy_manager.get('KisioDigital')
-
-        assert rt_system
-        resp = Obj()
-        resp.status_code = 200
-        resp.json = MagicMock(return_value={})
-        rt_system._call_timeo = MagicMock(return_value=resp)
-        rt_system._make_url = MagicMock(return_value={"http://kisiodigital.com"})
         response = self.query_region(query)
         stop_schedules = response['stop_schedules']
         assert len(stop_schedules) == 1
@@ -84,48 +154,7 @@ class TestDepartures(AbstractTestFixture):
         assert stop_time['date_time'][8:] == 'T113000'
 
     def test_stop_schedule_with_rt_and_without_destination(self):
-        query = self.query_template.format(sp='C:S0', dt='20160102T1100', data_freshness='')
-        from jormungandr import i_manager
-        instance = i_manager.instances.get('basic_schedule_test')
-        rt_system = instance.realtime_proxy_manager.get('KisioDigital')
-
-        assert rt_system
-        resp = Obj()
-        resp.status_code = 200
-        json = {
-            "CorrelationID": "AA",
-            "StopTimesResponse": [
-                {
-                    "StopID": "C:S0",
-                    "StopTimeoCode": "AAAAA",
-                    "StopLongName": "Malraux",
-                    "StopShortName": "Malraux",
-                    "StopVocalName": "Malraux",
-                    "ReferenceTime": "09:54:53",
-                    "NextStopTimesMessage": {
-                        "LineID": "AAA",
-                        "Way": "A",
-                        "LineMainDirection": "DIRECTION AA",
-                        "NextExpectedStopTime": [
-                            {
-                                "NextStop": "10:00:52",
-                                "Destination": "DIRECTION AA"
-                            },
-                            {
-                                "NextStop": "10:13:52",
-                                "Destination": "DIRECTION AA"
-                            }
-                        ]
-                    }
-                }
-            ]
-        }
-
-        resp.json = MagicMock(return_value=json)
-        rt_system._call_timeo = MagicMock(return_value=resp)
-        rt_system._make_url = MagicMock(return_value={"http://kisiodigital.com"})
-        rt_system.instance.ptref.get_stop_point = MagicMock(return_value=None)
-
+        query = self.query_template.format(sp='S41', dt='20160102T1100', data_freshness='')
         response = self.query_region(query)
         stop_schedules = response['stop_schedules']
         assert len(stop_schedules) == 1
@@ -150,75 +179,27 @@ class TestDepartures(AbstractTestFixture):
         assert notes[0]["id"] == "note:7a0967bbb281e0d1548d2d5bc6933a20"
 
     def test_stop_schedule_with_rt_and_with_destination(self):
-        query = self.query_template.format(sp='C:S0', dt='20160102T1100', data_freshness='')
-        from jormungandr import i_manager
-        instance = i_manager.instances.get('basic_schedule_test')
-        rt_system = instance.realtime_proxy_manager.get('KisioDigital')
-
-        assert rt_system
-        resp = Obj()
-        resp.status_code = 200
-        json = {
-            "CorrelationID": "AA",
-            "StopTimesResponse": [
-                {
-                    "StopID": "C:S0",
-                    "StopTimeoCode": "AAAAA",
-                    "StopLongName": "Malraux",
-                    "StopShortName": "Malraux",
-                    "StopVocalName": "Malraux",
-                    "ReferenceTime": "09:54:53",
-                    "NextStopTimesMessage": {
-                        "LineID": "AAA",
-                        "Way": "A",
-                        "LineMainDirection": "DIRECTION AA",
-                        "NextExpectedStopTime": [
-                            {
-                                "NextStop": "10:00:52",
-                                "Destination": "DIRECTION AA",
-                                "Terminus": "C:S0"
-
-                            },
-                            {
-                                "NextStop": "10:13:52",
-                                "Destination": "DIRECTION AA",
-                                "Terminus": "C:S0"
-                            }
-                        ]
-                    }
-                }
-            ]
-        }
-
-        resp.json = MagicMock(return_value=json)
-        rt_system._call_timeo = MagicMock(return_value=resp)
-        rt_system._make_url = MagicMock(return_value={"http://kisiodigital.com"})
-
-        stop_point = type_pb2.StopPoint()
-        stop_point.uri = "C:S0"
-        stop_point.name = "TEST DESTINATION"
-
-        rt_system.instance.ptref.get_stop_point = MagicMock(return_value=stop_point)
+        query = self.query_template.format(sp='S42', dt='20160102T1100', data_freshness='')
 
         response = self.query_region(query)
         stop_schedules = response['stop_schedules']
-        assert len(stop_schedules) == 1
+        assert len(stop_schedules) == 3
         stop_times = stop_schedules[0]['date_times']
         assert len(stop_times) == 2
         assert stop_times[0]['data_freshness'] == 'realtime'
         assert stop_times[0]['date_time'][8:] == 'T080052'
         links = stop_times[0]["links"]
         assert len(links) == 1
-        assert links[0]['id'] == 'note:422037618fec94aa0a93d6126a48157e'
+        assert links[0]['id'] == 'note:a9ce65528143e700d05dbfdffa78325c'
 
         assert stop_times[1]['data_freshness'] == 'realtime'
         assert stop_times[1]['date_time'][8:] == 'T081352'
         links = stop_times[1]["links"]
         assert len(links) == 1
-        assert links[0]['id'] == 'note:422037618fec94aa0a93d6126a48157e'
+        assert links[0]['id'] == 'note:a9ce65528143e700d05dbfdffa78325c'
 
         notes = response['notes']
         assert len(notes) == 1
         assert notes[0]["type"] == "notes"
-        assert notes[0]["value"] == "TEST DESTINATION"
-        assert notes[0]["id"] == "note:422037618fec94aa0a93d6126a48157e"
+        assert notes[0]["value"] == "C:S1"
+        assert notes[0]["id"] == "note:a9ce65528143e700d05dbfdffa78325c"
