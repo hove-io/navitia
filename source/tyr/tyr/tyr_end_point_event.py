ce4374a7dd05caea5e4c521ac1bad7a9be477595
Alcalyn <doubjulien@hotmail.fr>
2016-05-27 16:13:34 +0200

    Add Tyr Endpoint events

---
+++ source/tyr/tyr/tyr_end_point_event.py
@@ -0,0 +1,39 @@
+# encoding=utf-8
+
+from rabbit_mq_handler import RabbitMqHandler
+from flask import current_app
+
+class EndPointEventMessage:
+
+    CREATE = 'create_end_point'
+    UPDATE = 'update_end_point'
+    DELETE = 'delete_end_point'
+
+    def __init__(self, event_name, end_point, old_end_point_name=None):
+        self._event_name = event_name
+        self._end_point_old_name = old_end_point_name
+        self._end_point = end_point
+
+    def get_message(self):
+        payload = {
+            'event': self._event_name,
+            'data': {
+                'name': self._end_point.name,
+                'default': self._end_point.default,
+                'hostnames': self._end_point.hostnames
+            }
+        }
+
+        if self._event_name == self.UPDATE:
+            payload['data']['last_name'] = self._end_point_old_name
+
+        return payload
+
+
+class TyrEventsRabbitMq(object):
+
+    def __init__(self):
+        self._rabbit_mq_handler = RabbitMqHandler(current_app.config['CELERY_BROKER_URL'], "tyr_event_exchange")
+
+    def request(self, end_point_event_message):
+        self._rabbit_mq_handler.publish(end_point_event_message.get_message(), None, 'json')
