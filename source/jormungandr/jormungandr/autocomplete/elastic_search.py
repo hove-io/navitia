1fbcfe22c00962bed13552f44bcc4dc0eb965b14
xlqian <patrick.qian@canaltp.fr>
2016-06-07 16:51:36 +0200

    display admin coord

--- source/jormungandr/jormungandr/autocomplete/elastic_search.py
+++ source/jormungandr/jormungandr/autocomplete/elastic_search.py
@@ -50,8 +50,11 @@ class Lit(fields.Raw):
 
 ww_admin = {
     "id": fields.String,
-    # "insee": dict["id"][6:],
-    # "coord": ??
+    "insee": fields.String(attribute="insee"),
+    "coord": {
+        "lon": fields.Float(attribute="coord.lon"),
+        "lat": fields.Float(attribute="coord.lat"),
+    },
     "level": fields.Integer,
     "name": fields.String,
     "label": fields.String(attribute="name"),
