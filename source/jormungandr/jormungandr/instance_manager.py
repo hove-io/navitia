df17aa53cabc370e7999f4ee595eb44a7e73518a
Abderrahim Azime <abderrahim.azime@canaltp.fr>
2016-08-29 15:50:26 +0200

    jorumngandr: valhalla called

--- source/jormungandr/jormungandr/instance_manager.py
+++ source/jormungandr/jormungandr/instance_manager.py
@@ -113,7 +113,9 @@ class InstanceManager(object):
                 with open(file_name) as f:
                     config_data = json.load(f)
                     name = config_data['key']
+                    default_roting_proxy = {'class': 'jormungandr.routing.kraken.Kraken'}
                     instance = Instance(self.context, name, config_data['zmq_socket'],
+                                        config_data.get('routing_proxy', default_roting_proxy),
                                         config_data.get('realtime_proxies', []))
             else:
                 logging.getLogger(__name__).warn('impossible to init an instance with the configuration '
