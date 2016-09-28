aa50a51e7d554a264d131e26f813cea2c99d04f2
patoche <patrick.degeilh@canaltp.fr>
2016-09-28 12:01:55 +0200

    Tyr: use navitiacommon geojson parsing and validation

--- source/tyr/tyr/validations.py
+++ source/tyr/tyr/validations.py
@@ -7,22 +7,3 @@ def datetime_format(value):
     """Parse a valid looking date in the format YYYYmmddTHHmmss"""
 
     return datetime.strptime(value, "%Y%m%dT%H%M%SZ")
-
-def is_geometry_valid(geometry):
-    geometry_str = json.dumps(geometry)
-    valid = geojson.is_valid(geojson.loads(geometry_str))
-    return 'valid' in valid and (valid['valid'] == 'yes' or valid['valid'] == '')
-
-def json_format(value):
-    if value:
-        is_valid_object = True
-        features= value.get('features')
-        if not features:
-            is_valid_object = is_geometry_valid(value)
-        else:
-            for feature in features:
-                geometry = feature.get('geometry')
-                is_valid_object = is_valid_object and is_geometry_valid(geometry)
-        if not is_valid_object:
-            raise ValueError('invalid geometry')
-    return value
