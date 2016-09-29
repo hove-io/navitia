7eb0f3dcc341291fd423a0d852cab2351188bb96
Pascal Rhod <pascal.rhod@canaltp.fr>
2016-09-29 16:03:28 +0200

    tyr - fix tests relative path

--- source/tyr/tests/tests_default_settings.py
+++ source/tyr/tests/tests_default_settings.py
@@ -1,2 +1,3 @@
+import os
 #Path to the directory where the configuration file of each instance of ed are defined
-INSTANCES_DIR = './tests/fixtures/'
+INSTANCES_DIR = os.path.join(os.path.dirname(__file__), 'fixtures')
