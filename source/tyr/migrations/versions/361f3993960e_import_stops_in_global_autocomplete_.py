1e2ac1f05b0c9492445b0a76268b999f5a5cdb86
antoine-de <antoine.desbordes@gmail.com>
2017-01-03 12:11:42 +0100

    changes down revision
    because another PR has been merged

--- source/tyr/migrations/versions/361f3993960e_import_stops_in_global_autocomplete_.py
+++ source/tyr/migrations/versions/361f3993960e_import_stops_in_global_autocomplete_.py
@@ -8,7 +8,7 @@ Create Date: 2016-12-23 11:48:47.003811
 
 # revision identifiers, used by Alembic.
 revision = '361f3993960e'
-down_revision = '57ee5581cc30'
+down_revision = '502b7c6fd00b'
 
 from alembic import op
 import sqlalchemy as sa
