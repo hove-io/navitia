68f384274f01c5e460d6651ab26c3448d389f4f8
antoine-de <antoine.desbordes@gmail.com>
2017-01-19 12:24:01 +0100

    Kraken: fix a crash on heat_map
    the heat_maps were crashing when no streetnetwork data were loaded.

--- source/jormungandr/tests/journey_common_tests.py
+++ source/jormungandr/tests/journey_common_tests.py
@@ -1035,6 +1035,13 @@ class OnBasicRouting():
         eq_(response['journeys'][0]['sections'][0]['type'], 'public_transport')
         eq_(response['journeys'][0]['sections'][0]['additional_informations'][0], 'odt_with_zone')
 
+    def test_heat_map_without_sn(self):
+        """
+        heat map call on a dataset with no streetnetwork
+        should not crash ;)
+        """
+        self.query_region('heat_maps?from=stop_point:B&datetime=20120615T080000&max_duration=60')
+
 
 @dataset({"main_routing_test": {},
           "basic_routing_test": {'check_killed': False}})
