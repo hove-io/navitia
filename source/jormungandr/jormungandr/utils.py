0f862919ed8ec1ea0c14600da8960110eaeb9182
Pierre-Etienne Bougue <pierre-etienne.bougue@canaltp.fr>
2017-02-09 18:23:31 +0100

    Jormun: fix test
    Following an upgrade of protobuf lib in python (no version fixed in requirements), the serialize has changed a little, leading to the test error

--- source/jormungandr/jormungandr/utils.py
+++ source/jormungandr/jormungandr/utils.py
@@ -313,13 +313,13 @@ def get_pt_object_coord(pt_object):
 
     >>> pt_object = type_pb2.PtObject()
     >>> pt_object.embedded_type = type_pb2.POI
-    >>> pt_object.poi.coord.lon = 42
-    >>> pt_object.poi.coord.lat = 41
+    >>> pt_object.poi.coord.lon = 42.42
+    >>> pt_object.poi.coord.lat = 41.41
     >>> coord = get_pt_object_coord(pt_object)
     >>> coord.lon
-    42
+    42.42
     >>> coord.lat
-    41
+    41.41
     """
     map_coord = {
         type_pb2.STOP_POINT: "stop_point",
