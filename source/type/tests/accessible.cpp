7cce7f2a876511b641efab1132dbe555c676453c
patoche <patrick.degeilh@canaltp.fr>
2017-03-15 17:50:15 +0100

    kraken: add accessible test

--- source/type/tests/accessible.cpp
+++ source/type/tests/accessible.cpp
@@ -34,51 +34,99 @@ www.navitia.io
 
 #include "type/type.h"
 using namespace navitia::type;
-BOOST_AUTO_TEST_CASE(required_false_properties_false) {
+BOOST_AUTO_TEST_CASE(wcb_required_false_properties_false) {
     Properties required_properties= 0;
     StopPoint sp;
     BOOST_CHECK_EQUAL(sp.accessible(required_properties), true);
 }
-BOOST_AUTO_TEST_CASE(required_false_properties_true) {
+BOOST_AUTO_TEST_CASE(wcb_required_false_properties_true) {
     Properties required_properties = 0;
     StopPoint sp;
     sp.set_property(hasProperties::WHEELCHAIR_BOARDING);
     BOOST_CHECK_EQUAL(sp.accessible(required_properties), true);
 }
-BOOST_AUTO_TEST_CASE(required_true_properties_false) {
+BOOST_AUTO_TEST_CASE(wcb_required_true_properties_false) {
     Properties required_properties = 1;
     StopPoint sp;
     BOOST_CHECK_EQUAL(sp.accessible(required_properties), false);
 }
-BOOST_AUTO_TEST_CASE(required_true_properties_true) {
+BOOST_AUTO_TEST_CASE(wcb_required_true_properties_true) {
     Properties required_properties = 1;
     StopPoint sp;
     sp.set_property(hasProperties::WHEELCHAIR_BOARDING);
     BOOST_CHECK_EQUAL(sp.accessible(required_properties), true);
 }
-BOOST_AUTO_TEST_CASE(required_false_properties_false_const) {
+BOOST_AUTO_TEST_CASE(wcb_required_false_properties_false_const) {
     Properties required_properties= 0;
     StopPoint tmp_sp;
     const StopPoint sp = tmp_sp;
     BOOST_CHECK_EQUAL(sp.accessible(required_properties), true);
 }
-BOOST_AUTO_TEST_CASE(required_false_properties_true_const) {
+BOOST_AUTO_TEST_CASE(wcb_required_false_properties_true_const) {
     Properties required_properties = 0;
     StopPoint tmp_sp;
     tmp_sp.set_property(hasProperties::WHEELCHAIR_BOARDING);
     const StopPoint sp = tmp_sp;
     BOOST_CHECK_EQUAL(sp.accessible(required_properties), true);
 }
-BOOST_AUTO_TEST_CASE(required_true_properties_false_const) {
+BOOST_AUTO_TEST_CASE(wcb_required_true_properties_false_const) {
     Properties required_properties = 1;
     StopPoint tmp_sp;
     const StopPoint sp = tmp_sp;
     BOOST_CHECK_EQUAL(sp.accessible(required_properties), false);
 }
-BOOST_AUTO_TEST_CASE(required_true_properties_true_const) {
+BOOST_AUTO_TEST_CASE(wcb_required_true_properties_true_const) {
     Properties required_properties = 1;
     StopPoint tmp_sp;
     tmp_sp.set_property(hasProperties::WHEELCHAIR_BOARDING);
+    const StopPoint sp = tmp_sp;
+    BOOST_CHECK_EQUAL(sp.accessible(required_properties), true);
+}
+BOOST_AUTO_TEST_CASE(ba_required_false_properties_false) {
+    Properties required_properties= 0;
+    StopPoint sp;
+    BOOST_CHECK_EQUAL(sp.accessible(required_properties), true);
+}
+BOOST_AUTO_TEST_CASE(ba_required_false_properties_true) {
+    Properties required_properties = 0;
+    StopPoint sp;
+    sp.set_property(hasProperties::BIKE_ACCEPTED);
+    BOOST_CHECK_EQUAL(sp.accessible(required_properties), true);
+}
+BOOST_AUTO_TEST_CASE(ba_required_true_properties_false) {
+    Properties required_properties = 16;
+    StopPoint sp;
+    BOOST_CHECK_EQUAL(sp.accessible(required_properties), false);
+}
+BOOST_AUTO_TEST_CASE(ba_required_true_properties_true) {
+    Properties required_properties = 0;
+    StopPoint sp;
+    sp.set_property(hasProperties::BIKE_ACCEPTED);
+    BOOST_CHECK_EQUAL(sp.accessible(required_properties), true);
+}
+BOOST_AUTO_TEST_CASE(ba_required_false_properties_false_const) {
+    Properties required_properties= 0;
+    StopPoint tmp_sp;
+    const StopPoint sp = tmp_sp;
+    BOOST_CHECK_EQUAL(sp.accessible(required_properties), true);
+}
+BOOST_AUTO_TEST_CASE(ba_required_false_properties_true_const) {
+    Properties required_properties = 0;
+    StopPoint tmp_sp;
+    tmp_sp.set_property(hasProperties::BIKE_ACCEPTED);
+    const StopPoint sp = tmp_sp;
+    BOOST_CHECK_EQUAL(sp.accessible(required_properties), true);
+}
+BOOST_AUTO_TEST_CASE(ba_required_true_properties_false_const) {
+    Properties required_properties = 16;
+    StopPoint tmp_sp;
+    const StopPoint sp = tmp_sp;
+    BOOST_CHECK_EQUAL(sp.accessible(required_properties), false);
+}
+BOOST_AUTO_TEST_CASE(ba_required_true_properties_true_const) {
+    Properties required_properties = 16;
+    StopPoint tmp_sp;
+    tmp_sp.set_property(hasProperties::BIKE_ACCEPTED);
     const StopPoint sp = tmp_sp;
     BOOST_CHECK_EQUAL(sp.accessible(required_properties), true);
 }
