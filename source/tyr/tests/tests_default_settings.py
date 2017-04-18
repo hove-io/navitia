831a3e6a458c99a710c4225addd38eb64ca03407
Pierre-Etienne Bougue <pierre-etienne.bougue@canaltp.fr>
2017-04-18 17:23:27 +0200

    little bonus, deactivate smtp/MX tests on emails for tyr tests

--- source/tyr/tests/tests_default_settings.py
+++ source/tyr/tests/tests_default_settings.py
@@ -1,3 +1,9 @@
 import os
 #Path to the directory where the configuration file of each instance of ed are defined
 INSTANCES_DIR = os.path.join(os.path.dirname(__file__), 'fixtures')
+
+#Validate the presence of a mx record on the domain
+EMAIL_CHECK_MX = False
+
+#Validate the email by connecting to the smtp server, but doesn't send an email
+EMAIL_CHECK_SMTP = False
