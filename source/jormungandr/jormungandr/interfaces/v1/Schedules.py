5d98f1c5a0180055b93441b76be90f4062d51c3e
Guillaume Pinot <texitoi@texitoi.eu>
2017-07-31 15:32:01 +0200

    get_obj_serializer machinery

--- source/jormungandr/jormungandr/interfaces/v1/Schedules.py
+++ source/jormungandr/jormungandr/interfaces/v1/Schedules.py
@@ -39,7 +39,7 @@ from jormungandr.interfaces.v1.fields import stop_point, route, pagination, PbFi
     display_informations_route, UrisToLinks, error, \
     enum_type, SplitDateTime, MultiLineString, PbEnum, feed_publisher
 from jormungandr.interfaces.v1.ResourceUri import ResourceUri, complete_links
-from jormungandr.interfaces.v1.decorators import get_serializer
+from jormungandr.interfaces.v1.decorators import get_obj_serializer
 from jormungandr.interfaces.v1.serializer import api
 import datetime
 from jormungandr.interfaces.argument import ArgumentDoc
@@ -59,8 +59,8 @@ from navitiacommon.parser_args_type import BooleanType, OptionValue
 
 class Schedules(ResourceUri, ResourceUtc):
 
-    def __init__(self, endpoint):
-        ResourceUri.__init__(self)
+    def __init__(self, endpoint, *args, **kwargs):
+        ResourceUri.__init__(self, *args, **kwargs)
         ResourceUtc.__init__(self)
         self.endpoint = endpoint
         self.parsers = {}
@@ -113,6 +113,8 @@ class Schedules(ResourceUri, ResourceUtc):
         parser_get.add_argument("disable_geojson", type=BooleanType(), default=False,
                                 help="remove geojson from the response")
 
+        self.get_decorators.insert(0, ManageError())
+        self.get_decorators.insert(1, get_obj_serializer(self))
         self.get_decorators.append(complete_links(self))
 
     def get(self, uri=None, region=None, lon=None, lat=None):
@@ -231,13 +233,9 @@ route_schedules = {
 class RouteSchedules(Schedules):
 
     def __init__(self):
-        super(RouteSchedules, self).__init__("route_schedules")
-
-    @get_serializer(serpy=api.RouteSchedulesSerializer, marshall=route_schedules)
-    @ManageError()
-    def get(self, uri=None, region=None, lon=None, lat=None):
-        return super(RouteSchedules, self).get(uri=uri, region=region, lon=lon,
-                                               lat=lat)
+        self.collections = route_schedules
+        super(RouteSchedules, self).__init__("route_schedules",
+                                             output_type_serializer=api.RouteSchedulesSerializer)
 
 
 stop_schedule = {
@@ -261,13 +259,9 @@ stop_schedules = {
 class StopSchedules(Schedules):
 
     def __init__(self):
-        super(StopSchedules, self).__init__("departure_boards")
-
-    @get_serializer(serpy=api.StopSchedulesSerializer, marshall=stop_schedules)
-    @ManageError()
-    def get(self, uri=None, region=None, lon=None, lat=None):
-        return super(StopSchedules, self).get(uri=uri, region=region, lon=lon,
-                                              lat=lat)
+        self.collections = stop_schedules
+        super(StopSchedules, self).__init__("departure_boards",
+                                            output_type_serializer=api.StopSchedulesSerializer)
 
 
 passage = {
@@ -344,25 +338,16 @@ class add_passages_links:
 class NextDepartures(Schedules):
 
     def __init__(self):
-        super(NextDepartures, self).__init__("next_departures")
-
-    @add_passages_links()
-    @get_serializer(serpy=api.DeparturesSerializer, marshall=departures)
-    @ManageError()
-    def get(self, uri=None, region=None, lon=None, lat=None,
-            dest="nb_stoptimes"):
-        return super(NextDepartures, self).get(uri=uri, region=region, lon=lon,
-                                               lat=lat)
+        self.collections = departures
+        super(NextDepartures, self).__init__("next_departures",
+                                             output_type_serializer=api.DeparturesSerializer)
+        self.get_decorators.append(add_passages_links())
 
 
 class NextArrivals(Schedules):
 
     def __init__(self):
-        super(NextArrivals, self).__init__("next_arrivals")
-
-    @add_passages_links()
-    @get_serializer(serpy=api.ArrivalsSerializer, marshall=arrivals)
-    @ManageError()
-    def get(self, uri=None, region=None, lon=None, lat=None):
-        return super(NextArrivals, self).get(uri=uri, region=region, lon=lon,
-                                             lat=lat)
+        self.collections = arrivals
+        super(NextArrivals, self).__init__("next_arrivals",
+                                           output_type_serializer=api.ArrivalsSerializer)
+        self.get_decorators.append(add_passages_links())
