8deb51eee179e9d78c6d4b0f899a31e8143397fb
Guillaume Pinot <texitoi@texitoi.eu>
2017-10-03 12:43:34 +0200

    corrections

--- source/type/pb_converter.cpp
+++ source/type/pb_converter.cpp
@@ -1345,8 +1345,9 @@ void PbCreator::Filler::fill_pb_object(const StopTimeCalandar* stop_time_calenda
         const std::vector<const nt::StopTime*> stop_times = {stop_time_calendar->stop_time};
         const auto meta_vj = stop_time_calendar->stop_time->vehicle_journey->meta_vj;
         for (const auto& message: meta_vj->get_applicable_messages(pb_creator.now, pb_creator.action_period)) {
-            if (! message->is_relevant(stop_times)) { continue; }
-            fill_message(message, rs_date_time);
+            if (message->is_relevant(stop_times)) {
+                fill_message(message, rs_date_time);
+            }
         }
     }
 
