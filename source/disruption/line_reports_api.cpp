75ef6064111bdbc73ec6fbb070da3cfc1d480f99
kadhikari <krishna.adhikari@canaltp.fr>
2017-11-23 12:41:42 +0100

    unwanted parameter deleted

--- source/disruption/line_reports_api.cpp
+++ source/disruption/line_reports_api.cpp
@@ -116,8 +116,8 @@ void line_reports(navitia::PbCreator& pb_creator,
                   const std::vector<std::string>& forbidden_uris,
                   const boost::optional<boost::posix_time::ptime>& since,
                   const boost::optional<boost::posix_time::ptime>& until) {
-    auto start = (since ? *since: bt::ptime(d.meta->production_date.begin()));
-    auto end = (until ? *until:  bt::ptime(d.meta->production_date.end()));
+    auto start = since ? *since: bt::ptime(d.meta->production_date.begin());
+    auto end = until ? *until:  bt::ptime(d.meta->production_date.end());
     pb_creator.action_period = bt::time_period(start, end);
 
     if (end < start) {
