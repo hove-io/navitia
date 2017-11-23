2d2a3dfe37d8c32971a82fffef35b471e6f33b1d
TURCK Erwan <erwan.turck@tisseo.fr>
2017-11-23 15:52:57 +0100

    Jormungandr: move schedule update methods in the proxy
    Allow us to have a custom merging between the datas from kraken and the
    datas from the proxy. If we just want the realtime proxy datas to be
    active for one day or even one hour, the proxy can define which datas
    from kraken it is removing and define how it does the merging.
    An example is in proxy_realtime_tests.

--- source/jormungandr/jormungandr/realtime_schedule/realtime_proxy.py
+++ source/jormungandr/jormungandr/realtime_schedule/realtime_proxy.py
@@ -30,8 +30,15 @@
 # www.navitia.io
 from __future__ import absolute_import, print_function, unicode_literals, division
 from abc import abstractmethod, ABCMeta
+from copy import deepcopy
+from jormungandr.schedule import RoutePoint
 from jormungandr.utils import timestamp_to_datetime, record_external_failure
+from jormungandr.utils import date_to_timestamp, pb_del_if
 from jormungandr import new_relic
+from navitiacommon import type_pb2
+import datetime
+import hashlib
+import logging
 import six
 
 class RealtimeProxyError(RuntimeError):
@@ -94,6 +101,83 @@ class RealtimeProxy(six.with_metaclass(ABCMeta, object)):
         except RealtimeProxyError as e:
             self.record_call('failure', reason=str(e))
             return None
+
+    # Method used to filter schedules from kraken before merging. By default remove all schedules.
+    # Overload this to keep some and mix kraken and proxy datas.
+    def _filter_base_stop_schedule(self, date_time):
+        return True
+
+    def _update_stop_schedule(self, stop_schedule, next_realtime_passages):
+        """
+        Update the stopschedule response with the new realtime passages
+
+        By default we remove all base schedule data and replace them with the realtime
+        Each proxy can define is own way to merge passages
+
+        If next_realtime_passages is None (and not if it's []) it means that the proxy failed,
+        so we use the base schedule
+        """
+        if next_realtime_passages is None:
+            return
+
+        logging.getLogger(__name__).debug('next passages: : {}'
+                                         .format(["dt: {}".format(d.datetime) for d in next_realtime_passages]))
+
+        # we clean up the old schedule
+        pb_del_if(stop_schedule.date_times, self._filter_base_stop_schedule)
+        for passage in next_realtime_passages:
+            new_dt = stop_schedule.date_times.add()
+            # the midnight is calculated from passage.datetime and it keeps the same timezone as passage.datetime
+            midnight = passage.datetime.replace(hour=0, minute=0, second=0, microsecond=0)
+            time = (passage.datetime - midnight).total_seconds()
+            new_dt.time = int(time)
+            new_dt.date = date_to_timestamp(midnight)
+
+            if passage.is_real_time:
+                new_dt.realtime_level = type_pb2.REALTIME
+            else:
+                new_dt.realtime_level = type_pb2.BASE_SCHEDULE
+
+            # we also add the direction in the note
+            if passage.direction:
+                note = type_pb2.Note()
+                note.note = passage.direction
+                note_uri = hashlib.md5(note.note.encode('utf-8')).hexdigest()
+                note.uri = 'note:{md5}'.format(md5=note_uri)  # the id is a md5 of the direction to factorize them
+                new_dt.properties.notes.extend([note])
+        stop_schedule.date_times.sort(key=lambda dt: dt.date + dt.time)
+
+    # By default filter passage if they are on the same route point
+    def _filter_base_passage(self, passage, route_point):
+        return RoutePoint(passage.route, passage.stop_point) == route_point
+
+    def _update_passages(self, passages, route_point, template, next_realtime_passages):
+        if next_realtime_passages is None:
+            return
+
+        # filter passages with proxy method
+        pb_del_if(passages, lambda passage: self._filter_base_passage(passage, route_point))
+
+        # append the realtime passages
+        for rt_passage in next_realtime_passages:
+            new_passage = deepcopy(template)
+            new_passage.stop_date_time.arrival_date_time = date_to_timestamp(rt_passage.datetime)
+            new_passage.stop_date_time.departure_date_time = date_to_timestamp(rt_passage.datetime)
+
+            if rt_passage.is_real_time:
+                new_passage.stop_date_time.data_freshness = type_pb2.REALTIME
+            else:
+                new_passage.stop_date_time.data_freshness = type_pb2.BASE_SCHEDULE
+
+            # we also add the direction in the note
+            if rt_passage.direction:
+                new_passage.pt_display_informations.direction = rt_passage.direction
+
+            # we add physical mode from route
+            if len(route_point.pb_route.physical_modes) > 0:
+                new_passage.pt_display_informations.physical_mode = route_point.pb_route.physical_modes[0].name
+
+            passages.extend([new_passage])
 
     @abstractmethod
     def status(self):
