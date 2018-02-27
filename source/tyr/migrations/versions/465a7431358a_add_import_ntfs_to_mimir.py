b10372a767c410fd474cba60357bd36c52594f3a
Benoit BRISSET <benoit.brisset@gmail.com>
2018-02-27 14:52:49 +0100

    Update bdd revision after rebase
    clean code

--- source/tyr/migrations/versions/465a7431358a_add_import_ntfs_to_mimir.py
+++ source/tyr/migrations/versions/465a7431358a_add_import_ntfs_to_mimir.py
@@ -1,23 +1,24 @@
-"""empty message
+"""Add import_ntfs_to_mimir field in instance
+
+Adding new bool flag to activate the data loading
+into mimir, when files are ntfs.
 
 Revision ID: 465a7431358a
-Revises: 3acdde6329db
+Revises: 242138b08c92
 Create Date: 2018-02-23 14:50:23.086805
 
 """
 
 # revision identifiers, used by Alembic.
 revision = '465a7431358a'
-down_revision = '3acdde6329db'
+down_revision = '242138b08c92'
 
 from alembic import op
 import sqlalchemy as sa
 
 
 def upgrade():
-    op.add_column('instance', sa.Column('import_ntfs_in_mimir', sa.Boolean(), nullable=True))
-    op.execute('update instance set import_ntfs_in_mimir=false')
-    op.alter_column('instance', 'import_ntfs_in_mimir', nullable=False)
+    op.add_column('instance', sa.Column('import_ntfs_in_mimir', sa.Boolean(), nullable=False, server_default='False'))
 
 
 def downgrade():
