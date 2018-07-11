e7c2a10e1f20ccc6d24aa37b3badb7991fc78bbe
Guillaume P <texitoi@texitoi.eu>
2018-07-11 15:19:16 +0200

    add test for since, until, odt_level and forbidden_uris in ptref_ng

--- source/ptreferential/tests/ptref_ng_test.cpp
+++ source/ptreferential/tests/ptref_ng_test.cpp
@@ -34,8 +34,11 @@ www.navitia.io
 #include <boost/test/unit_test.hpp>
 #include "ptreferential/ptreferential_ng.h"
 #include "ptreferential/ptreferential.h"
+#include "ed/build_helper.h"
 
 using namespace navitia::ptref;
+using navitia::type::Type_e;
+using navitia::type::OdtLevel_e;
 
 static void assert_expr(const std::string& to_parse, const std::string& expected) {
     const ast::Expr expr = parse(to_parse);
@@ -131,4 +134,108 @@ BOOST_AUTO_TEST_CASE(parse_legacy_tests) {
     assert_expr(R"(stop_areas.uri="bob the \\ er")", R"(stop_areas.uri("bob the \\ er"))");
     assert_expr(R"(vehicle_journey.has_headsign("john"))", R"(vehicle_journey.has_headsign("john"))");
     assert_expr(R"(vehicle_journey.has_disruption())", R"(vehicle_journey.has_disruption())");
+}
+
+void assert_odt_level(const Type_e requested_type,
+                      const std::string& request,
+                      const OdtLevel_e odt_level,
+                      const std::string& expected) {
+    const auto req = make_request(requested_type, request, {}, odt_level, {}, {}, {});
+    assert_expr(req, expected);
+}
+
+BOOST_AUTO_TEST_CASE(make_request_odt_level) {
+    assert_odt_level(Type_e::VehicleJourney, "", OdtLevel_e::all, "all");
+    assert_odt_level(Type_e::VehicleJourney, "", OdtLevel_e::zonal, "all");
+    assert_odt_level(Type_e::Line, "", OdtLevel_e::all, "all");
+    assert_odt_level(Type_e::Line, "", OdtLevel_e::zonal, "(all AND line.odt_level(\"zonal\"))");
+    assert_odt_level(Type_e::Line, "all or all", OdtLevel_e::zonal,
+                     "((all OR all) AND line.odt_level(\"zonal\"))");
+}
+
+void assert_since_until(const Type_e requested_type,
+                        const std::string& request,
+                        const std::string& since_str,
+                        const std::string& until_str,
+                        const std::string& expected) {
+    boost::optional<boost::posix_time::ptime> since, until;
+    if (!since_str.empty()) { since = boost::posix_time::from_iso_string(since_str); }
+    if (!until_str.empty()) { until = boost::posix_time::from_iso_string(until_str); }
+    const auto req = make_request(requested_type, request, {}, OdtLevel_e::all, since, until, {});
+    assert_expr(req, expected);
+}
+
+BOOST_AUTO_TEST_CASE(make_request_since_until) {
+    assert_since_until(Type_e::Line, "", "", "", "all");
+    assert_since_until(Type_e::Line, "", "20180714T1337", "20180714T1338", "all");
+    assert_since_until(
+        Type_e::VehicleJourney, "", "20180714T1337", "20180714T1338",
+        R"((all AND vehicle_journey.between("20180714T133700Z", "20180714T133800Z")))"
+    );
+    assert_since_until(
+        Type_e::VehicleJourney, "", "20180714T1337", "",
+        R"((all AND vehicle_journey.since("20180714T133700Z")))"
+    );
+    assert_since_until(
+        Type_e::VehicleJourney, "", "", "20180714T1338",
+        R"((all AND vehicle_journey.until("20180714T133800Z")))"
+    );
+    assert_since_until(
+        Type_e::Impact, "", "20180714T1337", "20180714T1338",
+        R"((all AND disruption.between("20180714T133700Z", "20180714T133800Z")))"
+    );
+    assert_since_until(
+        Type_e::Impact, "", "20180714T1337", "",
+        R"((all AND disruption.since("20180714T133700Z")))"
+    );
+    assert_since_until(
+        Type_e::Impact, "", "", "20180714T1338",
+        R"((all AND disruption.until("20180714T133800Z")))"
+    );
+    assert_since_until(
+        Type_e::Impact, "all or all", "", "20180714T1338",
+        R"(((all OR all) AND disruption.until("20180714T133800Z")))"
+    );
+}
+
+void assert_forbidden(const Type_e requested_type,
+                      const std::string& request,
+                      const std::vector<std::string>& forbidden,
+                      const std::string& expected) {
+    ed::builder b("20180710");
+    b.generate_dummy_basis();
+    b.finish();
+    const auto req = make_request(requested_type, request, forbidden, OdtLevel_e::all, {}, {}, *b.data);
+    assert_expr(req, expected);
+}
+
+BOOST_AUTO_TEST_CASE(make_request_forbidden_uris) {
+    assert_forbidden(Type_e::Line, "", {}, "all");
+    assert_forbidden(Type_e::Line, "", {"Bus"}, R"((all - commercial_mode.id("Bus")))");
+    assert_forbidden(Type_e::Line, "", {"Bus", "0x1"},
+                     R"((all - (commercial_mode.id("Bus") OR commercial_mode.id("0x1"))))");
+    assert_forbidden(Type_e::Line, "all or all", {"Bus", "0x1"},
+                     R"(((all OR all) - (commercial_mode.id("Bus") OR commercial_mode.id("0x1"))))");
+}
+
+BOOST_AUTO_TEST_CASE(make_request_odt_level_forbidden_uris) {
+    ed::builder b("20180710");
+    b.generate_dummy_basis();
+    b.finish();
+    const auto req = make_request(Type_e::Line, "all or all", {"Bus", "0x1"}, OdtLevel_e::zonal, {}, {}, *b.data);
+    assert_expr(req, R"((((all OR all) AND line.odt_level("zonal")) - (commercial_mode.id("Bus") OR commercial_mode.id("0x1"))))");
+}
+
+BOOST_AUTO_TEST_CASE(make_request_since_forbidden_uris) {
+    ed::builder b("20180710");
+    b.generate_dummy_basis();
+    b.finish();
+    const auto req = make_request(Type_e::Impact,
+                                  "all or all",
+                                  {"Bus", "0x1"},
+                                  OdtLevel_e::all,
+                                  boost::posix_time::from_iso_string("20180714T1337"),
+                                  {},
+                                  *b.data);
+    assert_expr(req, R"((((all OR all) AND disruption.since("20180714T133700Z")) - (commercial_mode.id("Bus") OR commercial_mode.id("0x1"))))");
 }
