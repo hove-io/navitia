a4ea35c9b22d87f097c5d0cdd9b584ff2d3fb6f3
mbounabi <Mehdi.Bounabi@Kisio.org>
2018-07-19 15:32:18 +0200

    First draft: readme + new tests

--- gatling/readme.md
+++ gatling/readme.md
@@ -1,8 +1,24 @@
+To test the load and performance of the different Navitia scenarios, the framework Gatling is used
+
 How to run
 ===========
 
-gatling must be run from its directory
+Gatling can be downloaded from the gatling.io page and is run with an executable script.
+Here's an example of command to run the Navitia tests:
+```
+<PATHTO>/bin/gatling.sh -df <PATHTO>/navitia/gatling/data -sf <PATHTO>/navitia/gatling/simulations
+```
+This command will show a list of simulations, based on the file found in the folder "simulations". In the command above, it is defined with the argument -sf (note that there can be default simulations found in the downloaded gatling folder).
+A file with the arguments of the requests to run is provided with the argument -df.
 
-```
-./bin/gatling.sh -df <PATHTO>/navitia/gatling/data -sf <PATHTO>/navitia/gatling/simulations
-```
+Once executed, select the simulation number and the next steps can be skipped in our case:'simulation id' and 'description' (can be used to specify the commit id tested)
+
+At the end of the simulation, a stats html page is generated.
+
+The navitia simulations require to have running coverage (Jormungandr and Kraken) locally.
+Note: to reproduce a behaviour similar to the prod platform, it is necessary to launch several processes of Jormun using uwsgi.
+
+How to add tests
+================
+
+The requests done for the simulations are '/journeys'. You can find in the data file (argument -df in the command above) the data needed for the requests. To add a test, add a line in the data file filling each argument as described in the first line of the file.
--- gatling/simulations/navitia/distributed.scala
+++ gatling/simulations/navitia/distributed.scala
@@ -9,7 +9,7 @@ object Base {
         .baseURL("http://localhost:5000/v1/coverage") //jormun
         .acceptEncodingHeader("gzip, deflate")
 
-  def query(scn: String) = {
+  def query(scn: String, first_mode: String, last_mode:String) = {
     val requests = csv("journeys_fr-idf.csv").circular
       exec().feed(requests).exec(
         http("${type}")
@@ -18,15 +18,60 @@ object Base {
           .queryParam("to", "${to}")
           .queryParam("datetime", "${datetime}")
           .queryParam("datetime_represents", "${datetime_represents}")
+          .queryParam("first_section_mode[]", first_mode)
+          .queryParam("last_section_mode[]", last_mode)
           .queryParam("_override_scenario", scn)
           .check(status.is(200)))
   }
 }
 
-class Distributed extends Simulation {
-    setUp(scenario("distributed").exec(Base.query("distributed")).inject(constantUsersPerSec(1) during(300) randomized).protocols(Base.httpConf))
+class Distributed_Walking_Walking extends Simulation {
+  setUp(scenario("Distributed_Walking_Walking").exec(Base.query("distributed", "walking", "walking"))
+      .inject(constantUsersPerSec(1) during(300) randomized).protocols(Base.httpConf))
 }
 
-class NewDefault extends Simulation {
-    setUp(scenario("new_default").exec(Base.query("new_default")).inject(constantUsersPerSec(1) during(300) randomized).protocols(Base.httpConf))
+class Distributed_Walking_Bike extends Simulation {
+    setUp(scenario("Distributed_Walking_Bike").exec(Base.query("distributed", "walking", "bike"))
+      .inject(constantUsersPerSec(1) during(300) randomized).protocols(Base.httpConf))
 }
+
+class Distributed_Bike_Walking extends Simulation {
+    setUp(scenario("Distributed_Bike_Walking").exec(Base.query("distributed", "bike", "walking"))
+      .inject(constantUsersPerSec(1) during(300) randomized).protocols(Base.httpConf))
+}
+
+class Distributed_Car_Walking extends Simulation {
+    setUp(scenario("Distributed_Car_Walking").exec(Base.query("distributed", "car", "walking"))
+      .inject(constantUsersPerSec(1) during(300) randomized).protocols(Base.httpConf))
+}
+
+class Distributed_Walking_Car extends Simulation {
+    setUp(scenario("Distributed_Walking_Car").exec(Base.query("distributed", "walking", "car"))
+      .inject(constantUsersPerSec(1) during(300) randomized).protocols(Base.httpConf))
+}
+
+class NewDefault_Walking_Walking extends Simulation {
+    setUp(scenario("NewDefault_Walking_Walking").exec(Base.query("new_default", "walking", "walking"))
+      .inject(constantUsersPerSec(1) during(300) randomized).protocols(Base.httpConf))
+}
+
+class NewDefault_Walking_Bike extends Simulation {
+    setUp(scenario("NewDefault_Walking_Bike").exec(Base.query("new_default", "walking", "bike"))
+      .inject(constantUsersPerSec(1) during(300) randomized).protocols(Base.httpConf))
+}
+
+class NewDefault_Bike_Walking extends Simulation {
+    setUp(scenario("NewDefault_Bike_Walking").exec(Base.query("new_default", "bike", "walking"))
+      .inject(constantUsersPerSec(1) during(300) randomized).protocols(Base.httpConf))
+}
+
+class NewDefault_Car_Walking extends Simulation {
+    setUp(scenario("NewDefault_Car_Walking").exec(Base.query("new_default", "car", "walking"))
+      .inject(constantUsersPerSec(1) during(300) randomized).protocols(Base.httpConf))
+}
+
+class NewDefault_Walking_Car extends Simulation {
+    setUp(scenario("NewDefault_Walking_Car").exec(Base.query("new_default", "walking", "car"))
+      .inject(constantUsersPerSec(1) during(300) randomized).protocols(Base.httpConf))
+}
+
