f31eca8e9dd68d5f3335f3de8ed3ade224a54703
Alexandre Jacquin <alexandre.jacquin@canaltp.fr>
2018-08-16 08:45:59 +0200

    Jormun: fix regression on coord format
    Not sure that `g` suffix is what we want  as it may output float
    formatted with exponent.
    
    > General format. For a given precision p >= 1, this rounds the number to p significant digits and then formats the result in either fixed-point format or in scientific notation, depending on its magnitude.
    > from https://docs.python.org/2/library/string.html

--- source/jormungandr/jormungandr/interfaces/v1/serializer/base.py
+++ source/jormungandr/jormungandr/interfaces/v1/serializer/base.py
@@ -296,7 +296,7 @@ class DoubleToStringField(Field):
 
     def to_value(self, value):
         # we don't want to loose precision while converting a double to string
-        return '%.16f' % value
+        return '%.16g' % value
 
 
 class DescribedField(LambdaField):
