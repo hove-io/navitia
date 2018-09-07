7a970150f08cb7b457227f62d4d8524d511f1ed9
Nonifier <olivier.georget@kisio.org>
2018-09-07 15:28:14 +0200

    Handle multiple subrepo in check_submodules.sh

--- source/scripts/check_submodules.sh
+++ source/scripts/check_submodules.sh
@@ -1,66 +1,29 @@
 #!/bin/bash
 
 red=`tput setaf 1`
+reset=`tput sgr0`
+_errors=("")
+
+function _add_error() {
+	_errors=("${_errors[@]}" "${red}$1")
+}
+
+function _check_error() {
+	err_num=${#_errors[@]}
+	if [ $err_num -gt 0 ]; then
+		i=0
+		while [[ $i -lt $err_num ]]; do
+			echo "${_errors[$i]}"
+			((i=i+1))
+		done
+		echo "${reset}"
+		exit 1
+	fi
+}
 
 submodules=`git submodule | tr -s \  : `  # trim the spaces, and convert them to colon
 
-echo "CI : $CI"
-echo "TRAVIS : $TRAVIS"
-echo "CONTINUOUS_INTEGRATION : $CONTINUOUS_INTEGRATION"
-echo "DEBIAN_FRONTEND : $DEBIAN_FRONTEND"
-echo "HAS_JOSH_K_SEAL_OF_APPROVAL : $HAS_JOSH_K_SEAL_OF_APPROVAL"
-echo "USER : $USER"
-echo "HOME : $HOME"
-echo "LANG : $LANG"
-echo "LC_ALL : $LC_ALL"
-echo "RAILS_ENV : $RAILS_ENV"
-echo "RACK_ENV : $RACK_ENV"
-echo "MERB_ENV : $MERB_ENV"
-echo "JRUBY_OPTS : $JRUBY_OPTS"
-echo "JAVA_HOME : $JAVA_HOME"
-echo "TRAVIS_ALLOW_FAILURE : $TRAVIS_ALLOW_FAILURE"
-echo "TRAVIS_BRANCH : $TRAVIS_BRANCH"
-echo "TRAVIS_BUILD_DIR : $TRAVIS_BUILD_DIR"
-echo "TRAVIS_BUILD_ID : $TRAVIS_BUILD_ID"
-echo "TRAVIS_BUILD_NUMBER : $TRAVIS_BUILD_NUMBER"
-echo "TRAVIS_COMMIT : $TRAVIS_COMMIT"
-echo "TRAVIS_COMMIT_MESSAGE : $TRAVIS_COMMIT_MESSAGE"
-echo "TRAVIS_COMMIT_RANGE : $TRAVIS_COMMIT_RANGE"
-echo "TRAVIS_EVENT_TYPE : $TRAVIS_EVENT_TYPE"
-echo "TRAVIS_JOB_ID : $TRAVIS_JOB_ID"
-echo "TRAVIS_JOB_NUMBER : $TRAVIS_JOB_NUMBER"
-echo "TRAVIS_OS_NAME : $TRAVIS_OS_NAME"
-echo "TRAVIS_OSX_IMAGE : $TRAVIS_OSX_IMAGE"
-echo "TRAVIS_PULL_REQUEST : $TRAVIS_PULL_REQUEST"
-echo "TRAVIS_PULL_REQUEST_BRANCH : $TRAVIS_PULL_REQUEST_BRANCH"
-echo "TRAVIS_PULL_REQUEST_SHA : $TRAVIS_PULL_REQUEST_SHA"
-echo "TRAVIS_PULL_REQUEST_SLUG : $TRAVIS_PULL_REQUEST_SLUG"
-echo "TRAVIS_SECURE_ENV_VARS : $TRAVIS_SECURE_ENV_VARS"
-echo "TRAVIS_TEST_RESULT : $TRAVIS_TEST_RESULT"
-echo "TRAVIS_TAG : $TRAVIS_TAG"
-echo "TRAVIS_BUILD_STAGE_NAME : $TRAVIS_BUILD_STAGE_NAME"
-echo "TRAVIS_DART_VERSION : $TRAVIS_DART_VERSION"
-echo "TRAVIS_GO_VERSION : $TRAVIS_GO_VERSION"
-echo "TRAVIS_HAXE_VERSION : $TRAVIS_HAXE_VERSION"
-echo "TRAVIS_JDK_VERSION : $TRAVIS_JDK_VERSION"
-echo "TRAVIS_JULIA_VERSION : $TRAVIS_JULIA_VERSION"
-echo "TRAVIS_NODE_VERSION : $TRAVIS_NODE_VERSION"
-echo "TRAVIS_OTP_RELEASE : $TRAVIS_OTP_RELEASE"
-echo "TRAVIS_PERL_VERSION : $TRAVIS_PERL_VERSION"
-echo "TRAVIS_PHP_VERSION : $TRAVIS_PHP_VERSION"
-echo "TRAVIS_PYTHON_VERSION : $TRAVIS_PYTHON_VERSION"
-echo "TRAVIS_R_VERSION : $TRAVIS_R_VERSION"
-echo "TRAVIS_RUBY_VERSION : $TRAVIS_RUBY_VERSION"
-echo "TRAVIS_RUST_VERSION : $TRAVIS_RUST_VERSION"
-echo "TRAVIS_SCALA_VERSION : $TRAVIS_SCALA_VERSION"
-echo "TRAVIS_MARIADB_VERSION : $TRAVIS_MARIADB_VERSION"
-echo "TRAVIS_XCODE_SDK : $TRAVIS_XCODE_SDK"
-echo "TRAVIS_XCODE_SCHEME : $TRAVIS_XCODE_SCHEME"
-echo "TRAVIS_XCODE_PROJECT : $TRAVIS_XCODE_PROJECT"
-echo "TRAVIS_XCODE_WORKSPACE : $TRAVIS_XCODE_WORKSPACE"
-
-for submod in $submodules
-do
+for submod in $submodules; do
 	submod_len=${#submod}
 	module=${submod:1:$submod_len} # remove the first character that can be a '+' or '-'
 
@@ -75,12 +38,12 @@ do
 		is_merged=`git branch --all --merged HEAD --no-color | grep $remote_ref`
 		contained=`git branch --all --contains HEAD --no-color | grep $remote_ref`
 		if [ -z "$is_merged" -a -z "$contained" ]; then
-		  echo "${red}A submodule points to a commit not merged to its head ! "
-		  echo "${red} > submodule: $module_path"
-		  echo "${red} > commit: $sha1"
-		  echo "${red} > HEAD: $remote_ref"
-		  exit 1
+			_add_error "Submodule $module_path points to a commit not merged to its head ! "
+			_add_error " > submodule: $module_path"
+			_add_error " > commit: $sha1"
+			_add_error " > HEAD: $remote_ref"
 		fi
 	popd > /dev/null
 done
 
+_check_error
