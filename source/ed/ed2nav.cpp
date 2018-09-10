e78db23b62414cfd2f9dc20095c615780d9beb11
Benoit Brisset <benoit.brisset@gmail.com>
2018-09-10 09:32:35 +0200

    add syslog to test

--- source/ed/ed2nav.cpp
+++ source/ed/ed2nav.cpp
@@ -1,28 +1,28 @@
 /* Copyright © 2001-2014, Canal TP and/or its affiliates. All rights reserved.
-  
+
 This file is part of Navitia,
     the software to build cool stuff with public transport.
- 
+
 Hope you'll enjoy and contribute to this project,
     powered by Canal TP (www.canaltp.fr).
 Help us simplify mobility and open public transport:
     a non ending quest to the responsive locomotion way of traveling!
-  
+
 LICENCE: This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU Affero General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
-   
+
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 GNU Affero General Public License for more details.
-   
+
 You should have received a copy of the GNU Affero General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
-  
+
 Stay tuned using
-twitter @navitia 
+twitter @navitia
 IRC #navitia on freenode
 https://groups.google.com/d/forum/navitia
 www.navitia.io
@@ -162,8 +162,6 @@ struct FindAdminWithCities {
 
 int ed2nav(int argc, const char * argv[])
 {
-    navitia::init_app();
-    auto logger = log4cplus::Logger::getInstance("log");
     std::string output, connection_string, region_name, cities_connection_string;
     double min_non_connected_graph_ratio;
     po::options_description desc("Allowed options");
@@ -184,7 +182,8 @@ int ed2nav(int argc, const char * argv[])
         ("connection-string", po::value<std::string>(&connection_string)->required(),
          "database connection parameters: host=localhost user=navitia dbname=navitia password=navitia")
         ("cities-connection-string", po::value<std::string>(&cities_connection_string)->default_value(""),
-         "cities database connection parameters: host=localhost user=navitia dbname=cities password=navitia");
+         "cities database connection parameters: host=localhost user=navitia dbname=cities password=navitia")
+        ("active_syslog,l", "Active log redirection in local syslog");
 
     po::variables_map vm;
     po::store(po::parse_command_line(argc, argv, desc), vm);
@@ -196,6 +195,13 @@ int ed2nav(int argc, const char * argv[])
         return 0;
     }
 
+    if(vm.count("active_syslog")){
+        init_logger(true);
+    } else {
+        init_logger();
+    }
+    auto logger = log4cplus::Logger::getInstance("log");
+    navitia::init_signal_handling();
 
     if(vm.count("config-file")){
         std::ifstream stream;
