4ffd08843a668320f916a49cb3be1262eae1d1ec
Benoit Brisset <benoit.brisset@gmail.com>
2018-09-12 11:36:52 +0200

    update ed binaries

--- source/ed/fusio2ed.cpp
+++ source/ed/fusio2ed.cpp
@@ -1,28 +1,28 @@
 /* Copyright © 2001-2014, Canal TP and/or its affiliates. All rights reserved.
-  
+
 This file is part of Navitia,
     the software to build cool stuff with public transport.
- 
+
 Hope you'll enjoy and contribute to this project,
     powered by Canal TP (www.canaltp.fr).
 Help us simplify mobility and open public transport:
     a non ending quest to the responsive locomotion way of traveling!
-  
+
 LICENCE: This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU Affero General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
-   
+
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 GNU Affero General Public License for more details.
-   
+
 You should have received a copy of the GNU Affero General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
-  
+
 Stay tuned using
-twitter @navitia 
+twitter @navitia
 IRC #navitia on freenode
 https://groups.google.com/d/forum/navitia
 www.navitia.io
@@ -49,9 +49,6 @@ namespace pt = boost::posix_time;
 
 int main(int argc, char * argv[])
 {
-    navitia::init_app();
-    auto logger = log4cplus::Logger::getInstance("log");
-
     std::string input, date, connection_string,
                 fare_dir;
     double simplify_tolerance;
@@ -68,7 +65,9 @@ int main(int argc, char * argv[])
         ("config-file", po::value<std::string>(), "Path to configuration file")
         ("connection-string", po::value<std::string>(&connection_string)->required(),
              "Database connection parameters: host=localhost "
-             "user=navitia dbname=navitia password=navitia");
+             "user=navitia dbname=navitia password=navitia")
+        ("local_syslog", "Active log redirection within local syslog")
+        ("log_comment", po::value<std::string>(), "optional field to add extra information like coverage name");
 
     po::variables_map vm;
     po::store(po::parse_command_line(argc, argv, desc), vm);
@@ -78,6 +77,12 @@ int main(int argc, char * argv[])
                   << navitia::config::navitia_build_type << std::endl;
         return 0;
     }
+
+    // Construct logger and signal handling
+    std::string log_comment = "";
+    if (vm.count("log_comment")) { log_comment = vm["log_comment"].as<std::string>(); }
+    navitia::init_app("ed2nav", "DEBUG", vm.count("local_syslog"), log_comment);
+    auto logger = log4cplus::Logger::getInstance("log");
 
     if(vm.count("config-file")){
         std::ifstream stream;
