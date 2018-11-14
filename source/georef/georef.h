5072ca36cce5a14af6d528962f6311987d58bf76
Benoit Brisset <benoit.brisset@gmail.com>
2018-11-14 16:33:49 +0100

    Change serialized data + increment version number

--- source/georef/georef.h
+++ source/georef/georef.h
@@ -1,28 +1,28 @@
 /* Copyright © 2001-2014, Canal TP and/or its affiliates. All rights reserved.
-  
+
 This file is part of Navitia,
     the software to build cool stuff with public transport.
- 
+
 Hope you'll enjoy and contribute to this project,
     powered by Canal TP (www.canaltp.fr).
 Help us simplify mobility and open public transport:
     a non ending quest to the responsive locomotion way of traveling!
-  
+
 LICENCE: This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU Affero General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
-   
+
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 GNU Affero General Public License for more details.
-   
+
 You should have received a copy of the GNU Affero General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
-  
+
 Stay tuned using
-twitter @navitia 
+twitter @navitia
 IRC #navitia on freenode
 https://groups.google.com/d/forum/navitia
 www.navitia.io
@@ -139,6 +139,7 @@ struct HouseNumber{
 struct Way :public nt::Nameable, nt::Header{
 public:
     std::string way_type;
+    bool visible;
     std::string comment;
     // liste des admins
     std::vector<Admin*> admin_list;
@@ -155,7 +156,7 @@ public:
     nt::GeographicalCoord projected_centroid(const Graph&) const;
     nt::MultiLineString make_multiline(const Graph&) const;
     template<class Archive> void serialize(Archive & ar, const unsigned int) {
-      ar & idx & name & comment & uri & way_type & admin_list & house_number_left & house_number_right & edges & geoms;
+      ar & idx & name & comment & uri & way_type & visible & admin_list & house_number_left & house_number_right & edges & geoms;
     }
     std::string get_label() const;
 
