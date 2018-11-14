71b5676ce8139b252434028e380e0a1de4201d59
Benoit Brisset <benoit.brisset@gmail.com>
2018-11-14 16:34:49 +0100

    Filter way with visible=false

--- source/georef/georef.cpp
+++ source/georef/georef.cpp
@@ -1,28 +1,28 @@
 /* Copyright © 2001-2014, Canal TP and/or its affiliates. All rights reserved.
-  
+
 This file is part of Navitia,
     the software to build cool stuff with public transport.
- 
+
 Hope you'll enjoy and contribute to this project,
     powered by Canal TP (www.canaltp.fr).
 Help us simplify mobility and open public transport:
     a non ending quest to the responsive locomotion way of traveling!
-  
+
 LICENCE: This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU Affero General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
-   
+
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 GNU Affero General Public License for more details.
-   
+
 You should have received a copy of the GNU Affero General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
-  
+
 Stay tuned using
-twitter @navitia 
+twitter @navitia
 IRC #navitia on freenode
 https://groups.google.com/d/forum/navitia
 www.navitia.io
@@ -484,7 +484,7 @@ void GeoRef::build_admin_map(){
     * Sinon le barycentre de la rue
 */
 std::vector<nf::Autocomplete<nt::idx_t>::fl_quality> GeoRef::find_ways(const std::string & str, const int nbmax, const int search_type, std::function<bool(nt::idx_t)> keep_element, const std::set<std::string>& ghostwords) const{
-    std::vector<nf::Autocomplete<nt::idx_t>::fl_quality> to_return;
+    std::vector<nf::Autocomplete<nt::idx_t>::fl_quality> to_return, results;
     boost::tokenizer<> tokens(str);
 
     int search_number = str_to_int(*tokens.begin());
@@ -504,9 +504,17 @@ std::vector<nf::Autocomplete<nt::idx_t>::fl_quality> GeoRef::find_ways(const std
         search_str = str;
     }
     if (search_type == 0){
-        to_return = fl_way.find_complete(search_str, nbmax, keep_element, ghostwords);
+        results = fl_way.find_complete(search_str, nbmax, keep_element, ghostwords);
     }else{
-        to_return = fl_way.find_partial_with_pattern(search_str, word_weight, nbmax, keep_element, ghostwords);
+        results = fl_way.find_partial_with_pattern(search_str, word_weight, nbmax, keep_element, ghostwords);
+    }
+
+    // filter with visible parameter
+    for(auto& item: results){
+       Way* way = this->ways[item.idx];
+       if (way->visible == true) {
+            to_return.push_back(item);
+       }
     }
 
     /// récupération des coordonnées du numéro recherché pour chaque rue
