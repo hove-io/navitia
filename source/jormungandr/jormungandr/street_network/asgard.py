7091a3436f1d6e1c000927d843d62f9c49686ec2
Aurelien <aurelien.laporte@canaltp.fr>
2019-01-16 14:55:54 +0100

    Making asgard connector inherit from kraken connector

--- source/jormungandr/jormungandr/street_network/asgard.py
+++ source/jormungandr/jormungandr/street_network/asgard.py
@@ -30,25 +30,22 @@
 from __future__ import absolute_import, print_function, unicode_literals, division
 import logging
 from jormungandr.exceptions import TechnicalError
-from jormungandr.utils import get_pt_object_coord
 
-from jormungandr.street_network.valhalla import Valhalla
+from jormungandr.street_network.kraken import Kraken
 
 from contextlib import contextmanager
 import queue
-from navitiacommon import response_pb2, request_pb2, type_pb2
+from navitiacommon import response_pb2
 from zmq import green as zmq
-import six
 
-def get_uri_pt_object(obj):
-    return 'coord:{c.lon}:{c.lat}'.format(c=get_pt_object_coord(obj))
 
-class Asgard(Valhalla):
+class Asgard(Kraken):
     def __init__(
         self, instance, service_url, asgard_socket, modes=[], id='asgard', timeout=10, api_key=None, **kwargs
     ):
         super(Asgard, self).__init__(instance, service_url, modes, id, timeout, api_key, **kwargs)
         self.asgard_socket = asgard_socket
+        self.timeout = timeout
         self._sockets = queue.Queue()
 
     def get_street_network_routing_matrix(self, origins, destinations, mode, max_duration, request, **kwargs):
@@ -58,53 +55,23 @@ class Asgard(Valhalla):
             "car": request['car_speed'],
             "bss": request['bss_speed'],
         }
-        req = request_pb2.Request()
-        req.requested_api = type_pb2.street_network_routing_matrix
 
-        for o in origins:
-            orig = req.sn_routing_matrix.origins.add()
-            orig.place = get_uri_pt_object(o)
-            orig.access_duration = 0
-        for d in destinations:
-            dest = req.sn_routing_matrix.destinations.add()
-            dest.place = get_uri_pt_object(d)
-            dest.access_duration = 0
-
-        req.sn_routing_matrix.mode = mode
-        req.sn_routing_matrix.speed = speed_switcher.get(mode, kwargs.get("walking"))
-        req.sn_routing_matrix.max_duration = max_duration
+        req = self._create_sn_routing_matrix_request(
+            origins, destinations, mode, max_duration, speed_switcher, **kwargs
+        )
 
         res = self._call_asgard(req)
         # TODO handle car park
-        if res.HasField('error'):
-            logging.getLogger(__name__).error('routing matrix query error {}'.format(res.error))
-            raise TechnicalError('routing matrix fail')
+        self._check_for_error_and_raise(res)
         return res.sn_routing_matrix
 
-    def _direct_path(self, mode, pt_object_origin, pt_object_destination, fallback_extremity, request, direct_path_type):
-        req = request_pb2.Request()
-        req.requested_api = type_pb2.direct_path
-        req.direct_path.origin.place = get_uri_pt_object(pt_object_origin)
-        req.direct_path.origin.access_duration = 0
-        req.direct_path.destination.place = get_uri_pt_object(pt_object_destination)
-        req.direct_path.destination.access_duration = 0
-        req.direct_path.datetime = fallback_extremity.datetime
-        req.direct_path.clockwise = fallback_extremity.represents_start
-        req.direct_path.streetnetwork_params.origin_mode = mode
-        req.direct_path.streetnetwork_params.destination_mode = mode
-        req.direct_path.streetnetwork_params.walking_speed = request['walking_speed']
-        req.direct_path.streetnetwork_params.max_walking_duration_to_pt = request['max_walking_duration_to_pt']
-        req.direct_path.streetnetwork_params.bike_speed = request['bike_speed']
-        req.direct_path.streetnetwork_params.max_bike_duration_to_pt = request['max_bike_duration_to_pt']
-        req.direct_path.streetnetwork_params.bss_speed = request['bss_speed']
-        req.direct_path.streetnetwork_params.max_bss_duration_to_pt = request['max_bss_duration_to_pt']
-        req.direct_path.streetnetwork_params.car_speed = request['car_speed']
-        req.direct_path.streetnetwork_params.max_car_duration_to_pt = request['max_car_duration_to_pt']
-        req.direct_path.streetnetwork_params.car_no_park_speed = request['car_no_park_speed']
-        req.direct_path.streetnetwork_params.max_car_no_park_duration_to_pt = request['max_car_no_park_duration_to_pt']
-
+    def _direct_path(
+        self, mode, pt_object_origin, pt_object_destination, fallback_extremity, request, direct_path_type
+    ):
+        req = self._create_direct_path_request(
+            mode, pt_object_origin, pt_object_destination, fallback_extremity, request
+        )
         return self._call_asgard(req)
-
 
     @contextmanager
     def socket(self, context):
