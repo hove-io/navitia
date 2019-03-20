ef4c733e6e17f5c9f7eb754f11463498f3194c03
Guillaume P <TeXitoi@users.noreply.github.com>
2019-03-20 17:30:36 +0100

    Update source/kraken/tests/realtime_test.cpp
    Co-Authored-By: benoit-bst <32099204+benoit-bst@users.noreply.github.com>

--- source/kraken/tests/realtime_test.cpp
+++ source/kraken/tests/realtime_test.cpp
@@ -3336,7 +3336,7 @@ BOOST_FIXTURE_TEST_CASE(trip_id_that_doesnt_exist_must_be_rejected_in_classical_
     // the new trip update with bad id is blocked directly
     navitia::handle_realtime("feed-1", timestamp, new_trip, *b.data, true, true);
     b.data->build_raptor();
-    BOOST_REQUIRE_EQUAL(pt_data.meta_vjs.size(), 1);
+    BOOST_CHECK_EQUAL(pt_data.meta_vjs.size(), 1);
     BOOST_REQUIRE_EQUAL(pt_data.meta_vjs.exists("vj_id_doesnt_exist"), false);
 }
 
