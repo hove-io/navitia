139ad19dab13c5fe422254c7bc7581767c01c6d1
antoine-de <antoine.desbordes@gmail.com>
2019-06-05 15:01:02 +0200

    Tyr: fix revision
    fix 'conflict' on revision with
    https://github.com/CanalTP/navitia/pull/2802

--- source/tyr/migrations/versions/38cf24479595_admins_from_cities_db.py
+++ source/tyr/migrations/versions/38cf24479595_admins_from_cities_db.py
@@ -1,13 +1,13 @@
 """Add a field to use cities in osm2ed
 
 Revision ID: 38cf24479595
-Revises: 35d86bbbe642
+Revises: 5a941f98789c
 Create Date: 2019-05-09 17:09:41.400340
 
 """
 
 revision = '38cf24479595'
-down_revision = '35d86bbbe642'
+down_revision = '5a941f98789c'
 
 from alembic import op
 import sqlalchemy as sa
