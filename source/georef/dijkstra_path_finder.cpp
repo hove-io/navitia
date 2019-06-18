b00d44ce8fd48a89e21265dc07c568e1b603f5f8
XL <patrick.qian@canaltp.fr>
2019-06-18 10:46:06 +0200

    fix complilation error

--- source/georef/dijkstra_path_finder.cpp
+++ source/georef/dijkstra_path_finder.cpp
@@ -177,6 +177,7 @@ routing::map_stop_point_duration DijkstraPathFinder::find_nearest_stop_points(
 struct ProjectionGetterOnFly {
     const GeoRef& geo_ref;
     const type::Mode_e mode = type::Mode_e::Walking;
+    ProjectionGetterOnFly(const GeoRef& geo_ref, const type::Mode_e mode) : geo_ref(geo_ref), mode(mode) {}
     const georef::ProjectionData operator()(const type::GeographicalCoord& coord) const {
         return georef::ProjectionData{coord, geo_ref, mode};
     }
