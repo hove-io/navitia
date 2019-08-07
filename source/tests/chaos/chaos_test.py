36f4d19ca234e45ddeef59264b78f4beda16351e
Nonifier <olivier.georget@kisio.org>
2019-08-07 16:31:39 +0200

    chaos_tests Clean-up

--- source/tests/chaos/chaos_test.py
+++ source/tests/chaos/chaos_test.py
@@ -50,23 +50,12 @@ def postgres_docker():
         yield chaos_docker
 
 
-@pytest.fixture(scope="session")
-def chaos_db_docker(postgres_docker):
-    """
-    when the docker is started, we init a chaos db based on a snapshot
-    """
-    db_cnx_params = postgres_docker.get_db_params().old_school_cnx_string()
-    with psycopg2.connect(db_cnx_params) as conn:
-        with conn.cursor() as cursor:
-            yield {'db': cursor, 'connection_str': db_cnx_params}
-
-
-def test_loading_disruption_from_chaos(chaos_db_docker):
+def test_loading_disruption_from_chaos(postgres_docker):
     chaos_tests_dir = os.getenv('CHAOS_DB_TESTS_BUILD_DIR', '.')
     chaos_tests_exec = normpath(join(chaos_tests_dir, 'chaos_db_tests'))
 
     assert isfile(chaos_tests_exec), "Couldn't find test executable : {}".format(chaos_tests_exec)
 
     # Give the cpp test a connection string to the db
-    os.environ['CHAOS_DB_CONNECTION_STR'] = chaos_db_docker['connection_str']
+    os.environ['CHAOS_DB_CONNECTION_STR'] = postgres_docker.get_db_params().old_school_cnx_string()
     assert subprocess.check_call([chaos_tests_exec]) == 0
