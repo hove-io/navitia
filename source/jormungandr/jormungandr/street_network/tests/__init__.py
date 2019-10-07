a19b81932b87ba96a16ac6ac729cbeb21d64e95b
Alexandre Jacquin <alexandre.jacquin@kisio.com>
2019-10-07 13:10:45 +0200

    fix import issue

--- source/jormungandr/jormungandr/street_network/tests/__init__.py
+++ source/jormungandr/jormungandr/street_network/tests/__init__.py
@@ -27,5 +27,7 @@
 # https://groups.google.com/d/forum/navitia
 # www.navitia.io
 
-from streetnetwork_test_utils import MockKraken
+from __future__ import absolute_import, print_function, unicode_literals, division
+
+from jormungandr.street_network.tests.streetnetwork_test_utils import MockKraken
 from jormungandr.street_network.tests.streetnetwork_backend_mock import StreetNetworkBackendMock
