e4d770769407454ad7f3711a9541a689536a1772
AurelienLP <aurelien.laporte@canaltp.fr>
2019-10-07 15:07:26 +0200

    Adding max_nb_crowfly_by_mode in status

--- source/jormungandr/jormungandr/interfaces/v1/serializer/status.py
+++ source/jormungandr/jormungandr/interfaces/v1/serializer/status.py
@@ -76,6 +76,7 @@ class ParametersSerializer(serpy.Serializer):
     max_car_direct_path_duration = Field(schema_type=int)
     max_taxi_direct_path_duration = Field(schema_type=int)
     max_ridesharing_direct_path_duration = Field(schema_type=int)
+    max_nb_crowfly_by_mode = Field(schema_type=dict)
 
 
 class TravelerProfilesSerializer(serpy.Serializer):
