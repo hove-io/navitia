5acca5f23272d3e8efb241a8d86a27afb83c815b
krishna <krishna.adhikari@kisio.com>
2020-05-04 10:40:35 +0200

    Code refactor to manage realtime

--- source/jormungandr/jormungandr/schedule.py
+++ source/jormungandr/jormungandr/schedule.py
@@ -341,12 +341,7 @@ class MixedSchedule(object):
 
         return resp
 
-    def terminus_schedules(self, request):
-        resp = self.__stop_times(request, api=type_pb2.terminus_schedules, departure_filter=request["filter"])
-
-        if request['data_freshness'] != RT_PROXY_DATA_FRESHNESS:
-            return resp
-
+    def _manage_realtime(self, request, schedules):
         futures = []
         pool = gevent.pool.Pool(self.instance.realtime_pool_size)
 
@@ -362,15 +357,22 @@ class MixedSchedule(object):
                     self._get_next_realtime_passages(rt_proxy, route_point, request),
                 )
 
-        for terminus_schedule in resp.terminus_schedules:
-            route_point = _get_route_point_from_stop_schedule(terminus_schedule)
+        for schedule in schedules:
+            route_point = _get_route_point_from_stop_schedule(schedule)
             rt_proxy = self._get_realtime_proxy(route_point)
             if rt_proxy:
-                futures.append(pool.spawn(worker, rt_proxy, route_point, request, terminus_schedule))
+                futures.append(pool.spawn(worker, rt_proxy, route_point, request, schedule))
 
         for future in gevent.iwait(futures):
-            rt_proxy, terminus_schedule, next_rt_passages = future.get()
-            rt_proxy._update_stop_schedule(terminus_schedule, next_rt_passages)
+            rt_proxy, schedule, next_rt_passages = future.get()
+            rt_proxy._update_stop_schedule(schedule, next_rt_passages)
+
+    def terminus_schedules(self, request):
+        resp = self.__stop_times(request, api=type_pb2.terminus_schedules, departure_filter=request["filter"])
+
+        if request['data_freshness'] != RT_PROXY_DATA_FRESHNESS:
+            return resp
+        self._manage_realtime(request, resp.terminus_schedules)
         return resp
 
     def departure_boards(self, request):
@@ -379,28 +381,5 @@ class MixedSchedule(object):
         if request['data_freshness'] != RT_PROXY_DATA_FRESHNESS:
             return resp
 
-        futures = []
-        pool = gevent.pool.Pool(self.instance.realtime_pool_size)
-
-        # Copy the current request context to be used in greenlet
-        reqctx = utils.copy_flask_request_context()
-
-        def worker(rt_proxy, route_point, request, stop_schedule):
-            # Use the copied request context in greenlet
-            with utils.copy_context_in_greenlet_stack(reqctx):
-                return (
-                    rt_proxy,
-                    stop_schedule,
-                    self._get_next_realtime_passages(rt_proxy, route_point, request),
-                )
-
-        for stop_schedule in resp.stop_schedules:
-            route_point = _get_route_point_from_stop_schedule(stop_schedule)
-            rt_proxy = self._get_realtime_proxy(route_point)
-            if rt_proxy:
-                futures.append(pool.spawn(worker, rt_proxy, route_point, request, stop_schedule))
-
-        for future in gevent.iwait(futures):
-            rt_proxy, stop_schedule, next_rt_passages = future.get()
-            rt_proxy._update_stop_schedule(stop_schedule, next_rt_passages)
+        self._manage_realtime(request, resp.stop_schedules)
         return resp
