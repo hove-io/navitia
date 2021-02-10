c8f79e4fbe0a3324330ed0015a9744d677f85953
xlqian <patrick.qian@kisio.com>
2021-02-10 10:35:53 +0100

    fix test

--- source/tests/mock-kraken/mock_kraken.h
+++ source/tests/mock-kraken/mock_kraken.h
@@ -74,7 +74,8 @@ struct mock_kraken {
         }
 
         // Launch only one thread for the tests
-        threads.create_thread(std::bind(&doWork, std::ref(context), std::ref(data_manager), conf, std::ref(metric), 0));
+        threads.create_thread(
+            std::bind(&doWork, std::ref(context), std::ref(data_manager), conf, std::ref(metric), "myhostname", 0));
 
         // Connect work threads to client threads via a queue
         do {
