08a9c8df41fd13c7b7caf5f717e1e3afb6de7f71
xlqian <patrick.qian@kisio.com>
2021-04-21 10:42:46 +0200

    revert useless modification

--- source/benchmark/config.py
+++ source/benchmark/config.py
@@ -1,10 +1,12 @@
 import datetime
 
 NAVITIA_API_URL = 'http://127.0.0.1:5000/v1'
-COVERAGE = 'transilien'
+COVERAGE = 'stif'
 TOKEN = ''
 DISTANT_BENCH_OUTPUT = 'Navitia-Multimodal-Bench-Output'
-OUTPUT_DIR = "{}/output/{}".format(DISTANT_BENCH_OUTPUT, COVERAGE)
+OUTPUT_DIR = "{}/output/{}/{}".format(
+    DISTANT_BENCH_OUTPUT, COVERAGE, datetime.datetime.now().strftime("%Y%m%dT%H%M%S")
+)
 
 # setup logger
 import logging
