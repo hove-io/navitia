70da997386e898f4ca22eb50b776b43a3e0e9f65
krishna <krishna.adhikari@kisio.com>
2021-07-16 15:00:20 +0200

    Diagram and comments modified

--- source/ed/ed2nav.cpp
+++ source/ed/ed2nav.cpp
@@ -147,30 +147,24 @@ struct FindAdminWithCities {
 
     result_type get_admins_from_cities(const navitia::type::GeographicalCoord& c, georef::AdminRtree& admin_tree) {
         /*
-            For all admins that contain the coordinate in their boundary shape,
-            we want to search in db for their own inner admins as well.
+            We only fetch admins containings the coordinate in their boundary shape
+            because a left join between above result and another query using boundary shape
+            is very very slow.
 
-            +-----------------------------+
-            |A                            |
-            |   +-------+      +-------+  |
-            |   |B      |      |C      |  |
-            |   |       |      |   X   |  |
-            |   |       |      +-------+  |
-            |   |   Y   |  +------+       |
-            |   |       |  |D     |       |
-            |   +-------+  |      |       |
-            |              +------+       |
-            +-----------------------------+
+            +--------------------+
+            |A                   |
+            |                    |
+            |                    |
+            |                  +-|-------------+
+            |                  |X|             |
+            +------------------|+       B      |
+                               |               |
+                               |               |
+                               +---------------+
 
             For instance, when looking at 'X', we want:
-                - admins {A, C} to be returned
-                - BUT admins {A, B, C, D} added in the cache
+                - admins {A, B} to be returned and will be added in the cache
 
-            This is so that when looking at 'Y':
-                - admins {A, B} to be returned by the cache
-
-            If we don't search for all inner admins, looking for 'Y' would result in:
-                - admin {A} to be only returned by the cache
         */
         const auto sql_req = boost::format(R"sql(
             SELECT
