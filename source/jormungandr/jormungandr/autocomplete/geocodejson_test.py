7492ff36b3595103c507f4d4e91b8b94718723fc
Abderrahim AZIME <azime_1@yahoo.fr>
2021-09-22 14:36:47 +0200

    [Jormungandr] Timeout separation for Bragi and Elasticsearch

--- source/jormungandr/jormungandr/autocomplete/geocodejson_test.py
+++ source/jormungandr/jormungandr/autocomplete/geocodejson_test.py
@@ -36,7 +36,13 @@ import pytest
 import requests_mock
 
 
-def test_geocodejson_check_response(mock):
+class FakeInstance:
+    def __init__(self, name="test", poi_dataset="priv.test"):
+        self.name = name
+        self.poi_dataset = poi_dataset
+
+
+def test_geocodejson_check_response():
     url = 'https://geocode.json'
     with requests_mock.Mocker() as m:
         m.get('https://geocode.json/features/my_method', status_code=42, text='{"msg": "this is a response"}')
@@ -48,3 +54,35 @@ def test_geocodejson_check_response(mock):
 
         assert "42" in str(exc)
         assert "this is a response" in str(exc)
+
+
+def test_geocodejson_config_default_timeouts():
+    url = 'https://geocode.json'
+    gj = GeocodeJson(host=url)
+    assert gj.timeout == 2
+    assert gj.timeout_bragi_es == 2
+
+    assert gj.fast_timeout == 0.2
+    assert gj.fast_timeout_bragi_es == 0.2
+
+    request = {"q": "abcd", "count": 10, "type[]": ["stop_area"], "request_id": "001"}
+    params = gj.make_params(request, [FakeInstance()])
+
+    timeout_bragi_es = next((param[1] for param in params if param[0] == 'timeout'), None)
+    assert timeout_bragi_es == 2000
+
+
+def test_geocodejson_config_timeout_bragi_es():
+    url = 'https://geocode.json'
+    gj = GeocodeJson(host=url, timeout_bragi_es=1.8)
+    assert gj.timeout == 2
+    assert gj.timeout_bragi_es == 1.8
+
+    assert gj.fast_timeout == 0.2
+    assert gj.fast_timeout_bragi_es == 0.2
+
+    request = {"q": "abcd", "count": 10, "type[]": ["stop_area"], "request_id": "001"}
+    params = gj.make_params(request, [FakeInstance()])
+
+    timeout_bragi_es = next((param[1] for param in params if param[0] == 'timeout'), None)
+    assert timeout_bragi_es == 1800
