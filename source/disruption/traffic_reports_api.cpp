da8ba234b53feccf29124150fba666a49cd4b2ff
krishna <krishna.adhikari@kisio.com>
2022-02-24 09:28:40 +0100

    Fix traffic_reports with forbidden_uris on physical_modes

--- source/disruption/traffic_reports_api.cpp
+++ source/disruption/traffic_reports_api.cpp
@@ -343,8 +343,17 @@ void TrafficReport::disruptions_list(const std::string& filter,
         return;
     }
 
-    type::Indexes network_idx = ptref::make_query(type::Type_e::Network, filter, forbidden_uris, d);
-    add_networks(network_idx, d, now, filter_period);
+    type::Indexes network_idx;
+    try {
+        network_idx = ptref::make_query(type::Type_e::Network, filter, forbidden_uris, d);
+    } catch (const std::exception&) {
+    }
+    if (network_idx.size() > 0) {
+        add_networks(network_idx, d, now, filter_period);
+    }
+    // Get networks without forbidden_uris as this filter is used to filter other pt_objects
+    // We should not exclude ant network at this level
+    network_idx = ptref::make_query(type::Type_e::Network, filter, d);
     add_lines(filter, forbidden_uris, d, now, filter_period);
     add_stop_areas(network_idx, filter, forbidden_uris, d, now, filter_period);
     add_vehicle_journeys(network_idx, filter, forbidden_uris, d, now, filter_period);
