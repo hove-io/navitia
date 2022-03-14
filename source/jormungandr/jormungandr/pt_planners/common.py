4b3b3887ad7ac658f41e093b10da8723d180d0a8
xlqian <patrick.qian@kisio.com>
2022-03-14 11:45:00 +0100

    use timeout

--- source/jormungandr/jormungandr/pt_planners/common.py
+++ source/jormungandr/jormungandr/pt_planners/common.py
@@ -46,10 +46,13 @@ from navitiacommon import response_pb2
 
 
 class ZmqSocket(six.with_metaclass(ABCMeta, object)):
-    def __init__(self, zmq_context, zmq_socket, zmq_socket_type=None):
+    def __init__(
+        self, zmq_context, zmq_socket, timeout=app.config.get('INSTANCE_TIMEOUT', 10000), zmq_socket_type=None
+    ):
         self.zmq_socket = zmq_socket
         self.context = zmq_context
         self.sockets = deque()
+        self.timeout = timeout
         self.zmq_socket_type = zmq_socket_type
         self.breaker = pybreaker.CircuitBreaker(
             fail_max=app.config.get(str('CIRCUIT_BREAKER_MAX_INSTANCE_FAIL'), 5),
@@ -75,11 +78,9 @@ class ZmqSocket(six.with_metaclass(ABCMeta, object)):
             if not socket.closed:
                 self.sockets.append((socket, time.time()))
 
-    def _send_and_receive(
-        self, request, timeout=app.config.get('INSTANCE_TIMEOUT', 10000), quiet=False, **kwargs
-    ):
+    def _send_and_receive(self, request, quiet=False, **kwargs):
         logger = logging.getLogger(__name__)
-        deadline = datetime.utcnow() + timedelta(milliseconds=timeout)
+        deadline = datetime.utcnow() + timedelta(milliseconds=self.timeout)
         request.deadline = deadline.strftime('%Y%m%dT%H%M%S,%f')
 
         with self.socket(self.context) as socket:
@@ -94,7 +95,7 @@ class ZmqSocket(six.with_metaclass(ABCMeta, object)):
                         request.request_id = kwargs['flask_request_id']
 
             socket.send(request.SerializeToString())
-            if socket.poll(timeout=timeout) > 0:
+            if socket.poll(timeout=self.timeout) > 0:
                 pb = socket.recv()
                 resp = response_pb2.Response()
                 resp.ParseFromString(pb)
