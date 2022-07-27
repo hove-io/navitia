a8663f9300c685781da19882ac444e9fc9dff5e0
Pascal Benchimol <pascal.benchimol@polytechnique.edu>
2022-07-27 18:05:47 +0200

    fix previous tests

--- source/jormungandr/tests/rail_sections_tests.py
+++ source/jormungandr/tests/rail_sections_tests.py
@@ -101,7 +101,7 @@ class TestRailSections(AbstractTestFixture):
 
     def test_disruption_api_with_rail_section(self):
         r = self.default_query('disruptions')
-        assert len(get_not_null(r, 'disruptions')) == 6
+        assert len(get_not_null(r, 'disruptions')) == 8
         assert r['disruptions'][0]['id'] == 'rail_section_on_line1-1'
         assert r['disruptions'][1]['id'] == 'rail_section_on_line1-2'
         assert r['disruptions'][2]['id'] == 'rail_section_on_line1-3'
@@ -265,11 +265,11 @@ class TestRailSections(AbstractTestFixture):
 
     def test_line_reports_impacted_by_rail_section(self):
         r = self.default_query('line_reports')
-        assert len(get_not_null(r, 'disruptions')) == 6
-        is_valid_rail_section_disruption(r['disruptions'][0])
+        assert len(get_not_null(r, 'disruptions')) == 8
         is_valid_rail_section_disruption(r['disruptions'][1])
         is_valid_rail_section_disruption(r['disruptions'][2])
         is_valid_rail_section_disruption(r['disruptions'][3])
+        is_valid_rail_section_disruption(r['disruptions'][4])
 
     def test_terminus_schedules_impacted_by_rail_section(self):
         # terminus_schedules on line:1 gives us 3 rail_section impacts
@@ -860,7 +860,7 @@ class TestRailSections(AbstractTestFixture):
         r = self.default_query('traffic_reports')
         # only one network (base_network) is disrupted
         assert len(r['traffic_reports']) == 1
-        assert len(r['traffic_reports'][0]['stop_areas']) == 32
+        assert len(r['traffic_reports'][0]['stop_areas']) == 41
 
         for sa in r['traffic_reports'][0]['stop_areas']:
             if sa['id'] in ['stopAreaB', 'stopAreaC', 'stopAreaD']:
