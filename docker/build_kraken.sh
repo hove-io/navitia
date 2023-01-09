e14340fffb4b5ddeff6935444451a98d19a7fd76
Pascal Benchimol <pascal.benchimol@polytechnique.edu>
2023-01-09 18:05:25 +0100

    another_docker_for_build

--- docker/build_kraken.sh
+++ docker/build_kraken.sh
@@ -2,5 +2,6 @@
 cd /build/navitia/
 DEB_BUILD_OPTIONS=nocheck dpkg-buildpackage -b
 cd ..
-chmod -R 777 .
 mv *.deb /build/navitia/
+chmod  777 /build/navitia/*.deb
+chmod -R 777 build/navitia/build_packages/
