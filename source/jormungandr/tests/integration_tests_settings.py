63fa53720a736a05966f5e2d9f130391cc947aa8
pbench <52410095+pbench@users.noreply.github.com>
2023-01-25 12:14:06 +0100

    Revert "remove instance circuit breaker"

--- source/jormungandr/tests/integration_tests_settings.py
+++ source/jormungandr/tests/integration_tests_settings.py
@@ -65,6 +65,9 @@ MODULES = {
     }
 }
 
+# circuit breaker parameters, for the tests by default we don't want the circuit breaker
+CIRCUIT_BREAKER_MAX_INSTANCE_FAIL = 99999
+CIRCUIT_BREAKER_INSTANCE_TIMEOUT_S = 1
 
 GRAPHICAL_ISOCHRONE = True
 HEAT_MAP = True
