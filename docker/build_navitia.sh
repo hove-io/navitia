2ed286f9252a7a495d5c09e1c82ec47ed1d7b251
Abderrahim AZIME <azime_1@yahoo.fr>
2023-02-23 17:26:01 +0100

    Push jormun image

--- docker/build_navitia.sh
+++ docker/build_navitia.sh
@@ -2,7 +2,4 @@
 cd /navitia/navitia/
 mkdir -p build && cd build
 cmake -DCMAKE_BUILD_TYPE=Release ../source
-echo "-----"
-echo $(($(grep -c '^processor' /proc/cpuinfo)+1))
-echo "-----"
 make -j$(($(grep -c '^processor' /proc/cpuinfo)+1)) kraken
