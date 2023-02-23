c17507c0e4c85e96675c973109b9374d1125b2cb
Abderrahim AZIME <azime_1@yahoo.fr>
2023-02-23 14:32:36 +0100

    logs

--- docker/build_navitia.sh
+++ docker/build_navitia.sh
@@ -2,5 +2,10 @@
 cd /build/navitia/
 navitia_build_dir="$navitia_dir"/build_release
 mkdir -p "$navitia_build_dir" && cd "$navitia_build_dir"
+echo "*********************"
+ls -lt
+echo "*********************"
+echo pwd
+echo "*********************"
 cmake -DCMAKE_BUILD_TYPE=Release ../source
 make -j$(($(grep -c '^processor' /proc/cpuinfo)+1))
