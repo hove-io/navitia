16aeb1091acfd2e42f000f79f57c880e7c00fcf3
Abderrahim AZIME <azime_1@yahoo.fr>
2023-02-23 17:10:11 +0100

    not build proto

--- source/scripts/build_protobuf.sh
+++ source/scripts/build_protobuf.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-SOURCE_PROTO=${SOURCE_PROTO:-"source/navitia-proto"}
-OUTPUT_PROTO=${SOURCE_PROTO:-"../navitiacommon/navitiacommon"}
-pushd $SOURCE_PROTO
-protoc --python_out=$OUTPUT_PROTO  type.proto response.proto request.proto task.proto stat.proto
-2to3 -w $OUTPUT_PROTO/*_pb2.py
+pushd source/navitia-proto
+protoc --python_out=../navitiacommon/navitiacommon  type.proto response.proto request.proto task.proto stat.proto
+2to3 -w ../navitiacommon/navitiacommon/*_pb2.py
 popd
