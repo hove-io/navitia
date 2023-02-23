83e6b29c6e0bf86f50fd5e53b05d7d3023e51f44
Abderrahim AZIME <azime_1@yahoo.fr>
2023-02-23 16:43:14 +0100

    create jormun image

--- docker/build_navitia.sh
+++ docker/build_navitia.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 cd /navitia/navitia/
 mkdir -p build && cd build
-echo "*********************"
-ls .
-echo "*********************"
-ls -lt  ../
-echo "*********************"
 cmake -DCMAKE_BUILD_TYPE=Release ../source
-make -j$(($(grep -c '^processor' /proc/cpuinfo)+1))
+echo "-----"
+echo $(($(grep -c '^processor' /proc/cpuinfo)+1))
+echo "-----"
+make -j$(($(grep -c '^processor' /proc/cpuinfo)+1)) kraken
