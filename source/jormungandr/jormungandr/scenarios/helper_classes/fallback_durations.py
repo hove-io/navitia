5ab0193fcd78bb9deb5a780e25799288965264f3
Abderrahim AZIME <abderrahim.azime@hove.com>
2023-04-18 17:57:37 +0200

    Optim

--- source/jormungandr/jormungandr/scenarios/helper_classes/fallback_durations.py
+++ source/jormungandr/jormungandr/scenarios/helper_classes/fallback_durations.py
@@ -317,14 +317,14 @@ class FallbackDurations:
 
     def include_poi_access_points(self):
         return (
-            self._requested_place_obj.embedded_type == type_pb2.POI
+            self._request.get("_poi_access_points", False)
+            and self._requested_place_obj.embedded_type == type_pb2.POI
             and self._mode
             in [
                 FallbackModes.walking.name,
                 FallbackModes.bike.name,
             ]
             and self._requested_place_obj.poi.children
-            and self._request.get("_poi_access_points", False)
         )
 
     def _determine_centers_isochrone(self):
