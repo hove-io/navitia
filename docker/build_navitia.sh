7c9e99e17552c28c5523169a0b1e3494d0569a16
Pascal Benchimol <pascal.benchimol@polytechnique.edu>
2023-05-04 14:47:48 +0200

    build everything in debian11

--- docker/build_navitia.sh
+++ docker/build_navitia.sh
@@ -2,4 +2,4 @@
 cd /navitia/navitia/
 mkdir -p build && cd build
 cmake -DCMAKE_BUILD_TYPE=Release ../source
-make -j$(($(grep -c '^processor' /proc/cpuinfo)+1)) kraken
+make -j$(($(grep -c '^processor' /proc/cpuinfo)+1))
