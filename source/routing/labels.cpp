7a94b050db2aa68af012421dc69d95277fc6f2e8
Pierre-Etienne Bougue <pierre-etienne.bougue@canaltp.fr>
2023-06-15 18:08:25 +0200

    After review, revert this change
    May be reconsidered in the future though.

--- source/routing/labels.cpp
+++ source/routing/labels.cpp
@@ -40,7 +40,7 @@ Labels::Labels(const std::vector<type::StopPoint*> stop_points) : labels(stop_po
 Labels::Labels(const Map& dt_pts, const Map& dt_transfers, const Map& walkings, const Map& walking_transfers) {
     const auto& zip = boost::combine(dt_pts, dt_transfers.values(), walkings.values(), walking_transfers.values());
     labels.resize(zip.size());
-    for (const auto z : zip) {
+    for (const auto& z : zip) {
         const auto sp_idx = z.get<0>().first;
         labels[sp_idx] = Label{z.get<0>().second, z.get<1>(), z.get<2>(), z.get<3>()};
     }
@@ -52,7 +52,7 @@ std::array<Labels::Map, 4> Labels::inrow_labels() {
     for (auto& row_label : row_labels)
         row_label.resize(labels.size());
 
-    for (const auto l : labels) {
+    for (const auto& l : labels) {
         const auto& sp_idx = l.first;
         const auto& label = l.second;
 
