c8f62c06e8b733d2d3eeb8a23857f896c722c6d3
kadhikari <krishna.adhikari@kisio.com>
2023-06-23 15:57:16 +0200

    Update first or last coord in shape for on_demand_transport section

--- source/jormungandr/jormungandr/scenarios/helper_classes/helper_utils.py
+++ source/jormungandr/jormungandr/scenarios/helper_classes/helper_utils.py
@@ -467,6 +467,10 @@ class BeginningFallback(object):
     def set_journey_bound_datetime(self, journey):
         journey.departure_date_time = journey.sections[0].begin_date_time
 
+    def update_shape_coord(self, journey, coord):
+        first_coord = self.get_first_non_crowfly_section(journey).shape[0]
+        first_coord.CopyFrom(coord)
+
 
 class EndingFallback(object):
     def get_first_non_crowfly_section(self, journey):
@@ -492,6 +496,10 @@ class EndingFallback(object):
 
     def set_journey_bound_datetime(self, journey):
         journey.arrival_date_time = journey.sections[-1].end_date_time
+
+    def update_shape_coord(self, journey, coord):
+        last_coord = self.get_first_non_crowfly_section(journey).shape[-1]
+        last_coord.CopyFrom(coord)
 
 
 def _build_crowflies(pt_journeys, orig, dest):
@@ -528,6 +536,8 @@ def _build_crowfly(pt_journey, entry_point, mode, places_free_access, fallback_d
 
     if pt_obj.uri in places_free_access.odt:
         pt_obj.CopyFrom(entry_point)
+        # Update first or last coord in the shape
+        fallback_logic.update_shape_coord(pt_journey, get_pt_object_coord(pt_obj))
         return None
 
     section_datetime = fallback_logic.get_pt_section_datetime(pt_journey)
