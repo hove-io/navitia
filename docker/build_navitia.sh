d1cdae6d43e6ad50140071fc0974f2d5a5239b83
Abderrahim AZIME <abderrahim.azime@hove.com>
2023-07-11 16:59:01 +0200

    Fix Jormun version

--- docker/build_navitia.sh
+++ docker/build_navitia.sh
@@ -2,5 +2,4 @@
 cd /navitia/navitia/
 mkdir -p docker_build && cd docker_build
 cmake -DCMAKE_BUILD_TYPE=Release ../source
-(cd jormungandr && make -j$(($(grep -c '^processor' /proc/cpuinfo)+1)))
 make -j$(($(grep -c '^processor' /proc/cpuinfo)+1)) protobuf_files
