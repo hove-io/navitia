af20a40aec53da49066a3880d40f4cb91436537b
Abderrahim AZIME <azime_1@yahoo.fr>
2023-09-25 11:17:05 +0200

    [Jormungandr] monitor jormungandr added

--- docker/run_jormungandr.sh
+++ docker/run_jormungandr.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+app_processes=${1-1}
+monitor_processes=${2-0}
+
+file=/usr/src/app/jormungandr.wsgi
+jormungandr_ini=/usr/src/app/jormungandr.ini
+monitor_ini=/usr/src/app/monitor.ini
+
 # run apache2
 service apache2 start
 if [ $? == 1 ]
@@ -7,8 +14,16 @@ then
   echo "Error: failed to start apache2";
   exit 1
 fi
+
 # run UWSGI
-uwsgi --http 0.0.0.0:9090 --file /usr/src/app/jormungandr.wsgi
+if [ $monitor_processes -eq 1 ]
+then
+  echo "!!!!!!!!!!!!!!!!!!!!! Start Jormungandr with monitoring service !!!!!!!!!!!!!!!!!!!!!"
+	uwsgi --ini $jormungandr_ini --file $file --processes $app_processes & uwsgi --http --ini $monitor_ini --file $file --processes $monitor_processes
+else
+  echo "!!!!!!!!!!!!!!!!!!!!! Start Jormungandr without monitoring service !!!!!!!!!!!!!!!!!!!!!"
+	uwsgi --ini $jormungandr_ini --file $file --processes $app_processes
+fi
 
 if [ $? == 1 ]
 then
