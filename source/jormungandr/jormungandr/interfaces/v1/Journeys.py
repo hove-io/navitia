0cefe0ebf3cb2daae1011a9e1db3b807162f721c
Kodmain <contact@making.codes>
2023-10-26 11:44:16 +0200

    minor update

--- source/jormungandr/jormungandr/interfaces/v1/Journeys.py
+++ source/jormungandr/jormungandr/interfaces/v1/Journeys.py
@@ -194,16 +194,17 @@ class add_journey_href(object):
                         del args["from"]
                         del args["to"]
                         for section in journey['sections']:
-                            if section.get('type') == 'street_network':
-                                coords = section.get('geojson').get('coordinates', [])
-                                coords_bytes = encode_polyline(coords)
-                                encoded_bytes = base64.b64encode(coords_bytes.encode('utf-8'))
-                                args["path"] = encoded_bytes.decode('utf-8')
-                                args["distance"] = 10
-                                args['rel'] = 'obstacles'
+                            if section.get('type') != 'street_network':
+                                continue
+                            coords = section.get('geojson').get('coordinates')
+                            coords_bytes = encode_polyline(coords)
+                            encoded_bytes = base64.b64encode(coords_bytes.encode('utf-8'))
+                            args["path"] = encoded_bytes.decode('utf-8')
+                            args["distance"] = 10
+                            args['rel'] = 'obstacles'
 
-                                obstacle = create_external_link('v1.obstacles_nearby', **args)
-                                section['links'].append(obstacle)
+                            obstacle = create_external_link('v1.obstacles_nearby', **args)
+                            section['links'].append(obstacle)
 
             return objects
 
