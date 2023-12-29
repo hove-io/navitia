10774af7500e64cf0f8f43eee236b2d7cd96fdef
Abderrahim AZIME <azime_1@yahoo.fr>
2023-12-29 11:54:49 +0100

    Revert "get_stop_points_for_stop_area by pt_planner"

--- source/jormungandr/jormungandr/pt_planners/common.py
+++ source/jormungandr/jormungandr/pt_planners/common.py
@@ -165,20 +165,3 @@ def get_odt_stop_points(pt_planner, coord, request_id):
     req.coord.lon = coord.lon
     req.coord.lat = coord.lat
     return pt_planner.send_and_receive(req, request_id=request_id).stop_points
-
-
-def get_stop_points_for_stop_area(pt_planner, uri, request_id):
-    req = request_pb2.Request()
-    req.requested_api = type_pb2.PTREFERENTIAL
-    req.ptref.requested_type = type_pb2.STOP_POINT
-    req.ptref.count = 100
-    req.ptref.start_page = 0
-    req.ptref.depth = 0
-    req.ptref.filter = 'stop_area.uri = {uri}'.format(uri=uri)
-
-    result = pt_planner.send_and_receive(req, request_id=request_id)
-    if not result.stop_points:
-        logging.getLogger(__name__).info(
-            'PtRef, Unable to find stop_point with filter {}'.format(req.ptref.filter)
-        )
-    return result.stop_points
