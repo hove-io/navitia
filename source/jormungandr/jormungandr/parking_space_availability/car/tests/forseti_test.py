02a5250610bfeb807603af1f4c7661eca69f83fe
Kodflow <133899878+kodflow@users.noreply.github.com>
2024-01-22 12:10:29 +0100

    update format

--- source/jormungandr/jormungandr/parking_space_availability/car/tests/forseti_test.py
+++ source/jormungandr/jormungandr/parking_space_availability/car/tests/forseti_test.py
@@ -59,10 +59,12 @@ def parking_space_availability_forseti_get_informations_test():
                 "name": "786 Parking",
                 "coord": {"lat": 42.368227, "lon": -83.0779357},
                 "availability": True,
-                "currency": "USD",
-                "startTime": "2019-04-01T00:00:00Z",
-                "endTime": "2019-04-01T23:59:59Z",
-                "amount": 6000,
+                "price": {
+                    "currency": "USD",
+                    "startTime": "2019-04-01T00:00:00Z",
+                    "endTime": "2019-04-01T23:59:59Z",
+                    "amount": 6000,
+                }
             }
         ],
         "pagination": {"start_page": 0, "items_on_page": 25, "items_per_page": 25, "total_result": 1},
