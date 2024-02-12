f90d891fc4ee6f9ee26c0585f3f86cebd49a380c
Patrick Qian <patrick.qian@hove.com>
2024-02-12 11:26:36 +0100

    fix when poi are missing and install aws

--- source/tyr/tyr/poi_to_excluded_zones.py
+++ source/tyr/tyr/poi_to_excluded_zones.py
@@ -5,6 +5,8 @@ import logging
 
 
 def poi_to_excluded_zones(poi_file, output_dir, instance_name):
+    logger = logging.getLogger(__name__)
+
     tmp_path = "tmp/poi_{}".format(instance_name)
     import zipfile
 
@@ -31,9 +33,9 @@ def poi_to_excluded_zones(poi_file, output_dir, instance_name):
             excluded_geometries_ids[row[0]] = row[7]
 
     if excluded_geometries_ids.keys() != excluded_zones.keys():
-        logger.error("not all excluded zone's pois are found in poi.txt")
-        logger.error("excluded_geometries_ids: {}".format(excluded_geometries_ids.keys()))
-        logger.error("excluded_zones: {}".format(excluded_zones.keys()))
+        logger.warning("not all excluded zone's pois are found in poi.txt")
+        logger.warning("excluded_geometries_ids: {}".format(excluded_geometries_ids.keys()))
+        logger.warning("excluded_zones: {}".format(excluded_zones.keys()))
 
     # read geometries
     geometries_shapes = {}
@@ -46,9 +48,12 @@ def poi_to_excluded_zones(poi_file, output_dir, instance_name):
         geometry_id = excluded_geometries_ids.get(poi_id)
         if not geometry_id:
             logger.error("{} could not be found in poi.txt".format(row[0]))
+            continue
+
         shape = geometries_shapes.get(geometry_id)
         if not shape:
             logger.error("{} could not be found in geometries.txt".format(geometry_id))
+            continue
 
         for i, zone in enumerate(zones):
             output_id = "{}_{}_{}".format(poi_id, i, instance_name)
