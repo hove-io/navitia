b2049876404dba8cdc7656709b94a6943cbb5c38
Abderrahim AZIME <azime_1@yahoo.fr>
2024-03-19 09:48:18 +0100

    fmt

--- source/kraken/tests/worker_test.cpp
+++ source/kraken/tests/worker_test.cpp
@@ -190,9 +190,8 @@ BOOST_FIXTURE_TEST_CASE(no_wheelchair_on_vj_tests, fixture) {
 
     BOOST_CHECK_EQUAL(resp.feed_publishers_size(), 1);
     pbnavitia::FeedPublisher fp = resp.feed_publishers(0);
-    BOOST_CHECK_EQUAL(fp.license(),"ODBL");
-    BOOST_CHECK_EQUAL(fp.name(),"builder");
-
+    BOOST_CHECK_EQUAL(fp.license(), "ODBL");
+    BOOST_CHECK_EQUAL(fp.name(), "builder");
 }
 
 BOOST_FIXTURE_TEST_CASE(wheelchair_on_vj_tests, fixture) {
@@ -210,8 +209,8 @@ BOOST_FIXTURE_TEST_CASE(wheelchair_on_vj_tests, fixture) {
 
     BOOST_CHECK_EQUAL(resp.feed_publishers_size(), 1);
     pbnavitia::FeedPublisher fp = resp.feed_publishers(0);
-    BOOST_CHECK_EQUAL(fp.license(),"ODBL");
-    BOOST_CHECK_EQUAL(fp.name(),"builder");
+    BOOST_CHECK_EQUAL(fp.license(), "ODBL");
+    BOOST_CHECK_EQUAL(fp.name(), "builder");
 }
 
 BOOST_FIXTURE_TEST_CASE(wheelchair_on_stop_tests, fixture) {
