c58d0f5d6655dfebab82e57e4d9c75e184355ccb
David Quintanel <david.quintanel@hove.com>
2024-10-21 17:15:45 +0200

    mock-kraken

--- docker/build_kraken_deb11.sh
+++ docker/build_kraken_deb11.sh
@@ -3,8 +3,16 @@
 cd /navitia/navitia/
 mkdir -p docker_build && cd docker_build
 cmake -DCMAKE_BUILD_TYPE=Release ../source
+
+# kraken
 make -j$(($(grep -c '^processor' /proc/cpuinfo)+1)) protobuf_files
 make -j$(($(grep -c '^processor' /proc/cpuinfo)+1)) kraken
+
+# mocks for integration tests
+make -j$(($(grep -c '^processor' /proc/cpuinfo)+1)) departure_board_test
+make -j$(($(grep -c '^processor' /proc/cpuinfo)+1)) main_routing_test
+
+# some binaries for tyr-worker
 make -j$(($(grep -c '^processor' /proc/cpuinfo)+1)) cities
 make -j$(($(grep -c '^processor' /proc/cpuinfo)+1)) ed2nav
 make -j$(($(grep -c '^processor' /proc/cpuinfo)+1)) fare2ed
