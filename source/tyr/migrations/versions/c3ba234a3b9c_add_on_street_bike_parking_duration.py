8ac0f19a2c56399f5d94900f18d401a5525affab
Pondonda <pondonda@gmail.com>
2024-12-03 09:35:38 +0100

    adding server_default

--- source/tyr/migrations/versions/c3ba234a3b9c_add_on_street_bike_parking_duration.py
+++ source/tyr/migrations/versions/c3ba234a3b9c_add_on_street_bike_parking_duration.py
@@ -15,7 +15,10 @@ import sqlalchemy as sa
 
 
 def upgrade():
-    op.add_column('instance', sa.Column('on_street_bike_parking_duration', sa.Integer(), nullable=False))
+    op.add_column(
+        'instance',
+        sa.Column('on_street_bike_parking_duration', sa.Integer(), nullable=False, server_default='500'),
+    )
 
 
 def downgrade():
