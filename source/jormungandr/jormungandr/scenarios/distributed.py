e823c01278fdee9f839f2e794188088363f8bd69
Pondonda <pondonda@gmail.com>
2024-12-11 13:37:17 +0100

    fix signe for arrival and max_bike_duration_to_pt

--- source/jormungandr/jormungandr/scenarios/distributed.py
+++ source/jormungandr/jormungandr/scenarios/distributed.py
@@ -184,9 +184,9 @@ class Distributed(object):
                 context.streetnetwork_path_pool.add_feed_publishers(request, requested_direct_path_modes, res)
                 return res
 
-            # if the parkmode is set to "on street" we need to add additional time to the max_bike_duration_to_pt
-            if ParkMode.on_street.name in request.get("park_mode", ""):
-                request["max_bike_duration_to_pt"] += request.get("on_street_bike_parking_duration", 0)
+            # if the parkmode is set to "on street" we need to subtract additional time to the max_bike_duration_to_pt
+            if ParkMode.on_street.name == request.get("park_mode", ""):
+                request["max_bike_duration_to_pt"] -= request.get("on_street_bike_parking_duration", 0)
 
             # We'd like to get the duration of a direct path to do some optimizations in ProximitiesByCrowflyPool and
             # FallbackDurationsPool.
