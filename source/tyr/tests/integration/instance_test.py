297d6325e3d502e282fba9becd3b88552f1b31f1
krishna <krishna.adhikari@hove.com>
2025-03-24 17:17:02 +0100

    Add some tests

--- source/tyr/tests/integration/instance_test.py
+++ source/tyr/tests/integration/instance_test.py
@@ -112,6 +112,19 @@ def test_get_and_put_instance_with_same_journey_schedules_configuration(create_i
         '/v0/instances/{}'.format(create_instance), data=json.dumps(params), content_type='application/json'
     )
     assert resp['same_journey_schedules_configuration'] == sjs_configuration
+    assert resp['same_journey_schedules_configuration']['min_nb_journeys'] == 5
+
+    sjs_configuration = {
+        "allowed_id_type": ["stop_point", "stop_area", "commercial_mode"],
+        "min_nb_journeys": 2,
+    }
+    params = {"same_journey_schedules_configuration": sjs_configuration}
+    resp = api_put(
+        '/v0/instances/{}'.format(create_instance), data=json.dumps(params), content_type='application/json'
+    )
+    assert resp['same_journey_schedules_configuration'] == sjs_configuration
+    assert "commercial_mode" in resp['same_journey_schedules_configuration']['allowed_id_type']
+    assert resp['same_journey_schedules_configuration']['min_nb_journeys'] == 2
 
 
 def test_get_instance_with_traveler_profile(create_instance):
