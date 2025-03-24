54e3997a888693a9eb9ee036661639fff50c556d
krishna <krishna.adhikari@hove.com>
2025-03-24 17:09:54 +0100

    Add test on put

--- source/tyr/tests/integration/instance_test.py
+++ source/tyr/tests/integration/instance_test.py
@@ -97,11 +97,21 @@ def test_get_instance(create_instance):
     assert resp[0]['id'] == create_instance
 
 
-def test_get_instance_with_same_journey_schedules_configuration(create_instance):
+def test_get_and_put_instance_with_same_journey_schedules_configuration(create_instance):
     resp = api_get('/v0/instances/fr')
     assert len(resp) == 1
     assert 'same_journey_schedules_configuration' in resp[0]
     assert resp[0]['same_journey_schedules_configuration'] == {}
+
+    sjs_configuration = {
+        "allowed_id_type": ["stop_point"],
+        "min_nb_journeys": 5,
+    }
+    params = {"same_journey_schedules_configuration": sjs_configuration}
+    resp = api_put(
+        '/v0/instances/{}'.format(create_instance), data=json.dumps(params), content_type='application/json'
+    )
+    assert resp['same_journey_schedules_configuration'] == sjs_configuration
 
 
 def test_get_instance_with_traveler_profile(create_instance):
