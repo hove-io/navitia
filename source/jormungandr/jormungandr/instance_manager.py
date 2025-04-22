f47c225509abc12673caf818f9d9f870b4f5fbdd
Patrick Qian <patrick.qian@hove.com>
2025-04-22 15:24:23 +0200

    fix when authorized instance is not configured

--- source/jormungandr/jormungandr/instance_manager.py
+++ source/jormungandr/jormungandr/instance_manager.py
@@ -323,7 +323,17 @@ class InstanceManager(object):
             # Requests without any coverage
             # fetch all the authorized instances (free + private) using cached function has_access()
             authorized_instances_name = self.get_all_available_instances_names(user)
-            authorized_instances = [self.instances[i_name] for i_name in authorized_instances_name]
+            authorized_instances = []
+            for i_name in authorized_instances_name:
+                if i_name in self.instances:
+                    authorized_instances.append(self.instances[i_name])
+                else:
+                    logging.getLogger(__name__).warning(
+                        '{} is authorized but is not available for Jormungandr, please check the instances configurations'.format(
+                            i_name
+                        )
+                    )
+
             if not authorized_instances:
                 # user doesn't have access to any of the instances
                 context = 'User has no access to any instance'
