2e066c6a4b50958d3799890a2849aa0eb0495ffe
krishna <krishna.adhikari@hove.com>
2025-06-11 11:42:42 +0200

    [Migration debian11]: Use binary mode

--- source/tyr/tyr/binarisation.py
+++ source/tyr/tyr/binarisation.py
@@ -854,7 +854,7 @@ def osm2mimir(self, autocomplete_instance, filename, job_id, dataset_uid, autoco
     custom_config_config_toml = '{}/{}.toml'.format(working_directory, custom_config)
     data = autocomplete_instance.config_toml.encode("utf-8")
     cosmogony_file = models.DataSet.get_cosmogony_file_path()
-    with open(custom_config_config_toml, 'w') as f:
+    with open(custom_config_config_toml, 'wb') as f:
         f.write(data)
     params = get_osm2mimir_params(
         autocomplete_instance,
