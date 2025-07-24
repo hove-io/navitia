4d56689d1e84504ee9dcda5c1e09db38321adfa8
vgeof-hove <vivien.geoffroy@hove.com>
2025-07-24 10:39:29 +0200

    Clean ci (#4421)
    * check
    
    * Clean-up CI after debian migration
    
    * Disable gcovr because time-consuming and no functionalities are developped
    
    * Stop running test again on releases. Push on dev is sufficient
    
    * remove unused clang-tidy automatic PR createion
    
    * add jormun image generation again
    
    * explain jormun build
    
    * Add call to corefront-aws-assets for jormun

--- docker/build_kraken.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd /build/navitia/
-DEB_BUILD_OPTIONS=nocheck dpkg-buildpackage -b
-cd ..
-mv *.deb /build/navitia/
-chmod  777 /build/navitia/*.deb
-chmod -R 777 /build/navitia/build_package/
