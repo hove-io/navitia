f1e8c741412759eb307daae57938fd2eea5734e6
Abderrahim AZIME <azime_1@yahoo.fr>
2026-02-23 08:59:54 +0100

    correction

--- source/jormungandr/jormungandr/interfaces/v1/Schedules.py
+++ source/jormungandr/jormungandr/interfaces/v1/Schedules.py
@@ -328,10 +328,13 @@ class add_passages_links:
             max_dt = "19000101T000000"
             min_dt = "29991231T235959"
             time_field = "arrival_date_time" if api == "arrivals" else "departure_date_time"
+            if not passages:
+                logging.getLogger(__name__).debug("Passage not found for {} api".format(api))
+                return response, status, other
             for passage_ in passages:
                 dt = passage_.get("stop_date_time", {}).get(time_field)
                 if not dt:
-                    logging.getLogger(__name__).error("Passage ignord, {} not found for {}", time_field, api)
+                    logging.getLogger(__name__).error("Passage ignord, {} not found for {}".format(time_field, api))
                     continue
                 if min_dt > dt:
                     min_dt = dt
@@ -339,7 +342,7 @@ class add_passages_links:
                     max_dt = dt
 
             if max_dt == "19000101T000000" or min_dt == "29991231T235959":
-                logging.getLogger(__name__).error("Unable to compute the next and previous links for {}", api)
+                logging.getLogger(__name__).error("Unable to compute the next and previous links for {}".format(api))
                 return response, status, other
 
             if "links" not in response:
