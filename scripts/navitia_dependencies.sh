bd8adb3f6c3d2b6d9fbc5a2bd1a6701e5c908049
Krishna Prasad Adhikari <krishna.adhikari@hove.com>
2026-06-22 10:50:21 +0200

    chore: nav-5795 delete mutualized image for CI
    Delte credential
    
    move code
    
    Add yes
    
    update for pre-commit
    
    clang installed
    
    Build test
    
    Formatting
    
    Add liblog4cplus-dev
    
    Adding libgoogle-perftools-dev
    
    Adding libssl-dev
    
    Uni installation
    
    rename job
    
    use depth
    
    clang-format
    
    Code reorganized with script file
    
    Install git before
    
    Add -y

---
+++ scripts/navitia_dependencies.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+apt clean
+apt update --fix-missing
+apt install -y -o Acquire::Retries=10 \
+    libpq5 python3.9-dev python3-pip \
+    git libgeos-c1v5 ca-certificates \
+    protobuf-compiler 2to3 \
+    libgoogle-perftools-dev libboost-all-dev \
+    libprotobuf-dev \
+    liblog4cplus-dev libzmq3-dev libpqxx-dev \
+    python3-setuptools  postgresql-client  \
+    gettext-base jq libssl-dev \
+    libosmpbf-dev libproj-dev gcc g++ cmake \
+    virtualenv clang-format
