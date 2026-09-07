4ad5ea1140015c2e918aaac1458fbb00d4f85dd2
Krishna Prasad Adhikari <krishna.adhikari@hove.com>
2026-09-07 15:18:52 +0200

    another test

--- scripts/navitia_dependencies.sh
+++ scripts/navitia_dependencies.sh
@@ -2,7 +2,9 @@
 set -e
 apt clean
 rm -rf /var/lib/apt/lists/*
-printf 'deb [check-valid-until=no] http://snapshot.debian.org/archive/debian/20260831T000000Z bullseye main\ndeb [check-valid-until=no] http://snapshot.debian.org/archive/debian-security/20250801T000000Z bullseye-security main\ndeb [check-valid-until=no] http://snapshot.debian.org/archive/debian/20250801T000000Z bullseye-updates main\n' > /etc/apt/sources.list
+echo "deb http://archive.debian.org/debian/ bullseye main" > /etc/apt/sources.list
+echo "deb http://archive.debian.org/debian/ bullseye-updates main" > /etc/apt/sources.list
+echo "deb http://archive.debian.org/debian-security/ bullseye-security main" > /etc/apt/sources.list
 apt-get -o Acquire::Check-Valid-Until=false update
 apt install -y -o Acquire::Retries=10 \
     libpq5 \
